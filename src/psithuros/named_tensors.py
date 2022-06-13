5abcb29d808aef4df8b90eeea1acc6359920c2ff
David Hall <dlwh@stanford.edu>
2022-06-12 23:26:08 -0700

    got constructors working for named arrays...

--- src/psithuros/named_tensors.py
+++ src/psithuros/named_tensors.py
@@ -2,7 +2,7 @@ import dataclasses
 import functools
 import inspect
 import typing
-from typing import List, Tuple, Dict, Sequence, Union, Optional, Callable, Iterable, _GenericAlias
+from typing import List, Tuple, Dict, Sequence, Union, Optional, Callable, Iterable, _GenericAlias, Any
 
 import equinox as eqx
 import jax
@@ -12,7 +12,7 @@ from jax import tree_structure
 from jax.experimental.maps import xmap, AxisName, ResourceSet
 
 
-class Array:
+class Array(jax.numpy.ndarray):
     def __class_getitem__(cls, item):
         if not isinstance(item, tuple):
             item = (item,)
@@ -24,51 +24,55 @@ class NamedArray:
     names: Tuple[AxisName, ...]
 
 
-def infer_named_axes_from_value(value):
-    if isinstance(value, eqx.Module):
-        return infer_named_axes_from_module(value)
-    elif isinstance(value, jax.numpy.ndarray):
-        return (...,)
-    elif isinstance(value, Iterable):
-        return [infer_named_axes_from_value(v) for v in value]
-
-
-
-
 def infer_named_axes_from_module(mod: eqx.Module):
     """Automatically get a "pytree" of named axes for an equinox Module. This can be passed to xmap."""
     # first split into the pytree
-    _, (dynamic_field_names, _, _) = mod.tree_flatten()
+    dynamic_values, aux = mod.tree_flatten()
+    dynamic_field_names = aux[0]
     fields: Sequence[dataclasses.Field] = dataclasses.fields(mod)  # type:ignore
     fields = {f.name: f for f in fields}
 
     named_shapes: List[Tuple[AxisName, ...]] = []
 
-    for name in dynamic_field_names:
+    for name, value in zip(dynamic_field_names, dynamic_values):
         if name not in fields:
             raise ValueError(f"Could not find field {name} in {mod.__class__}")
 
         field = fields[name]
-        shape = infer_named_axes_from_annotation(field.type)
+        shape = infer_named_axes(value=value, annotation=field.type)
         named_shapes.append(shape)
 
-    return named_shapes
+    return mod.__class__.tree_unflatten(aux, named_shapes)
 
-
-def infer_named_axes_from_annotation(annotation):
-    if isinstance(annotation, NamedArray):
-        return annotation.names
-    elif type(annotation) is _GenericAlias:
-        return tuple(infer_named_axes_from_annotation(a) for a in typing.get_args(annotation))
-    elif issubclass(annotation, eqx.Module):
+def infer_leaf_axes(annotation: type)-> List[Tuple[AxisName, ...]]:
+    if type(annotation) == eqx.module._ModuleMeta and issubclass(annotation, eqx.Module):
         # unfortunately need to replicate the logic in Module#tree_flatten
         shapes = []
         for field_ in dataclasses.fields(annotation):  # type: ignore
             if not field_.metadata.get("static", False):
-                shapes.append(infer_named_axes_from_annotation(field_.type))
-
+                shapes += infer_leaf_axes(field_.type)
         return shapes
-    return (...,)
+    elif isinstance(annotation, NamedArray):
+        return [annotation.names]
+    elif annotation is Array:
+        return [(...,)]
+    else:
+        return [(...,)]
+
+def infer_named_axes(value: Any, annotation: Optional[type]):
+    if isinstance(value, eqx.Module):
+        return infer_named_axes_from_module(value)
+    elif isinstance(annotation, NamedArray):
+        return annotation.names
+    elif isinstance(value, jax.numpy.ndarray):
+        return (...,)
+    elif annotation is Array:
+        return (...,)
+    else:
+        # TODO exploit tuple types: tuples, lists, dicts, etc.
+        leaves, structure = jax.tree_flatten(value)
+        leaf_axes = [infer_named_axes(leaf, None) for leaf in leaves]
+        return jax.tree_unflatten(structure, leaf_axes)
 
 
 def auto_xmap(fun: Callable = sentinel,
@@ -96,10 +100,10 @@ def auto_xmap(fun: Callable = sentinel,
         # We need the signature for figuring out the names of axes for passed-in arrays as well as what type
         # we're expected to return
         # infer the axes of all arguments:
-        arg_shapes = [infer_named_axes_from_value(arg) for arg in args]
+        arg_shapes = [infer_named_axes(arg, param.annotation) for (arg, param) in zip(args, sig.parameters.values())]
         if len(kwargs) > 0:
             raise NotImplementedError("kwargs not yet supported")
-        kwarg_shapes = {k: infer_named_axes_from_value(v) for k, v in kwargs.items()}
+        kwarg_shapes = {k: infer_named_axes(v, None) for k, v in kwargs.items()}
         # flatten the arguments into pytrees
         args_leaves, args_treedefs = jax.tree_flatten(args)
         kwargs_leaves_defs = {k: jax.tree_flatten(v) for k,v in kwargs.items()}
@@ -108,17 +112,17 @@ def auto_xmap(fun: Callable = sentinel,
 
         # attempt to figure out the return type
         # TODO: want to handle type vars...
-        return_axes = infer_named_axes_from_annotation(sig.return_annotation)
+        return_axes = infer_leaf_axes(sig.return_annotation)
 
         results_treedefs = None
 
         @functools.wraps(fun)
-        def function_to_xmap(*args_leaves, **kwargs_leaves):
+        def function_to_xmap(*args, **kwargs):
             # unflatten the arguments into pytrees
-            args_unflattened = [jax.tree_unflatten(treedef, leaf) for treedef, leaf in zip(args_treedefs, args_leaves)]
-            kwargs_unflattened = {k: jax.tree_unflatten(kwargs_treedefs[k], kwargs_leaves[k]) for k in kwargs_leaves}
+            # args_unflattened = [jax.tree_unflatten(treedef, leaf) for treedef, leaf in zip(args_treedefs, args_leaves)]
+            # kwargs_unflattened = {k: jax.tree_unflatten(kwargs_treedefs[k], kwargs_leaves[k]) for k in kwargs_leaves}
             # call the original function
-            results = fun(*args_unflattened, **kwargs_unflattened)
+            results = fun(*args, **kwargs)
             # flatten the results into pytrees
             nonlocal results_treedefs
             results_leaves, results_treedefs = jax.tree_flatten(results)
@@ -130,11 +134,61 @@ def auto_xmap(fun: Callable = sentinel,
         # TODO: need to handle return type
         # TODO: figure out how to use kwargs shapes
         f = xmap(function_to_xmap, in_axes=arg_shapes, out_axes=return_axes)
-        result_leaves = f(*args_leaves, **kwargs_leaves)
+        result_leaves = f(*args, **kwargs)
         result_unflattened = jax.tree_unflatten(results_treedefs, result_leaves)
         return result_unflattened
 
     return axis_extracting_function
+
+
+
+
+def xmapped_class(cls: typing.Type[eqx.Module]):
+
+    # this is pretty tricky to get right.
+    # we want to be able to make it appear that the class __init__ has
+    # an additional named argument "axis_sizes" that is a Dict[AxisName, int],
+    # analogous to the argument to xmap.
+    # To the user, when we call the class, we want them to pass in an axis_sizes
+    # when creating the class.
+    # Underneath, we first call the original __init__ with no arguments so that we can get a PyTree
+    # with the right shape to which we can apply axis_sizes. This gives us "out_axes".
+    # in_axes can be derived from looking at the arguments to the original __init__ and any accompanying types.
+    #
+    # Then we call xmap with the original __init__, the axis_sizes dict, the in_axes, and the out_axes.
+
+    # TODO: cache this?
+    @functools.wraps(cls.__new__)
+    def xmapped_new(wrapped_cls, *args, axis_sizes=None, axis_resources=None, backend=None, **kwargs):
+
+        tree_shape = None
+
+        def mk_instance(*args, **kwargs):
+            inst = cls(*args, **kwargs)
+            nonlocal tree_shape
+            leaves, tree_shape = jax.tree_flatten(inst)
+            return leaves
+        # # first, call the original __init__ with the original arguments, which will give us the original
+        # # module, from which we can derive the out_axes
+        # # this gives us the right shape to which we can apply axis_sizes
+        # print(kwargs)
+        # first, infer the axes of all arguments:
+        sig = inspect.signature(cls.__init__)
+        # bind the arguments to the signature, using None as "self" because we don't have or want it
+        bound_sig = sig.bind_partial(*((None,) + args), **kwargs)
+        bound_sig.apply_defaults()
+        # infer the axes of all arguments:
+        arg_shapes = [infer_named_axes(value, sig.parameters[arg_name].annotation) for arg_name, value in bound_sig.arguments.items() if arg_name != "self"]
+        # now infer the out_axes
+        out_axes = infer_leaf_axes(cls)
+
+        leaves = xmap(mk_instance, in_axes=arg_shapes, out_axes=out_axes, axis_sizes=axis_sizes or {}, axis_resources=axis_resources or {})(*args)
+        return jax.tree_unflatten(tree_shape, leaves)
+
+    # now return the class with the xmapped __init__
+    return type(cls.__name__ + "Wrapped", (), {"__new__": xmapped_new, "__init__": lambda self: ()})
+
+
 
 
 
--- tests/test_named_axes.py
+++ tests/test_named_axes.py
@@ -1,13 +1,21 @@
 from typing import Tuple
 
+import jax
 import optax
 import pytest
 
 import jax.numpy as jnp
 from jax import tree_structure, tree_flatten, tree_unflatten
 from jax.experimental.maps import xmap
-from psithuros.named_tensors import infer_named_axes_from_module, Array, auto_xmap
+from psithuros.named_tensors import *
 import equinox as eqx
+
+class MyModule(eqx.Module):
+    named: Array["x", "y"]
+    unnamed1: Array
+    unnamed2: Array[...]
+    partially_named: Array["x", ...]
+    static_field: int = eqx.static_field()
 
 def show_example(structured):
   flat, tree = tree_flatten(structured)
@@ -18,29 +26,29 @@ def show_example(structured):
 
 def test_infer_named_axes():
 
-    class MyModule(eqx.Module):
-        named: Array["x", "y"]
-        unnamed1: Array
-        unnamed2: Array[...]
-        partially_named: Array["x", ...]
-        static_field: int = eqx.static_field()
 
     mod = MyModule(named=jnp.ones((2, 3)), unnamed1=jnp.ones((2)), unnamed2=jnp.ones((2)), partially_named=jnp.ones((2, 3)), static_field=1)
     show_example(mod)
 
     axes = infer_named_axes_from_module(mod)
 
-    assert axes == [("x", "y"), (..., ), (...,), ("x", ...,)]
+    structure = jax.tree_structure(mod)
+    expected_axes = jax.tree_unflatten(structure, [("x", "y"), (..., ), (...,), ("x", ...,)])
+
+    assert axes == expected_axes
 
 
 def test_auto_xmap_identity():
-    class MyModule(eqx.Module):
-        named: Array["x", "y"]
-        unnamed1: Array
-        unnamed2: Array[...]
-        partially_named: Array["x", ...]
-        static_field: int = eqx.static_field()
+    def identity(x: Array["x", "y"]) -> Array["x", "y"]:
+        return x
 
+    arr = jnp.ones((2, 3))
+
+    fun = auto_xmap(identity)
+    assert jnp.all(jnp.isclose(fun(arr), arr))
+
+
+def test_auto_xmap_identity_module():
     mod = MyModule(named=jnp.ones((2, 3)), unnamed1=jnp.ones((2)), unnamed2=jnp.ones((2)), partially_named=jnp.ones((2, 3)), static_field=1)
 
     def identity(x) -> MyModule:
@@ -51,13 +59,6 @@ def test_auto_xmap_identity():
 
 
 def test_auto_xmap_optax():
-    class MyModule(eqx.Module):
-        named: Array["x", "y"]
-        unnamed1: Array
-        unnamed2: Array[...]
-        partially_named: Array["x", ...]
-        static_field: int = eqx.static_field()
-
     mod = MyModule(named=jnp.ones((2, 3)), unnamed1=jnp.ones((2)), unnamed2=jnp.ones((2)), partially_named=jnp.ones((2, 3)), static_field=1)
     adam = optax.adam(1E-4)
     opt = adam.init(mod)
@@ -68,3 +69,81 @@ def test_auto_xmap_optax():
     fun = auto_xmap(identity)
     assert fun(mod, opt) == (mod, opt)
 
+
+class MyModuleInit(eqx.Module):
+    named: Array["x", "y"]
+    unnamed1: Array
+    unnamed2: Array[...]
+    partially_named: Array["x", ...]
+    static_field: int = eqx.static_field()
+
+    def __init__(self):
+        self.named = jnp.ones(())
+        self.unnamed1 = jnp.ones(())
+        self.unnamed2 = jnp.ones(())
+        self.partially_named = jnp.ones(10)
+        self.static_field = 1
+
+
+def test_xmap_class_init():
+    XMappedModule = xmapped_class(MyModuleInit)
+    mod = XMappedModule(axis_sizes={"x": 2, "y": 3})
+    assert(isinstance(mod, MyModuleInit))
+    assert(mod.named.shape == (2, 3))
+    assert(mod.unnamed1.shape == ())
+    assert(mod.unnamed2.shape == ())
+    assert(mod.partially_named.shape == (2, 10))
+
+
+def test_xmap_class_nested_init():
+    class Mod2(eqx.Module):
+        inner: MyModuleInit
+
+        def __init__(self):
+            self.inner = MyModuleInit()
+
+    XMappedModule = xmapped_class(Mod2)
+    mod2 = XMappedModule(axis_sizes={"x": 2, "y": 3})
+    assert(isinstance(mod2, Mod2))
+    mod = mod2.inner
+    assert(mod.named.shape == (2, 3))
+    assert(mod.unnamed1.shape == ())
+    assert(mod.unnamed2.shape == ())
+    assert(mod.partially_named.shape == (2, 10))
+
+
+def test_xmap_class_init_with_args():
+    class ModWithArgs(eqx.Module):
+        array: Array["x", "y"]
+        array2: Array["x", ...]
+
+        def __init__(self, in_array: Array["x", "y"]):
+            self.array = in_array
+            self.array2 = jnp.zeros(10)
+
+    XMappedModule = xmapped_class(ModWithArgs)
+    in_array = jnp.ones((2, 3))
+    mod = XMappedModule(in_array)
+    assert(isinstance(mod, ModWithArgs))
+    assert(mod.array.shape == (2, 3))
+    assert(mod.array2.shape == (2, 10))
+
+def test_xmap_class_init_with_args_partial_dim():
+    class ModWithArgs(eqx.Module):
+        array: Array["x", "y"]
+        array2: Array["x", ...]
+        unaxised: Array[...]
+
+        def __init__(self, in_array: Array["y"], unaxised: Array[...]):
+            self.array = in_array
+            self.array2 = jnp.zeros(10)
+            self.unaxised = unaxised
+
+    XMappedModule = xmapped_class(ModWithArgs)
+    in_array = jnp.ones(3)
+    unaxised = jnp.ones(10)
+    mod = XMappedModule(in_array, unaxised, axis_sizes={"x": 2})
+    assert(isinstance(mod, ModWithArgs))
+    assert(mod.array.shape == (2, 3))
+    assert(mod.array2.shape == (2, 10))
+    assert(mod.unaxised.shape == (10, ))
