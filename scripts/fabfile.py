a4ba8d3a95b51a2cbc1acfcca71715d8e943d6cb
David Hall <dlwh@stanford.edu>
2022-06-28 16:05:21 -0700

    add fabfiles for setting up the tpus

---
+++ scripts/fabfile.py
@@ -0,0 +1,32 @@
+from fabric import *
+
+@task
+def hello(c):
+    c.run('echo "Hello World!"')
+
+
+@task
+def setup(c):
+    "run first time setup: upload the setup file and run it"
+    c.put('setup-tpu-vm.sh', 'setup-tpu-vm.sh')
+    c.sudo('bash setup-tpu-vm.sh')
+    enable_nfs(c)
+
+@task
+def enable_nfs(c):
+    c: Connection
+    nfs_server = c.config['nfs_server']
+    MOUNT_POINT = "/files"
+    c.sudo(f"mkdir -p {MOUNT_POINT}")
+    current_line = c.run(f'grep {nfs_server} /etc/fstab || true', warn=True, hide=True)
+    desired_line = f'{nfs_server}:/propulsion {MOUNT_POINT} nfs defaults 0 0'
+    if current_line.stdout != desired_line:
+        print(f"Current: {current_line}\nDesired: {desired_line}")
+        # first remove the bad line
+        c.sudo(f'grep -v {nfs_server} /etc/fstab > /tmp/fstab.new')
+        # then add the good line
+        c.sudo(f'echo "{desired_line}" >> /tmp/fstab.new')
+        # then move the new fstab back into place
+        c.sudo('mv /tmp/fstab.new /etc/fstab')
+
+    c.sudo('mount -a')
--- scripts/setup-tpu-vm.sh
+++ scripts/setup-tpu-vm.sh
@@ -1,4 +1,3 @@
-set -e
 # broadly based on https://github.com/ayaka14732/tpu-starter
 
 # tcmalloc interferes with intellij remote ide
@@ -9,25 +8,16 @@ sudo patch -b /etc/environment << EOF
 > #LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc.so.4"
 EOF
 
+# don't complain if already applied
+retCode=$?
+[[ $retCode -le 1 ]] || exit $retCode
+
 sudo apt update
 sudo apt upgrade -y
 
 # python 310
 sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt install -y python3.10-full python3.10-dev
+sudo apt install -y python3.10-full python3.10-dev nfs-common
 
-python3.10 -m venv $HOME/.venv310
-source $HOME/.venv310/bin/activate
-
-pip install -U pip
-pip install -U wheel
-
-# jax
-pip install -U "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-
-# TF
-#wget https://gist.github.com/ayaka14732/4954f64b7246beafabb45b636d96e92a/raw/d518753d166f3b77009d1f228101d93ff733d0d2/tensorflow-2.10.0-cp310-cp310-linux_x86_64.whl.0 https://gist.github.com/ayaka14732/4954f64b7246beafabb45b636d96e92a/raw/d518753d166f3b77009d1f228101d93ff733d0d2/tensorflow-2.10.0-cp310-cp310-linux_x86_64.whl.1
-#cat tensorflow-2.10.0-cp310-cp310-linux_x86_64.whl.0 tensorflow-2.10.0-cp310-cp310-linux_x86_64.whl.1 > tensorflow-2.10.0-cp310-cp310-linux_x86_64.whl
-#rm -f tensorflow-2.10.0-cp310-cp310-linux_x86_64.whl.0 tensorflow-2.10.0-cp310-cp310-linux_x86_64.whl.1
-#pip install tensorflow-2.10.0-cp310-cp310-linux_x86_64.whl
+echo "source /files/venv310/bin/activate" >> ~/.bashrc
