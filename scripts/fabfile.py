d3d96aff20414890ec7eb635022a72cb40506377
David Hall <dlwh@stanford.edu>
2022-07-17 21:20:58 -0700

    update setup scripts/fabfile for v256

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -11,23 +11,3 @@ def setup(c):
     c: Connection
     c.put('setup-tpu-vm.sh', 'setup-tpu-vm.sh')
     c.run('bash setup-tpu-vm.sh')
-    enable_nfs(c)
-
-@task
-def enable_nfs(c):
-    c: Connection
-    nfs_server = c.config['nfs_server']
-    MOUNT_POINT = "/files"
-    c.sudo(f"mkdir -p {MOUNT_POINT}")
-    current_line = c.run(f'grep {nfs_server} /etc/fstab || true', warn=True, hide=True)
-    desired_line = f'{nfs_server}:/propulsion {MOUNT_POINT} nfs defaults 0 0'
-    if current_line.stdout != desired_line:
-        print(f"Current: {current_line}\nDesired: {desired_line}")
-        # first remove the bad line
-        c.sudo(f'grep -v {nfs_server} /etc/fstab > /tmp/fstab.new')
-        # then add the good line
-        c.sudo(f'echo "{desired_line}" >> /tmp/fstab.new')
-        # then move the new fstab back into place
-        c.sudo('mv /tmp/fstab.new /etc/fstab')
-
-    c.sudo('mount -a')
--- scripts/setup-tpu-vm.sh
+++ scripts/setup-tpu-vm.sh
@@ -1,7 +1,8 @@
+set -x
 # broadly based on https://github.com/ayaka14732/tpu-starter
 
 # tcmalloc interferes with intellij remote ide
-sudo yes no | patch -b /etc/environment << EOF
+sudo patch -f -b /etc/environment << EOF
 2c2
 < LD_PRELOAD="/usr/lib/x86_64-linux-gnu/libtcmalloc.so.4"
 ---
@@ -14,10 +15,31 @@ retCode=$?
 
 #sudo apt update
 #sudo apt upgrade -y
-#
-## python 310
-#sudo apt install -y software-properties-common
-#sudo add-apt-repository -y ppa:deadsnakes/ppa
-#sudo apt install -y python3.10-full python3.10-dev nfs-common
 
-sudo bash -c 'echo "source /files/venv310/bin/activate" >> /etc/profile.d/activate_shared_venv.sh'
+# install python 3.10 and nfs
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt install -y python3.10-full python3.10-dev nfs-common
+
+
+# set up nfs
+NFS_SERVER=10.5.220.250
+MOUNT_POINT="/files"
+sudo mkdir -p ${MOUNT_POINT}
+CURRENT_NFS_ENTRY=$(grep ${NFS_SERVER} /etc/fstab)
+DESIRED_NFS_ENTRY="${NFS_SERVER}:/propulsion ${MOUNT_POINT} nfs defaults 0 0"
+# if different, fix
+if [ "$CURRENT_NFS_ENTRY" != "$DESIRED_NFS_ENTRY" ]; then
+  set -e
+  echo "Setting up nfs"
+  grep -v "${NFS_SERVER}" /etc/fstab > /tmp/fstab.new
+  echo "${DESIRED_NFS_ENTRY}" >> /tmp/fstab.new
+  # then move the new fstab back into place
+  sudo cp /etc/fstab /etc/fstab.orig
+  sudo mv /tmp/fstab.new /etc/fstab
+fi
+sudo mount -a
+
+
+# default to loading the venv
+sudo bash -c "echo \"source ${MOUNT_POINT}/venv310/bin/activate\" > /etc/profile.d/activate_shared_venv.sh"
