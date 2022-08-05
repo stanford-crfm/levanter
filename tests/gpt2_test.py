aee0842e5eb6ec4723ba5b44f329aa775520ff30
David Hall <dlwh@stanford.edu>
2022-08-05 11:22:36 -0700

    bring back dropout, change how heads are arranged in attention to maybe get some better fusion?

--- tests/gpt2_test.py
+++ tests/gpt2_test.py
@@ -9,7 +9,7 @@ def test_gradient_checkpointing():
     # ensure that gradient checkpointing doesn't change the output
     # (this is a regression test for a bug that caused the output to change)
     for num_blocks in [1, 2, 4, 8, 12]:
-        config = Gpt2Config(seq_len=16, hidden_dim=64, num_layers=num_blocks, num_heads=8, gradient_checkpointing=False)
+        config = Gpt2Config(seq_len=16, hidden_dim=72, num_layers=num_blocks, num_heads=8, gradient_checkpointing=False)
         config_checkpoint = dataclasses.replace(config, gradient_checkpointing=True)
         key = PRNGKey(0)
 
@@ -18,7 +18,7 @@ def test_gradient_checkpointing():
         model = Gpt2LMHeadModel(vocab, config, key=key)
         model_checkpoint = Gpt2LMHeadModel(vocab, config_checkpoint, key=key)
 
-        input_ids = jnp.arange(128, dtype=jnp.int32)
+        input_ids = jnp.arange(16, dtype=jnp.int32)
 
         a1 = model(input_ids, key)
         a2 = model_checkpoint(input_ids, key)
