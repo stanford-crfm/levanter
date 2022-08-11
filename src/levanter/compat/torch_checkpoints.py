da05cac419f1fb951c11659372dd6ad5cae4d976
David Hall <dlwh@stanford.edu>
2022-08-10 23:18:41 -0700

    factor out loading torch weights

--- src/levanter/compat/torch_checkpoints.py
+++ src/levanter/compat/torch_checkpoints.py
@@ -6,9 +6,8 @@ from typing import Optional
 
 import numpy as np
 import torch
-import torch.nn as nn
 
-# torch checkpoint
+import equinox as eqx
 from huggingface_hub import hf_hub_url, cached_download
 import jax
 from jax.random import PRNGKey
@@ -70,7 +69,18 @@ def load_torch_gpt2_checkpoint(location_or_id, map_location=None):
     key = PRNGKey(0)
     model = Gpt2LMHeadModel(vocab, lev_config, key=key)
 
+    model = use_torch_weights(model, checkpoint)
+
+    return model
+
+
+def use_torch_weights(model: eqx.Module, checkpoint: dict)-> eqx.Module:
+    """Given an equinox Module that implements torch_key_leaves (as per our gpt2 implementation),
+    return a modified version of the module with the weights initialized to those values"""
+
+    # TODO: make a class for torch_key_leaves that we can use here
     torch_keys = model.torch_key_leaves()
+
     def to_jax(t: Optional[torch.Tensor])->Optional[jnp.ndarray]:
         if t is None:
             return None
@@ -83,6 +93,7 @@ def load_torch_gpt2_checkpoint(location_or_id, map_location=None):
         assert old.shape == new.shape, f"{old.shape} != {new.shape}"
 
     model = jax.tree_unflatten(structure, tensors)
+
     return model
 
 
@@ -103,7 +114,7 @@ if __name__ == '__main__':
 
     input = rand_input(PRNGKey(0), 1, config.n_positions)
 
-    torch_out = torch_model(torch.Tensor(onp.array(input, dtype=onp.int_)).to(torch.int32))
+    torch_out = torch_model(torch.from_numpy(onp.array(input)).to(torch.int32))
     torch_out = torch_out.logits[0].detach().cpu().numpy()
 
     jax_out = model(input[0], key=None)
