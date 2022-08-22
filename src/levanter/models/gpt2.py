e4dd4cb7a322edb5bb00db841c9418259d86793a
David Hall <dlwh@stanford.edu>
2022-08-22 11:40:18 -0700

    fix for key/infernece just to be sure

--- src/levanter/models/gpt2.py
+++ src/levanter/models/gpt2.py
@@ -381,10 +381,12 @@ class Gpt2LMHeadModel(TorchSerializationMixin, eqx.Module):
             mp=mp,
         )
 
-    def __call__(self, input_ids, key):
+    def __call__(self, input_ids, *, inference, key):
+        if not inference and key is None:
+            raise ValueError("key must be provided for training")
         k_embed, k_transformer = jax_utils.maybe_rng_split(key, 2)
-        hidden_states = self.embeddings.embed(input_ids, inference=key is None, key=k_embed)
-        hidden_states = self.transformer(hidden_states, inference=key is None, key=k_transformer)
+        hidden_states = self.embeddings.embed(input_ids, inference=inference, key=k_embed)
+        hidden_states = self.transformer(hidden_states, inference=inference, key=k_transformer)
         lm_logits = self.embeddings.unembed(hidden_states)
 
         return lm_logits
