8730e219bb31731b21f9afffe51aad4fea5612de
David Hall <dlwh@stanford.edu>
2022-08-22 14:07:05 -0700

    add test that we can scan over the not 0th axis

--- tests/test_haliax_hof.py
+++ tests/test_haliax_hof.py
@@ -18,3 +18,18 @@ def test_scan():
 
     assert jnp.all(jnp.isclose(total, jnp.sum(named1.array, axis=(0, 1, 2))))
     assert jnp.all(jnp.equal(selected.array, named1.take(Width, 2).array))
+
+
+def test_scan_not_0th_axis():
+    Height = Axis("Height", 10)
+    Width = Axis("Width", 3)
+    Depth = Axis("Depth", 4)
+    named1 = hax.random.uniform(PRNGKey(0), (Height, Width, Depth))
+
+    def scan_fun(acc, x):
+        return acc + jnp.sum(x.array), x.take(Width, 2)
+
+    total, selected = hax.scan(scan_fun, Depth, 0.0, named1)
+
+    assert jnp.all(jnp.isclose(total, jnp.sum(named1.array, axis=(0, 1, 2))))
+    assert jnp.all(jnp.equal(selected.array, named1.take(Width, 2).rearrange(selected.axes).array))
