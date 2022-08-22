029725764f214e0acb7d0edaf5dd40c453eaa2b7
David Hall <dlwh@stanford.edu>
2022-08-22 14:09:15 -0700

    add fold_left since it's so much simpler

--- src/haliax/hof.py
+++ src/haliax/hof.py
@@ -1,4 +1,5 @@
 import dataclasses
+from functools import partial
 from typing import Any, Callable, Tuple, TypeVar
 
 import jax
@@ -39,8 +40,6 @@ def scan(f: Callable[[Carry, X], Tuple[Carry, Y]], axis: Axis, init: Carry, xs: 
     """
     Scan over a named axis. Arrays that are not part of a named axis will have their 0th dim scanned over
     """
-    if axis.size == 0:
-        return init, xs
 
     # This implementation is a bit tricky.
     # First we have to hoist the axis we're scanning over to the front of the array.
@@ -49,26 +48,10 @@ def scan(f: Callable[[Carry, X], Tuple[Carry, Y]], axis: Axis, init: Carry, xs: 
     # but don't yet have the scanned axis as ones of `axes`, so we use _ScannedArrayResult that doesn't check
     # invariants until we're ready to create the result.
 
-    def ensure_first(leaf):
-        if isinstance(leaf, NamedArray):
-            return leaf.rearrange((axis, ...))
-        else:
-            return leaf
-
-    def named_array_leaf(leaf):
-        return isinstance(leaf, NamedArray)
-
-    axis_first_xs = jax.tree_util.tree_map(ensure_first, xs, is_leaf=named_array_leaf)
+    axis_first_xs = jax.tree_util.tree_map(partial(_ensure_first, axis), xs, is_leaf=_named_array_leaf)
 
     # now get a template for where we fold over the axis in question
-    def select_0th(leaf):
-        if isinstance(leaf, NamedArray):
-            return leaf.take(axis, 0)
-        else:
-            # other leaves don't matter
-            return leaf
-
-    x_elem = jax.tree_util.tree_map(select_0th, axis_first_xs, is_leaf=named_array_leaf)
+    x_elem = jax.tree_util.tree_map(partial(_select_0th, axis), axis_first_xs, is_leaf=_named_array_leaf)
     x_elem_structure = jax.tree_util.tree_structure(x_elem)
 
     def chill_named_arrays(leaf):
@@ -81,7 +64,7 @@ def scan(f: Callable[[Carry, X], Tuple[Carry, Y]], axis: Axis, init: Carry, xs: 
     def wrapped_fn(carry, x):
         x = jax.tree_util.tree_unflatten(x_elem_structure, x)
         carry, y = f(carry, x)
-        y = jax.tree_util.tree_map(chill_named_arrays, y, is_leaf=named_array_leaf)
+        y = jax.tree_util.tree_map(chill_named_arrays, y, is_leaf=_named_array_leaf)
         return carry, y
 
     leaves = jax.tree_util.tree_leaves(axis_first_xs)
@@ -98,4 +81,58 @@ def scan(f: Callable[[Carry, X], Tuple[Carry, Y]], axis: Axis, init: Carry, xs: 
     return carry, ys
 
 
-__all__ = ["scan"]
+def _select_0th(axis, leaf):
+    if isinstance(leaf, NamedArray):
+        return leaf.take(axis, 0)
+    else:
+        # other leaves don't matter
+        return leaf
+
+
+def _named_array_leaf(leaf):
+    return isinstance(leaf, NamedArray)
+
+
+def _ensure_first(axis, leaf):
+    if isinstance(leaf, NamedArray):
+        return leaf.rearrange((axis, ...))
+    else:
+        return leaf
+
+
+def fold_left(
+    fn: Callable[[Carry, X], Carry], axis: Axis, init: Carry, xs: X, reverse: bool = False, unroll: int = 1
+) -> Carry:
+    """
+    Slightly simpler implementation of scan that folds over the first axis of the array, not returning intermediates.
+
+    If reverse is True, the fold is actually a fold_right
+    """
+    if axis.size == 0:
+        return init
+
+    # This implementation is a bit tricky.
+    # First we have to hoist the axis we're scanning over to the front of the array.
+    # Then we have to scan over the 0th dim of the arrays (as flattened non-pytrees)
+    # We have to be careful that we don't try to create NamedArrays that have the shape of the scanned result
+    # but don't yet have the scanned axis as ones of `axes`, so we use _ScannedArrayResult that doesn't check
+    # invariants until we're ready to create the result.
+
+    axis_first_xs = jax.tree_util.tree_map(partial(_ensure_first, axis), xs, is_leaf=_named_array_leaf)
+
+    # now get a template for where we fold over the axis in question
+    x_elem = jax.tree_util.tree_map(partial(_select_0th, axis), axis_first_xs, is_leaf=_named_array_leaf)
+    x_elem_structure = jax.tree_util.tree_structure(x_elem)
+
+    # now we can fold over the axis
+    def wrapped_fn(carry, x):
+        x = jax.tree_util.tree_unflatten(x_elem_structure, x)
+        return fn(carry, x), None
+
+    leaves = jax.tree_util.tree_leaves(axis_first_xs)
+    carry, _ = lax.scan(wrapped_fn, init, leaves, reverse=reverse, unroll=unroll)
+
+    return carry
+
+
+__all__ = ["scan", "fold_left"]
