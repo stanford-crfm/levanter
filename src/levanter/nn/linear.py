0a6506e1c7bea29006e4415a51a2936d1f309b90
David Hall <dlwh@stanford.edu>
2022-08-26 14:15:21 -0700

    used namedarray a bit more so that we can shard it

--- src/levanter/nn/linear.py
+++ src/levanter/nn/linear.py
@@ -1,10 +1,10 @@
-from typing import List, Optional
+from typing import List, Optional, Sequence, Tuple, TypeVar, Union
 
 import equinox as eqx
 import jmp
 
 import haliax as hax
-from haliax import Axis, NamedArray
+from haliax import AxisSpec, NamedArray
 from levanter.jax_utils import named_call
 
 
@@ -12,16 +12,14 @@ class NamedLinear(eqx.Module):
     weight: NamedArray
     bias: Optional[NamedArray]
 
-    in_axis: Axis = eqx.static_field()
-    out_axis: Axis = eqx.static_field()
+    in_axis: AxisSpec = eqx.static_field()
+    out_axis: AxisSpec = eqx.static_field()
     mp: jmp.Policy = eqx.static_field()
 
-    def __init__(self, in_axis: Axis, out_axis: Axis, *, key, mp: jmp.Policy, include_bias=True):
-        self.weight = hax.random.normal(key, (in_axis, out_axis), dtype=mp.param_dtype) * 0.02
-        if include_bias:
-            self.bias = hax.zeros(out_axis, dtype=mp.param_dtype)
-        else:
-            self.bias = None
+    def __init__(self, in_axis: AxisSpec, out_axis: AxisSpec, *, key, mp: jmp.Policy, include_bias=True):
+        joint_spec = hax.concat_axis_specs(in_axis, out_axis)
+        self.weight = hax.random.normal(key, joint_spec, dtype=mp.param_dtype) * 0.02
+        self.bias = hax.zeros(out_axis, dtype=mp.param_dtype) if include_bias else None
 
         self.in_axis = in_axis
         self.out_axis = out_axis
@@ -29,16 +27,14 @@ class NamedLinear(eqx.Module):
 
     @named_call(name="linear")
     def __call__(self, inputs):
-        # TODO: actually take and output named arrays
-        # out = inputs.dot(self.in_axis, self.weight)
-
-        kernel = self.weight.array
-        q = inputs @ kernel
+        q: NamedArray = inputs.dot(self.in_axis, self.weight)
         q = self.mp.cast_to_compute(q)
-
         if self.bias is not None:
-            q = q + self.bias.array
-            q = self.mp.cast_to_compute(q)
+            # TODO: add support for binary ops like addition to named axes and broadcasting
+            out_axis = _ensure_tuple(self.out_axis)
+            arr = q.rearrange((...,) + out_axis).array + self.bias.array
+            arr = self.mp.cast_to_output(arr)
+            q = NamedArray(arr, q.axes)
 
         return q
 
@@ -51,3 +47,12 @@ def _apply_prefix(prefix: Optional[str], leaves: List[Optional[str]]) -> List[Op
         return leaves
     else:
         return [prefix + "." + leaf if leaf else prefix for leaf in leaves]
+
+
+T = TypeVar("T")
+
+
+def _ensure_tuple(x: Union[Sequence[T], T]) -> Tuple[T, ...]:
+    if isinstance(x, Sequence):
+        return tuple(x)
+    return (x,)
