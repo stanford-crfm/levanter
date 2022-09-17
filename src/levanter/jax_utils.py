460aef3ede019df4d7195862ef0fcb55fd964807
David Hall <dlwh@stanford.edu>
2022-09-16 22:43:47 -0700

    improve comments

--- src/levanter/jax_utils.py
+++ src/levanter/jax_utils.py
@@ -1,10 +1,9 @@
 import functools as ft
 from pathlib import Path
-from typing import Callable, Optional, Sequence, Tuple, TypeVar, Union
+from typing import Callable, Tuple, TypeVar
 
 import equinox as eqx
 import jax
-import numpy as np
 from chex import PRNGKey
 from equinox.custom_types import PyTree
 from jax import lax
@@ -13,31 +12,7 @@ from jax import random as jrandom
 from jax.experimental.global_device_array import GlobalDeviceArray
 from jax.interpreters.pxla import PartitionSpec
 
-
-def maybe_rng_split(key: Optional[PRNGKey], num: int = 2):
-    """Splits a random key into multiple random keys. If the key is None, then it replicates the None. Also handles
-    num == 1 case"""
-    if key is None:
-        return [None] * num
-    elif num == 1:
-        return jnp.reshape(key, (1,) + key.shape)
-    else:
-        return jrandom.split(key, num)
-
-
-def shaped_rng_split(key, split_shape: Union[int, Sequence[int]] = 2) -> jrandom.KeyArray:
-    if isinstance(split_shape, int):
-        num_splits = split_shape
-        split_shape = (num_splits,) + key.shape
-    else:
-        num_splits = np.prod(split_shape)
-        split_shape = tuple(split_shape) + key.shape
-
-    if num_splits == 1:
-        return jnp.reshape(key, split_shape)
-
-    unshaped = maybe_rng_split(key, num_splits)
-    return jnp.reshape(unshaped, split_shape)
+from haliax.jax_utils import shaped_rng_split
 
 
 def jnp_to_python(a: jnp.ndarray):
