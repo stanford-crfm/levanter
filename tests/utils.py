7ad626cf4e28cd881a290bcd2155263f7e2da310
David Hall <dlwh@stanford.edu>
2022-10-05 16:22:24 -0700

    move some test utility to test util files

--- tests/utils.py
+++ tests/utils.py
@@ -1,6 +1,99 @@
+from typing import Callable, List, Optional
+
+import equinox as eqx
 import jax
 import pytest
+from chex import assert_trees_all_close
+from equinox import nn as nn
+from equinox import static_field
 
 
 def skip_if_not_enough_devices(count: int):
     return pytest.mark.skipif(len(jax.devices()) < count, reason=f"Not enough devices ({len(jax.devices())})")
+
+
+class MLP(eqx.Module):
+    """slightly less annoying MLP, used for testing purposes"""
+
+    layers: List[nn.Linear]
+    activation: Callable = eqx.static_field()
+    final_activation: Callable = eqx.static_field()
+    in_size: int = static_field()
+    out_size: int = static_field()
+    width_size: int = static_field()
+    depth: int = static_field()
+
+    def __init__(
+        self,
+        in_size: int,
+        out_size: int,
+        width_size: int,
+        depth: int,
+        activation: Callable = jax.nn.relu,
+        final_activation: Callable = lambda x: x,
+        *,
+        key: "jax.random.PRNGKey",
+        **kwargs,
+    ):
+        """**Arguments**:
+
+        - `in_size`: The size of the input layer.
+        - `out_size`: The size of the output layer.
+        - `width_size`: The size of each hidden layer.
+        - `depth`: The number of hidden layers.
+        - `activation`: The activation function after each hidden layer. Defaults to
+            ReLU.
+        - `final_activation`: The activation function after the output layer. Defaults
+            to the identity.
+        - `key`: A `jax.random.PRNGKey` used to provide randomness for parameter
+            initialisation. (Keyword only argument.)
+        """
+
+        super().__init__(**kwargs)
+        keys = jax.random.split(key, depth + 1)
+        layers = []
+        if depth == 0:
+            layers.append(nn.Linear(in_size, out_size, key=keys[0]))
+        else:
+            layers.append(nn.Linear(in_size, width_size, key=keys[0]))
+            for i in range(depth - 1):
+                layers.append(nn.Linear(width_size, width_size, key=keys[i + 1]))
+            layers.append(nn.Linear(width_size, out_size, key=keys[-1]))
+        self.layers = layers
+        self.in_size = in_size
+        self.out_size = out_size
+        self.width_size = width_size
+        self.depth = depth
+        self.activation = activation  # type: ignore
+        self.final_activation = final_activation  # type: ignore
+
+    def __call__(self, x, *, key: Optional["jax.random.PRNGKey"] = None):
+        """**Arguments:**
+
+        - `x`: A JAX array with shape `(in_size,)`.
+        - `key`: Ignored; provided for compatibility with the rest of the Equinox API.
+            (Keyword only argument.)
+
+        **Returns:**
+
+        A JAX array with shape `(out_size,)`.
+        """
+        for layer in self.layers[:-1]:
+            x = layer(x)
+            x = self.activation(x)
+        x = self.layers[-1](x)
+        x = self.final_activation(x)
+        return x
+
+
+def assert_trees_not_close(a, b):
+    try:
+        assert_trees_all_close(jax.tree_util.tree_leaves(arrays_only(a)), jax.tree_util.tree_leaves(arrays_only(b)))
+    except AssertionError:
+        pass
+    else:
+        raise AssertionError("Trees are equal")
+
+
+def arrays_only(x):
+    return eqx.filter(x, eqx.is_inexact_array_like)
