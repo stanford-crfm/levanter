e409f4c9822b535990f17de0844d3069c30bf23c
David Hall <dlwh@stanford.edu>
2022-10-05 10:10:15 -0700

    use TensorStore for serialization, which will let us do distributed serialization

---
+++ src/levanter/tensorstore_serialization.py
@@ -0,0 +1,106 @@
+# References:
+# * Orbax: https://github.com/google/orbax/blob/11d2934ecfff77e86b5e07d0fef02b67eff4511b/orbax/checkpoint/pytree_checkpoint_handler.py#L312
+import asyncio
+import logging
+from functools import partial
+
+import jax
+import jax.experimental.gda_serialization.serialization as gda_ser
+import jax.numpy as jnp
+import jax.tree_util as jtu
+import numpy as np
+import tensorstore
+from jax.experimental.global_device_array import GlobalDeviceArray
+from jax.interpreters.pxla import ShardedDeviceArray
+from tensorstore import TensorStore
+
+from levanter import jax_utils
+
+
+logger = logging.getLogger(__name__)
+
+
+def tree_serialize_leaves_tensorstore(checkpoint_dir, pytree):
+    leaf_key_paths = jax_utils.leaf_key_paths(pytree)
+    specs = jtu.tree_map(partial(_tensorstore_spec_for, checkpoint_dir), leaf_key_paths)
+
+    # TODO: jax gda_ser has a fancy async manager thing to checkpoint while training, would be good but not right now.
+    # gda_ser only supports saving GlobalDeviceArrays, so we can't use its top-level function run_serialization.
+    # however we're inspired by its implementation, meaning we'll make a tree of futures and wait on them.
+    async def _do_serialize():
+        futures = jtu.tree_map(_serialize_one_leaf, pytree, specs)
+        return await asyncio.gather(*jtu.tree_leaves(futures))
+
+    asyncio.run(_do_serialize())
+
+
+def _tensorstore_spec_for(checkpoint_dir, key_path: str):
+    checkpoint_path = f"{checkpoint_dir}/{key_path.replace('.', '/')}"
+    ts_spec = gda_ser.get_tensorstore_spec(checkpoint_path)
+    return ts_spec
+
+
+async def _serialize_one_leaf(x, spec):
+    if isinstance(x, GlobalDeviceArray):
+        return await gda_ser.async_serialize(x, spec)
+    elif isinstance(x, (bool, float, complex, int)):
+        return await save_array_to_tensorstore(np.array(x), spec)
+    elif x is None:
+        return
+    elif isinstance(x, jnp.ndarray):
+        return await save_array_to_tensorstore(x, spec)
+    elif isinstance(x, np.ndarray):
+        return await save_array_to_tensorstore(x, spec)
+    else:
+        raise TypeError(f"Can't serialize {type(x)}")
+
+
+async def save_array_to_tensorstore(x, spec):
+    # TODO: to support ShardedDeviceArray, we have to figure out how to identify what slice of the array
+    # we have.
+    assert not isinstance(x, ShardedDeviceArray), "ShardedDeviceArray not supported currently"
+    if jax.process_index() == 0:
+        if x.dtype == jnp.bfloat16:
+            # Tensorstore uses 'bfloat16', not '<V2'.
+            dtype = "bfloat16"
+        else:
+            dtype = np.dtype(x.dtype).str
+        t = await tensorstore.open(
+            tensorstore.Spec(spec), create=True, shape=x.shape, dtype=dtype, context=gda_ser.TS_CONTEXT
+        )
+
+        await t.write(x)
+
+
+async def load_array_from_tensorstore(spec):
+    t: TensorStore = await tensorstore.open(tensorstore.Spec(spec), context=gda_ser.TS_CONTEXT)
+    return await t.read("C")
+
+
+async def _deserialize_one_leaf(like, spec):
+    if isinstance(like, GlobalDeviceArray):
+        return await gda_ser.async_deserialize(
+            like.mesh, like.mesh_axes, spec, global_shape=like.shape, dtype=like.dtype
+        )
+    elif isinstance(like, (bool, float, complex, int)):
+        arr = await load_array_from_tensorstore(spec)
+        return arr.item()
+    elif like is None:
+        return None
+    elif isinstance(like, jnp.ndarray) or isinstance(like, np.ndarray):
+        return await load_array_from_tensorstore(spec)
+    else:
+        raise TypeError(f"Can't deserialize {type(like)}")
+
+
+def tree_deserialize_leaves_tensorstore(checkpoint_dir, pytree):
+    leaf_key_paths = jax_utils.leaf_key_paths(pytree)
+    specs = jtu.tree_map(partial(_tensorstore_spec_for, checkpoint_dir), leaf_key_paths)
+
+    async def _do_deserialize():
+        futures = jtu.tree_map(_deserialize_one_leaf, pytree, specs)
+        leaves, structure = jtu.tree_flatten(futures)
+        values = await asyncio.gather(*leaves)
+        return jtu.tree_unflatten(structure, values)
+
+    return asyncio.run(_do_deserialize())
---
+++ tests/test_tensorstore_serialization.py
@@ -0,0 +1,93 @@
+from tempfile import TemporaryDirectory
+
+import equinox as eqx
+import jax
+import jax.numpy as jnp
+import numpy as np
+import optax
+from chex import assert_trees_all_close
+from utils import MLP, arrays_only, assert_trees_not_close
+
+from levanter.tensorstore_serialization import tree_deserialize_leaves_tensorstore, tree_serialize_leaves_tensorstore
+
+
+def test_tensorstore_checkpoint_simple():
+    key0 = jax.random.PRNGKey(0)
+    key1 = jax.random.PRNGKey(1)
+
+    def make_state(key):
+        model = MLP(in_size=2, out_size=1, width_size=2, depth=3, key=key)
+        optim = optax.adam(1e-4)
+        opt_state = optim.init(arrays_only(model))
+
+        return model, opt_state, key
+
+    initial_model, initial_opt_state, initial_key = make_state(key0)
+    rep_model, rep_state, rep_key = make_state(key1)
+
+    assert_trees_not_close(initial_model, rep_model)
+
+    with TemporaryDirectory() as tmpdir:
+        tree_serialize_leaves_tensorstore(tmpdir, (initial_model, initial_opt_state, initial_key))
+        restored_model, restored_optstate, rkey = tree_deserialize_leaves_tensorstore(
+            tmpdir,
+            (rep_model, rep_state, rep_key),
+        )
+
+        assert_trees_all_close(
+            jax.tree_util.tree_leaves(arrays_only(restored_model)),
+            jax.tree_util.tree_leaves(arrays_only(initial_model)),
+        )
+        assert all(np.isclose(rkey, initial_key))
+
+
+def test_checkpoint_steps():
+    key0 = jax.random.PRNGKey(0)
+    key1 = jax.random.PRNGKey(1)
+
+    optim = optax.adam(1e-4)
+
+    def make_state(key):
+        model = MLP(in_size=2, out_size=1, width_size=2, depth=3, key=key)
+        opt_state = optim.init(arrays_only(model))
+
+        return model, opt_state, key
+
+    initial_model, initial_opt_state, initial_key = make_state(key0)
+    data = jax.random.uniform(key0, (2, 2))
+
+    @eqx.filter_grad
+    def loss_fn(model, data):
+        m = jax.vmap(model)
+        return jnp.mean(jnp.square(m(data)))
+
+    model, state = initial_model, initial_opt_state
+    for i in range(3):
+        grad = loss_fn(model, data)
+        updates, state = optim.update(grad, state)
+        model = eqx.apply_updates(model, updates)
+
+    assert_trees_not_close(model, initial_model)
+    assert_trees_not_close(state, initial_opt_state)
+
+    rep_model, rep_state, rep_key = make_state(key1)
+    assert_trees_not_close(model, rep_model)
+    assert_trees_not_close(state, rep_state)
+
+    with TemporaryDirectory() as tmpdir:
+        tree_serialize_leaves_tensorstore(tmpdir, (model, state, initial_key, 3))
+        restored_model, restored_state, rkey, step = tree_deserialize_leaves_tensorstore(
+            tmpdir,
+            (rep_model, rep_state, rep_key, 0),
+        )
+        assert step == 3
+
+        assert_trees_all_close(
+            jax.tree_util.tree_leaves(arrays_only(restored_model)),
+            jax.tree_util.tree_leaves(arrays_only(model)),
+        )
+        assert_trees_all_close(
+            jax.tree_util.tree_leaves(arrays_only(restored_state)),
+            jax.tree_util.tree_leaves(arrays_only(state)),
+        )
+        assert step == 3
