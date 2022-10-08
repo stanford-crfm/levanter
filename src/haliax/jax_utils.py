40d309edd2ecfefd45b71d20fd1ae61fc10f50da
David Hall <dlwh@cs.stanford.edu>
2022-10-08 00:12:09 -0700

    improve named_pjit to make it fully eqx-like, and use it for all uses of pjit in gpt2_example (#31)
    * improve named_pjit to make it fully equinox-like, and use it for all uses of pjit in gpt2_example
    
    * fix test with removal of named_pjit_init
    
    * remove frozendict since we didn't use it
    
    * remove inadvertent log_compiles

--- src/haliax/jax_utils.py
+++ src/haliax/jax_utils.py
@@ -1,9 +1,15 @@
-from typing import Optional, Sequence, Union
+import functools as ft
+from typing import Callable, Optional, Sequence, Union
 
+import equinox as eqx
+import jax
 import numpy as np
 from chex import PRNGKey
+from equinox.compile_utils import Static
 from jax import numpy as jnp
 from jax import random as jrandom
+
+from haliax.util import is_jax_array_like
 
 
 def shaped_rng_split(key, split_shape: Union[int, Sequence[int]] = 2) -> jrandom.KeyArray:
@@ -30,3 +36,20 @@ def maybe_rng_split(key: Optional[PRNGKey], num: int = 2):
         return jnp.reshape(key, (1,) + key.shape)
     else:
         return jrandom.split(key, num)
+
+
+def filter_eval_shape(fun: Callable, *args, **kwargs):
+    """As `jax.eval_shape`, but allows any Python object as inputs and outputs, including
+    GlobalDeviceArrays (which equinox.filter_eval_shape does not support).
+    """
+    # TODO: file a bug
+
+    def _fn(_static, _dynamic):
+        _args, _kwargs = eqx.combine(_static, _dynamic)
+        _out = fun(*_args, **_kwargs)
+        _dynamic_out, _static_out = eqx.partition(_out, is_jax_array_like)
+        return _dynamic_out, Static(_static_out)
+
+    dynamic, static = eqx.partition((args, kwargs), is_jax_array_like)
+    dynamic_out, static_out = jax.eval_shape(ft.partial(_fn, static), dynamic)
+    return eqx.combine(dynamic_out, static_out.value)
--- src/haliax/partitioning.py
+++ src/haliax/partitioning.py
@@ -5,14 +5,18 @@ import typing
 from math import prod
 from typing import List, Mapping, Optional, Sequence, TypeVar, Union
 
+import equinox as eqx
 import jax
 from equinox import is_array
-from jax.experimental.pjit import pjit, with_sharding_constraint
+from equinox.compile_utils import compile_cache, get_fun_names, hashable_combine, hashable_partition
+from jax.experimental.global_device_array import GlobalDeviceArray
+from jax.experimental.pjit import FROM_GDA, pjit, with_sharding_constraint
 from jax.interpreters.pxla import PartitionSpec
 from jaxtyping import PyTree
 
 from .core import Axis, AxisSpec, NamedArray
-from .util import StringHolderEnum, ensure_tuple, is_named_array
+from .jax_utils import filter_eval_shape
+from .util import StringHolderEnum, ensure_tuple, is_jax_array_like, is_named_array
 
 
 LogicalAxisName = str
@@ -123,6 +127,9 @@ def infer_resource_partitions(tree: PyTree, resource_mapping: Optional[ResourceM
             return NamedArray(
                 PartitionSpec(*tuple(_resource_mapping.get(axis.name, None) for axis in node.axes)), node.axes
             )
+        elif isinstance(node, GlobalDeviceArray):
+            return FROM_GDA
+        # TODO: jax.Array
         else:
             return None
 
@@ -141,39 +148,139 @@ def eval_resource_partitions(fn):
     return f
 
 
-def named_pjit_init(cls: typing.Type[T], **pjit_args):
-    """Uses NamedArrays to infer the resource partitions for a module when creating it"""
+# This is more or less copy-pasted from Equinox's similar functions (pmap, vmap, etc), but
+# it's not really explained there so we'll explain it here.
+# Many jax functions work by compiling functions to XLA. The compilation process is expensive,
+# so we want to cache the compiled functions. However, the compiled functions are tied to the
+# "static" arguments to the functions. This is particularly important for a library like Equinox,
+# which Haliax is built on top of, because Equinox uses pytrees extensively for modules, and mixes "static"
+# configuration with "dynamic" data.
+# Thus we need to carefully partition the arguments to the function into "static" and "dynamic" arguments,
+# and cache our compiled functions based on the static arguments.
+# In Equinox conceptually there are three types of "arguments": positional, named, and the function itself.
+# All of these are pytrees, and we need to partition them into static and dynamic arguments.
+# Inside the function, we then combine the arguments into a single pytree, and pass that to the original function.
+# With pjit we also have "donated" arguments, which are arguments that we promise not to use after the function
+# returns. This is useful for conserving memory, but we also have to splice them back in.
+# Also recall that a "pytree" can split into leaves and a "treedef", which can then be reconstructed.
+@compile_cache
+def _named_pjit_cache(fun_names, **jitkwargs):
+    def fun_wrapped(dynamic_donated, dynamic_reserved, static):
+        dynamic = eqx.combine(dynamic_donated, dynamic_reserved)
+        dynamic_fun, dynamic_spec = dynamic
 
-    @functools.wraps(cls.__new__)
-    def init(*args, **kwargs):
-        inst = cls(*args, **kwargs)
-        return inst
+        (
+            static_fun_treedef,
+            static_fun_leaves,
+            static_spec_treedef,
+            static_spec_leaves,
+        ) = static
 
-    return named_pjit(init, **pjit_args)
+        fun = hashable_combine(dynamic_fun, static_fun_leaves, static_fun_treedef)
+        args, kwargs = hashable_combine(dynamic_spec, static_spec_leaves, static_spec_treedef)
+        out = fun(*args, **kwargs)
+        return out
+
+    fun_name, fun_qualname = fun_names
+    fun_wrapped.__name__ = fun_name
+    fun_wrapped.__qualname__ = fun_qualname
+
+    return pjit(fun_wrapped, static_argnums=2, donate_argnums=0, **jitkwargs)
 
 
-def named_pjit(fn=None, **pjit_args):
+def named_pjit(
+    fn=None,
+    axis_resources: Optional[ResourceMapping] = None,
+    *,
+    in_axis_resources: Optional[ResourceMapping] = None,
+    out_axis_resources: Optional[ResourceMapping] = None,
+    donate_args: Optional[PyTree] = None,
+    donate_kwargs: Optional[PyTree] = None,
+    **pjit_args,
+):
     """
-    Uses NamedArrays to infer the resource partitions for calling a function
+    A version of pjit that uses NamedArrays, GlobalDeviceArrays, and the provided resource mapping to infer the
+    resource partitions.
+
+    If no resource mapping is provided, this function attempts to use the global resource mapping.
+    If either of in_axis_resources or out_axis_resources is provided, then both must be provided.
+    If axis_resources is provided, then in_axis_resources and out_axis_resources must not be provided.
+
+    :param fn: The function to be pjit'd
+    :param axis_resources: A mapping from logical axis names to physical axis names
+    :param in_axis_resources: A mapping from logical axis names to physical axis names for the input
+    :param out_axis_resources: A mapping from logical axis names to physical axis names for the output
+    :param donate_args: A PyTree of booleans or function leaf->bool, indicating whether to donate arguments to the
+     computation
+    :param donate_kwargs: A PyTree of booleans or function leaf->bool, indicating whether to donate keyword arguments to
+        the computation
     """
+    # TODO: support jax.Array
+
+    if in_axis_resources is not None or out_axis_resources is not None:
+        if axis_resources is not None:
+            raise ValueError("Cannot provide both axis_resources and in_axis_resources/out_axis_resources")
+        if in_axis_resources is None or out_axis_resources is None:
+            raise ValueError("Must provide both in_axis_resources and out_axis_resources")
 
     if fn is None:
-        return functools.partial(named_pjit, **pjit_args)
+        return functools.partial(
+            named_pjit,
+            axis_resources=axis_resources,
+            in_axis_resources=in_axis_resources,
+            out_axis_resources=out_axis_resources,
+            donate_args=donate_args,
+            donate_kwargs=donate_kwargs,
+            **pjit_args,
+        )
+
+    if axis_resources is None and in_axis_resources is None:
+        axis_resources = _mapping_holder.thread_data.resource_mapping
+
+    if axis_resources is not None:
+        in_axis_resources = axis_resources
+        out_axis_resources = axis_resources
+
+    if in_axis_resources is None or out_axis_resources is None:
+        raise ValueError(
+            "Must provide in_axis_resources and out_axis_resources, or axis_resources, or have a global "
+            "mapping vis axis_mapping"
+        )
+
+    dynamic_fun, static_fun_leaves, static_fun_def = hashable_partition(fn, is_jax_array_like)
 
     @functools.wraps(fn)
     def f(*args, **kwargs):
-        in_resources = infer_resource_partitions((args, kwargs))
-        shapes = jax.eval_shape(fn, *args, **kwargs)
-        out_resources = infer_resource_partitions(shapes)
+        dynamic_argspec, static_argspec, static_arg_def = hashable_partition((args, kwargs), is_jax_array_like)
+        dynamic = (dynamic_fun, dynamic_argspec)
 
-        @functools.wraps(fn)
-        def fn_to_call(args, kwargs):
-            return fn(*args, **kwargs)
+        if donate_args is not None or donate_kwargs is not None:
+            dargs = donate_args or (False,) * len(args)
+            dkwargs = donate_kwargs or {k: False for k in kwargs}
+            dynamic_donated, dynamic_reserved = eqx.partition(dynamic, (False, (dargs, dkwargs)))
+        else:
+            dynamic_donated = jax.tree_util.tree_map(lambda _: None, dynamic)
+            dynamic_reserved = dynamic
 
-        return pjit(fn_to_call, in_resources, out_resources, **pjit_args)(args, kwargs)
+        static = (
+            static_fun_def,
+            static_fun_leaves,
+            static_arg_def,
+            static_argspec,
+        )
+
+        in_resources = infer_resource_partitions((dynamic_donated, dynamic_reserved), in_axis_resources)
+        shapes = filter_eval_shape(fn, *args, **kwargs)
+        out_resources = infer_resource_partitions(shapes, out_axis_resources)
+
+        my_pjit_args = dict(**pjit_args)
+        my_pjit_args["in_axis_resources"] = in_resources
+        my_pjit_args["out_axis_resources"] = out_resources
+        cached_pjitted_fun = _named_pjit_cache(get_fun_names(fn), **my_pjit_args)
+
+        return cached_pjitted_fun(dynamic_donated, dynamic_reserved, static)
 
     return f
-
 
 def physical_axis_name(axis: Axis) -> Optional[PhysicalAxis]:
     """Get the physical axis name for a logical axis"""
@@ -230,8 +337,8 @@ __all__ = [
     "auto_sharded",
     "infer_resource_partitions",
     "eval_resource_partitions",
-    "named_pjit_init",
     "named_pjit",
     "physical_axis_name",
     "pspec_for_axis",
+    "round_axis_for_partitioning",
 ]
