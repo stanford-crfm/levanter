82a89edac26ff7c65b208819c8ed46ff4f1868df
David Hall <dlwh@cs.stanford.edu>
2022-10-10 15:31:42 -0700

    Data loading tests (#33)
    * simple unit tests for TokenizedDocumentCache
    
    * delete old main test thing for document indexer
    
    * add an extra test for model_axis==1 for sharded data loading
    
    * simulate mulitprocessing just to be sure that's reasonable

--- tests/test_sharded_data_loading.py
+++ tests/test_sharded_data_loading.py
@@ -2,6 +2,7 @@ import itertools
 from typing import List
 
 import jax
+import jax.numpy as jnp
 import numpy as np
 from jax.experimental.global_device_array import GlobalDeviceArray, Shard
 from jax.experimental.maps import Mesh
@@ -42,7 +43,7 @@ def test_sharded_data_loading_model_axis_2():
     )
     with mesh:
 
-        mesh_info = MeshInfo(mesh, batch_size=4, per_device_parallelism=1)
+        mesh_info = MeshInfo(mesh, batch_size=len(devices) // 2, per_device_parallelism=1)
         seq_len = 128
         cache = _small_dataset(seq_len)
         dataset = ShardedIndexedDataset(cache, mesh_info, seq_len)
@@ -50,20 +51,68 @@ def test_sharded_data_loading_model_axis_2():
         batches: List[GlobalDeviceArray] = list(itertools.islice(dataset, 10))
         for batch in batches:
             assert batch.shape == dataset.batch_shape
-            # localized = pjit(
-            #     lambda x: x,
-            #     in_axis_resources=batch.mesh_axes,
-            #     out_axis_resources=PartitionSpec(None, None, None),
-            # )(batch)
             shard_i: Shard
-            for i, shard_i in enumerate(batch.global_shards):
-                data_axis_pos_i = shard_i.device.id // mesh_info.model_axis_size
-                for j, shard_j in enumerate(batch.global_shards):
-                    data_axis_pos_j = shard_j.device.id // mesh_info.model_axis_size
-                    if shard_i.data is not None and shard_j.data is not None:
-                        if data_axis_pos_i == data_axis_pos_j:
-                            assert np.all(shard_i.data == shard_j.data)
-                        else:
-                            assert not np.all(shard_i.data == shard_j.data)
+            check_batch_shard_consistency(batch, mesh_info)
 
-            # print(localized.device_buffers)
+
+def check_batch_shard_consistency(batch, mesh_info):
+    for i, shard_i in enumerate(batch.global_shards):
+        data_axis_pos_i = shard_i.device.id // mesh_info.model_axis_size
+        for j, shard_j in enumerate(batch.global_shards):
+            data_axis_pos_j = shard_j.device.id // mesh_info.model_axis_size
+            if shard_i.data is not None and shard_j.data is not None:
+                if data_axis_pos_i == data_axis_pos_j:
+                    assert np.all(shard_i.data == shard_j.data)
+                else:
+                    assert not np.all(shard_i.data == shard_j.data)
+
+
+def test_sharded_data_loading_model_axis_1():
+    devices = jax.devices()
+    model_axis_size = 1
+
+    mesh = Mesh(
+        np.array(devices).reshape(-1, model_axis_size),
+        (ResourceAxis.DATA, ResourceAxis.MODEL),
+    )
+    with mesh:
+
+        mesh_info = MeshInfo(mesh, batch_size=len(devices), per_device_parallelism=1)
+        seq_len = 128
+        cache = _small_dataset(seq_len)
+        dataset = ShardedIndexedDataset(cache, mesh_info, seq_len)
+
+        batches: List[GlobalDeviceArray] = list(itertools.islice(dataset, 10))
+        for batch in batches:
+            assert batch.shape == dataset.batch_shape
+            shard_i: Shard
+            check_batch_shard_consistency(batch, mesh_info)
+
+
+def test_sharded_data_loading_model_axis_1_override_process_indices():
+    devices = jax.devices()
+    model_axis_size = 1
+
+    mesh = Mesh(
+        np.array(devices).reshape(-1, model_axis_size),
+        (ResourceAxis.DATA, ResourceAxis.MODEL),
+    )
+    with mesh:
+        datasets = []
+        mesh_info = MeshInfo(mesh, batch_size=len(devices), per_device_parallelism=1)
+        for process_index in range(2):
+            seq_len = 128
+            cache = _small_dataset(seq_len)
+            dataset = ShardedIndexedDataset(
+                cache, mesh_info, seq_len, override_process_data_pos=process_index, override_process_data_groups=2
+            )
+            datasets.append(dataset)
+
+        batches: List[List[GlobalDeviceArray]] = [list(itertools.islice(dataset, 10)) for dataset in datasets]
+        b1: GlobalDeviceArray
+        for (b1, b2) in zip(*batches):
+            assert b1.shape == b2.shape
+            assert jnp.all(b1._value != b2._value)
+            shard_i: Shard
+            check_batch_shard_consistency(b1, mesh_info)
+            check_batch_shard_consistency(b2, mesh_info)
