d87762e625f32f545f863d4be8eff26ce4ce7728
David Hall <dlwh@stanford.edu>
2022-11-18 10:59:11 -0800

    clean up docs for ShardedIndexedDataset and rename to GlobalBatchDataset

--- src/levanter/data/sharded.py
+++ src/levanter/data/sharded.py
@@ -1,5 +1,4 @@
 import itertools
-from math import prod
 from typing import Iterator, Optional, Sequence, Tuple, TypeVar
 
 import jax
@@ -22,30 +21,32 @@ Ex = TypeVar("Ex")
 # TODO: write tests to verify this works when data spans multiple processes
 # ExampleShape = Union[Tuple[int, ...], Sequence[Tuple[int, ...]]]
 
-# This is hard for me to think about.
-
-# We use GlobalDeviceArrays to coordinate data loading. A GlobalDeviceArray is an array that has a global shape
-# but only has the data for some of the chunks of the array (namely, the ones on the local devices). Data can be
-# replicated across devices, so that more than one device has the same chunk of data. This is useful for
-# data or model parallelism.
-# GlobalDeviceArrays are constructed using from_batched_callback, which passes in a list of slices that correspond to
-# the entries in the GDA.
-#
-# For our purposes (data loading), we want to load batches of data from a dataset. We want to load the data in a
-# way that is compatible with the GDA, so that we can use the GDA to coordinate the data loading.
-# For standard language modeling tasks, the data is a sequence of tokens of some length, so the GDA has shape
-# (batch_size, num_tokens). We want to load batches of data that are compatible with this shape.
-# The device mesh is (data, model), and we want to replicate data across the model axis. We partition the above array as
-# (data, None), meaning that the batch axis is distributed across the data axis. Each process is responsible for
-# loading data for its devices. GDA's from_batched_callback will tell us how much data to load and for which device, so
-# we just have to load it. We do need to make sure that, in the event the data axis is larger than
-# num_devices_per_process, each process that is part of the same position in the device mesh loads the same data.
+_TensorSliceIndex = Tuple[slice, ...]
 
 
-class ShardedIndexedDataset(Dataset[GlobalDeviceArray]):
+class GlobalBatchDataset(Dataset[GlobalDeviceArray]):
+    """
+    GlobalBatchDataset wraps a "local dataset" (a dataset that is shardable and can be iterated over) to produce
+    GlobalDeviceArrays representing batches of data. A GlobalDeviceArray is an array that has a global shape
+    but only has the data for some of the chunks of the array (namely, the ones on the local devices).
+    Thus, each process loads the data for its devices.
+
+    The details are a bit complex: We have a device mesh of shape (data, model). We want each row of the device mesh to
+    get batch_size//num_rows examples. Usually, a process will be responsible for one or more entire rows, meaning
+    that it wil load data that is distinct from every other process. However, if num_cols > num_devices_per_process,
+    then some processes will need to load the same data. We use the process_mesh_position to determine which data to
+    load, by determining which row(s) of the device mesh the process is responsible for.
+
+    For now GlobalBatchDataset is restricted to datasets that return a single sequence of tokens.
+
+    :arg local_dataset: a dataset that is shardable and can be iterated over
+    :arg mesh: the device mesh
+    :arg batch_size: the batch size
+    """
+
     def __init__(
         self,
-        token_dataset: ShardableDataset[BatchEncoding],
+        local_dataset: ShardableDataset[BatchEncoding],
         mesh: Mesh,
         batch_size: int,
         *,
@@ -61,57 +62,70 @@ class ShardedIndexedDataset(Dataset[GlobalDeviceArray]):
         if not override_process_data_groups:
             assert num_data_process_groups <= jax.process_count()
 
-        self.token_dataset = token_dataset.shard(process_data_pos, num_data_process_groups)
+        self.local_dataset = local_dataset.shard(process_data_pos, num_data_process_groups)
 
     def __iter__(self) -> Iterator[GlobalDeviceArray]:
         # TODO: support not infinite iterators
         def loop_gen():
             while True:
-                for ex in self.token_dataset:
+                for ex in self.local_dataset:
                     yield ex
 
         it = loop_gen()
 
-        batch_shape = self.batch_shape
+        item_shape = self.item_shape
         pspec = self.partition_spec
 
-        assert len(batch_shape) == len(pspec)
+        assert len(item_shape) == len(pspec)
 
-        def callback(indices: Sequence[Tuple[slice, ...]]):
-            # TODO: it seems like we may want to just directly index into the tokenized dataset somehow. This seems a
-            #  bit more fragile
-            # there is one entry in indices per device. They may be identical.
-            # convert slices to tuples so we can use hashes
+        # This callback takes a sequence of slices indicating a grid of the data to load, and returns the data
+        # We're mostly going to ignore the slices, because we're streaming the data
+        # We get one slice per device: the slices will be identical if the data is replicated
+        # TODO: we may want to just directly index into the tokenized dataset somehow. This seems a bit more fragile
+
+        def callback(indices: Sequence[_TensorSliceIndex]):
             out = []
-            data_for_group = {}
-            for index_group in indices:
-                # begin, end, step
-                my_indices: Tuple[Tuple[int, int, int], ...] = tuple(
-                    s.indices(axis_size) for axis_size, s in zip(batch_shape, index_group)
-                )
-                assert all(s[2] == 1 for s in my_indices)  # ensure step is 1
-                slice_sizes = [s[1] - s[0] for s in my_indices]
-                num_examples = prod(slice_sizes[0:-1])
-                if my_indices not in data_for_group:
-                    data_for_group[my_indices] = np.stack(
+
+            # because more than one device can get the same data, we need to make sure we only load it once since we're
+            # streaming. This is the cache
+            data_for_slice = {}
+
+            for tslice_index in indices:
+                begin_ends_for_index = self._get_begin_end_for_slice(item_shape, tslice_index)
+                slice_sizes = [s[1] - s[0] for s in begin_ends_for_index]
+
+                num_examples = slice_sizes[0]
+
+                if begin_ends_for_index not in data_for_slice:
+                    data_for_slice[begin_ends_for_index] = np.stack(
                         list([ex["input_ids"] for ex in itertools.islice(it, num_examples)])
-                    ).reshape(*slice_sizes)
-                out.append(data_for_group[my_indices])
+                    )
+                out.append(data_for_slice[begin_ends_for_index])
 
             return out
 
         while True:
             yield GlobalDeviceArray.from_batched_callback(
-                batch_shape,
+                item_shape,
                 self.mesh,
                 pspec,
                 callback,
             )
+
+    @staticmethod
+    def _get_begin_end_for_slice(tensor_shape, tslice_index) -> Tuple[Tuple[int, int], ...]:
+        # begin, end, step
+        my_indices: Tuple[Tuple[int, int, int], ...] = tuple(
+            s.indices(axis_size) for axis_size, s in zip(tensor_shape, tslice_index)
+        )
+        assert all(s[2] == 1 for s in my_indices)  # ensure step is 1
+        return tuple(s[0:2] for s in my_indices)
 
     @property
     def partition_spec(self):
         return PartitionSpec(ResourceAxis.DATA, None)
 
     @property
-    def batch_shape(self):
-        return (self.batch_size, self.token_dataset.seq_len)
+    def item_shape(self):
+        # TODO: make datasets expose data shapes as pytrees
+        return (self.batch_size, self.local_dataset.seq_len)
