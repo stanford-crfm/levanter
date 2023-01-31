ae1bb54d1828b2a49b799d2eac03403e9e832454
David Hall <dlwh@stanford.edu>
2023-01-31 13:17:55 -0800

    wip

--- projects/instruction_tuning/cache_itune_dset.py
+++ projects/instruction_tuning/cache_itune_dset.py
@@ -4,15 +4,16 @@ import datasets
 try:
     dsQ = datasets.load_from_disk("instruction_tuning")
 except:  # noqa: E722
-    ds1 = datasets.load_dataset("Muennighoff/P3", split="train", streaming=True)
-    ds2 = datasets.load_dataset("Muennighoff/natural-instructions", split="train", streaming=True)
+    ds1 = datasets.load_dataset("Muennighoff/P3", split="train")
+    ds1.save_to_disk("instruction_tuning_p3")
+    ds2 = datasets.load_dataset("Muennighoff/natural-instructions", split="train")
     print("map")
-    ds2 = ds2.map(lambda x: {"inputs": "\n".join([x["definition"], x["input"]]), "targets": x["targets"]})
+    ds2 = ds2.map(lambda x: {"inputs": "\n".join([x["definition"], x["inputs"]]), "targets": x["targets"]})
     print("interleave")
 
     dsQ = datasets.interleave_datasets([ds1, ds2])
     print("shuffle")
-    dsQ = dsQ.shuffle(seed=42, buffer_size=10000)
+    dsQ = dsQ.shuffle(seed=42)
     print("save")
 
     dsQ.save_to_disk("instruction_tuning")
