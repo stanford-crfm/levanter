346d9fe6b9c9ae5ccefc29510ce9cf018094d0e3
David Hall <dlwh@stanford.edu>
2023-02-01 13:32:23 -0800

    wip

--- projects/instruction_tuning/cache_itune_dset.py
+++ projects/instruction_tuning/cache_itune_dset.py
@@ -1,23 +1,17 @@
+import json
+
 import datasets
+import tqdm
+import zstandard as zstd
 
 
-try:
-    dsQ = datasets.load_from_disk("instruction_tuning")
-except:  # noqa: E722
-    ds1 = datasets.load_dataset("Muennighoff/P3", split="train")
-    ds1.save_to_disk("instruction_tuning_p3")
-    ds2 = datasets.load_dataset("Muennighoff/natural-instructions", split="train")
-    print("map")
-    ds2 = ds2.map(lambda x: {"inputs": "\n".join([x["definition"], x["inputs"]]), "targets": x["targets"]})
-    print("interleave")
+ds1 = datasets.load_dataset("Muennighoff/P3", split="train", streaming=True)
+ds2 = datasets.load_dataset("Muennighoff/natural-instructions", split="train", streaming=True)
+ds2 = ds2.map(lambda x: {"inputs": "\n".join([x["definition"], x["inputs"]]), "targets": x["targets"]})
+ds = datasets.concatenate_datasets([ds1, ds2])
 
-    dsQ = datasets.interleave_datasets([ds1, ds2])
-    print("shuffle")
-    dsQ = dsQ.shuffle(seed=42)
-    print("save")
+compressor = zstd.ZstdCompressor(level=9)
 
-    dsQ.save_to_disk("instruction_tuning")
-    print("done")
-
-
-# TODO: export
+with zstd.open("itune-train.jsonl.zstd", "w", cctx=compressor) as out:
+    for ex in tqdm.tqdm(ds):
+        out.write(json.dumps(ex) + "\n")
