2a67686c8748f6a5add231fc48ace7cdc2e63816
David Hall <dlwh@stanford.edu>
2023-02-13 05:08:10 +0000

    fixes

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -6,4 +6,4 @@ LEV_ROOT=$(dirname "$(readlink -f $0)")/..
 
 source /files/venv32/bin/activate
 
-PYTHONPATH=${LEV_ROOT}:${LEV_ROOT}/src:${LEV_ROOT}/examples:$PYTHONPATH nohup "$@" > >(tee -a stdout-`hostname`.log) 2> >(tee -a stderr-`hostname`.log >&2)
+PYTHONPATH=${LEV_ROOT}:${LEV_ROOT}/src:${LEV_ROOT}/examples:$PYTHONPATH nohup "$@" &> log-`hostname`.log
