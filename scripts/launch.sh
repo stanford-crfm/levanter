b37e5447377201e1b7f530e71c123409155d6d9d
David Hall <dlwh@stanford.edu>
2023-02-15 17:00:18 -0800

    wip refactoring the setup scripts to not need NFS

--- scripts/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script is used for launching on TPU pods (or other direct run environments) via remote ssh with a virtual env
-set -e
-umask 000
-LEV_ROOT=$(dirname "$(readlink -f $0)")/..
-
-source /files/venv32/bin/activate
-
-PYTHONPATH=${LEV_ROOT}:${LEV_ROOT}/src:${LEV_ROOT}/examples:$PYTHONPATH nohup "$@" &> log-`hostname`.log
