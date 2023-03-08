207e8e3a1df05a4222ec1d53741d84480c40d9c0
David Hall <dlwh@cs.stanford.edu>
2023-03-08 14:12:53 -0800

    "Babysitting" script for running jobs with preemptible VMs (#86)

--- infra/setup-tpu-vm-nfs.sh
+++ infra/setup-tpu-vm-nfs.sh
@@ -14,14 +14,23 @@ retCode=$?
 [[ $retCode -le 1 ]] || exit $retCode
 
 # install python 3.10, latest git, and nfs
-sudo systemctl stop unattended-upgrades  # this frequently holds the apt lock
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo add-apt-repository -y ppa:git-core/ppa
-sudo apt-get update
-sudo apt-get install -y python3.10-full python3.10-dev nfs-common git golang
-sudo systemctl start unattended-upgrades
+#sudo apt-get install -y software-properties-common
+#sudo add-apt-repository -y ppa:deadsnakes/ppa
+#sudo add-apt-repository -y ppa:git-core/ppa
+#sudo apt-get update
+#sudo apt-get install -y python3.10-full python3.10-dev nfs-common git golang
 
+sudo systemctl stop unattended-upgrades  # this frequently holds the apt lock
+# sometimes apt-get update fails, so retry a few times
+for i in {1..5}; do
+  sudo apt-get install -y software-properties-common \
+  && sudo add-apt-repository -y ppa:deadsnakes/ppa \
+  && sudo add-apt-repository -y ppa:git-core/ppa \
+  && sudo apt-get update \
+  && sudo apt-get install -y python3.10-full python3.10-dev nfs-common git \
+  && break
+done
+sudo systemctl start unattended-upgrades
 
 # set up nfs
 NFS_SERVER=10.5.220.250
