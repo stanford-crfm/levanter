c14851995b942054f85d0c9eb925c67028b27591
John Thickstun <jthickstun@scdt.stanford.edu>
2023-03-09 09:44:56 -0800

    longer timeout

--- src/levanter/jax_utils.py
+++ src/levanter/jax_utils.py
@@ -159,7 +159,7 @@ def multihost_broadcast_sync(obj: X, is_source: Optional[bool] = None) -> X:
         pickled = pickle.dumps(obj, 0)  # 0 is pickle protocol. jax only accepts utf-8, and 0 gives us ascii
         client.key_value_set(key, pickled.decode("ascii"))
 
-    client.wait_at_barrier(f"multihost_broadcast_sync{_sync_counter}", timeout_in_ms=20_000)
+    client.wait_at_barrier(f"multihost_broadcast_sync{_sync_counter}", timeout_in_ms=200_000)
 
     if not is_source:
         pickled = bytes(client.blocking_key_value_get(key, timeout_in_ms=20_000), "ascii")
