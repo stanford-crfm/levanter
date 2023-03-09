8f3125b30136bcb1c3d27cdfd978391a907540bc
David Hall <dlwh@stanford.edu>
2023-03-08 22:35:29 -0800

    factor out a "retry" function

--- infra/setup-tpu-vm.sh
+++ infra/setup-tpu-vm.sh
@@ -1,6 +1,22 @@
 set -x
 # broadly based on https://github.com/ayaka14732/tpu-starter
 
+# we frequently deal with commands failing, and we like to loop until they succeed. this function does that for us
+function retry {
+  for i in {1..5}; do
+    $@
+    if [ $? -eq 0 ]; then
+      break
+    fi
+    if 5 == $i; then
+      >&2 echo "Error running $*, giving up"
+      exit 1
+    fi
+    >&2 echo "Error running $*, retrying in 5 seconds"
+    sleep 5
+  done
+}
+
 # tcmalloc interferes with intellij remote ide
 sudo patch -f -b /etc/environment << EOF
 2c2
@@ -15,15 +31,13 @@ retCode=$?
 
 # install python 3.10, latest git, and nfs
 sudo systemctl stop unattended-upgrades  # this frequently holds the apt lock
+sudo systemctl disable unattended-upgrades
 # sometimes apt-get update fails, so retry a few times
-for i in {1..5}; do
-  sudo apt-get install -y software-properties-common \
-  && sudo add-apt-repository -y ppa:deadsnakes/ppa \
-  && sudo add-apt-repository -y ppa:git-core/ppa \
-  && sudo apt-get update \
-  && sudo apt-get install -y python3.10-full python3.10-dev nfs-common git \
-  && break
-done
+retry sudo apt-get install -y software-properties-common
+retry sudo add-apt-repository -y ppa:deadsnakes/ppa
+retry sudo add-apt-repository -y ppa:git-core/ppa
+retry sudo apt-get -qq update
+retry sudo apt-get -qq install -y python3.10-full python3.10-dev nfs-common git
 sudo systemctl start unattended-upgrades
 
 VENV=~/venv310
@@ -32,26 +46,18 @@ if [ ! -d "$VENV" ]; then
     echo "Creating virtualenv at $VENV"
     python3.10 -m venv $VENV
 fi
+
+# don't spam the console with venv activation
+set +x
 source $VENV/bin/activate
+set -x
 
 pip install -U pip
 pip install -U wheel
 
 # jax and jaxlib
 # libtpu sometimes has issues installing for clinical (probably firewall?)
-# TODO: get these deps from somewhere reasonable
-for i in {1..5}; do
-    pip install -U "jax[tpu]==0.4.5" libtpu-nightly==0.1.dev20230216 -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
-    if [ $? -eq 0 ]; then
-        break
-    fi
-    if 5 == $i; then
-        echo "Error installing jax, giving up"
-        exit 1
-    fi
-    echo "Error installing jax, retrying in 5 seconds"
-    sleep 5
-done
+retry pip install -U "jax[tpu]==0.4.5" libtpu-nightly==0.1.dev20230216 -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
 # clone levanter
 git clone https://github.com/stanford-crfm/levanter.git
