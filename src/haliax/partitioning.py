4c3636dcee46d1fcc762beaecb1057932fa02d95
David Hall <dlwh@cs.stanford.edu>
2023-04-10 22:57:30 -0700

    remove most references to gda (#110)

--- src/haliax/partitioning.py
+++ src/haliax/partitioning.py
@@ -9,8 +9,7 @@ import equinox as eqx
 import jax
 from equinox import is_array
 from equinox.compile_utils import compile_cache, get_fun_names, hashable_combine, hashable_partition
-from jax.experimental.global_device_array import GlobalDeviceArray
-from jax.experimental.pjit import FROM_GDA, pjit, with_sharding_constraint
+from jax.experimental.pjit import pjit, with_sharding_constraint
 from jax.interpreters.pxla import PartitionSpec
 from jaxtyping import PyTree
 
@@ -131,8 +130,8 @@ def infer_resource_partitions(tree: PyTree, resource_mapping: Optional[ResourceM
                 PartitionSpec(*tuple(_resource_mapping.get(axis.name, None) for axis in node.axes)),  # type: ignore
                 node.axes,
             )
-        elif isinstance(node, GlobalDeviceArray):
-            return FROM_GDA
+        # elif isinstance(node, GlobalDeviceArray):
+        #     return FROM_GDA
         elif hasattr(node, "sharding"):
             return node.sharding
         else:
