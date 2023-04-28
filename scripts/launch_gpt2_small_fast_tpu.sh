a4fc0fb96bf30cd5d2e85a765f4ef9de4bad4f8a
David Hall <dlwh@stanford.edu>
2023-04-28 16:42:27 -0700

    go to 4 hours?

--- scripts/launch_gpt2_small_fast_tpu.sh
+++ scripts/launch_gpt2_small_fast_tpu.sh
@@ -3,4 +3,4 @@
 bash infra/babysit-tpu-vm.sh levanter-itest-32 -z us-east1-d --preemptible -t v3-32 -- \
     WANDB_API_KEY=$WANDB_API_KEY levanter/infra/run.sh python levanter/examples/gpt2_example.py \
     --config_path levanter/config/gpt2_small_fast.yaml \
-    --trainer.checkpointer.base_path gs://levanter-checkpoints/gpt-itest/ --trainer.checkpointer.save_interval 30m
+    --trainer.checkpointer.base_path gs://levanter-checkpoints/gpt-itest/ --trainer.checkpointer.save_interval 30m $*
