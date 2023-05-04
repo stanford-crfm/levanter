04b19a660b7fd02dd25c3716bdfaf67dcbbe6fd4
David Hall <dlwh@cs.stanford.edu>
2023-05-04 14:59:40 -0700

    Update setup script with some preemptive error checking (#134)
    * require ssh agent to use the setup scripts
    
    * require WANDB_API_KEY for small-fast

--- scripts/launch_gpt2_small_fast_tpu.sh
+++ scripts/launch_gpt2_small_fast_tpu.sh
@@ -1,5 +1,10 @@
 # Launches the "gpt_small_fast" model on a TPU node
 
+if [ -z "$WANDB_API_KEY" ]; then
+  echo "Error: WANDB_API_KEY not set"
+  exit 1
+fi
+
 bash infra/babysit-tpu-vm.sh levanter-itest-32 -z us-east1-d --preemptible -t v3-32 -- \
     WANDB_API_KEY=$WANDB_API_KEY levanter/infra/run.sh python levanter/examples/gpt2_example.py \
     --config_path levanter/config/gpt2_small_fast.yaml \
