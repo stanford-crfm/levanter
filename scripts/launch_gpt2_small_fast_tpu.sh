f0f729c2614aee1db466e25621995f5e340d29ae
David Hall <dlwh@stanford.edu>
2023-05-11 16:18:15 +0000

    fix val path for openwebtext in small_fast

--- scripts/launch_gpt2_small_fast_tpu.sh
+++ scripts/launch_gpt2_small_fast_tpu.sh
@@ -5,7 +5,7 @@ if [ -z "$WANDB_API_KEY" ]; then
   exit 1
 fi
 
-bash infra/babysit-tpu-vm.sh levanter-itest-32 -z us-east1-d --preemptible -t v3-32 -- \
+bash infra/babysit-tpu-vm.sh levanter-itest-32 -z us-east1-d -t v3-32 -b ray_writers -- \
     WANDB_API_KEY=$WANDB_API_KEY levanter/infra/run.sh python levanter/examples/gpt2_example.py \
     --config_path levanter/config/gpt2_small_fast.yaml \
     --trainer.checkpointer.base_path gs://levanter-checkpoints/gpt-itest/ --trainer.checkpointer.save_interval 30m $*
