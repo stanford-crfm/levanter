48f45ddb01329ae5ac2df21eb3128b8ef8313d38
David Hall <dlwh@cs.stanford.edu>
2023-05-17 19:46:07 -0700

    Logging for Ray preprocessing (#127)

--- scripts/cache_dataset.py
+++ scripts/cache_dataset.py
@@ -2,9 +2,11 @@ import os
 
 from dataclasses import dataclass
 
+import wandb
+
 import levanter
 from levanter.config import RayConfig
-from levanter.data.shard_cache import cache_dataset
+from levanter.data.shard_cache import RichMetricsMonitor, WandbMetricsMonitor, cache_dataset
 from levanter.data.text import BatchTokenizer, LMDatasetConfig
 from levanter.logging import init_logger
 
@@ -22,13 +24,21 @@ def main(args: RayCachedLMDatasetConfig):
 
     tokenizer = args.the_tokenizer
 
+    wandb.init(mode="offline")
+
     for split in args.splits:
         print(f"Caching {split} to {args.cache_dir}.")
+
         # connect or start the actor
         batch_tokenizer = BatchTokenizer(tokenizer)
         split_cache_dir = os.path.join(args.cache_dir, split)
         source = args.get_shard_source(split)
-        cache_dataset(split_cache_dir, source, batch_tokenizer)
+        cache = cache_dataset(split_cache_dir, source, batch_tokenizer, await_finished=False)
+
+        cache.attach_metrics_monitor(RichMetricsMonitor(source.num_shards))
+        cache.attach_metrics_monitor(WandbMetricsMonitor("preprocess/" + split, commit=True))
+
+        cache.await_finished()
         print(f"Finished caching {split} to {split_cache_dir}.")
 
 
