87772db2832ca92027284cb97ab428dae5456fd0
David Hall <dlwh@stanford.edu>
2023-05-25 20:35:56 -0700

    support more complex grad_accum calls

--- src/levanter/grad_accum.py
+++ src/levanter/grad_accum.py
@@ -50,13 +50,13 @@ def accumulate_gradients_sharded(
     batched_args: Optional[Union[bool, Tuple[bool, ...]]] = None,
     batched_kwargs: Optional[Union[bool, Dict[str, bool]]] = None,
 ) -> GradAndValFn[M, X]:
-
     """
     Accumulate gradients across a sharded batch, keeping a local copy of the gradient on each row of the data
      parallel axis. (If the model is not sharded, then a copy of the gradient is on each individual device.)
 
      Parameters:
         f: a function that takes a model and a batch of inputs and returns a tuple of (loss, gradient)
+        Batch: the axis along which the batch is sharded
         per_device_parallelism: how many examples to process at once on each device
         parameter_axis_mapping: the axis mapping for the model parameters
         batched_args: a tuple of booleans indicating whether each unnamed array argument is batched. Defaults to True for each argument.
@@ -85,32 +85,13 @@ def accumulate_gradients_sharded(
     # TODO: update docstring?
     @functools.wraps(f)
     def accum_f(model: M, *args: X, **kwargs) -> Tuple[float, M]:
+        should_batch_spec = _instantiate_arg_specs_for_args(batched_args, batched_kwargs, args, kwargs)
+
         # first things first, we want a copy of our gradient sharded like our model, along with a loss value
         loss = jnp.zeros(())
         with jax.named_scope("zeros"):
             grad = jax.tree_map(jnp.zeros_like, model)
             grad = shard_with_axis_mapping(grad, parameter_axis_mapping)
-
-        if batched_args is None:
-            batched_args_spec = (True,) * len(args)
-        elif isinstance(batched_args, bool):
-            batched_args_spec = (batched_args,) * len(args)
-        elif not isinstance(batched_args, tuple):
-            batched_args_spec = tuple(batched_args)
-        else:
-            batched_args_spec = batched_args
-
-        if len(batched_args_spec) < len(args):
-            batched_args_spec = batched_args_spec + (True,) * (len(args) - len(batched_args_spec))
-        elif len(batched_args_spec) > len(args):
-            raise ValueError(f"batched_args_spec must be a tuple of booleans of length {len(args)} (or less)")
-
-        if isinstance(batched_kwargs, bool):
-            batched_kwarg_spec = {k: batched_kwargs for k in kwargs}
-        elif batched_kwargs is None:
-            batched_kwarg_spec = batched_kwargs or {k: True for k in kwargs}
-        else:
-            batched_kwarg_spec = {k: batched_kwargs.get(k, True) for k in kwargs}
 
         # second, we want to reshape our data to (num_micro_steps, micro_batch_size, ...), sharded along the data axis
         (args, kwargs) = _reshape_for_microbatch(
@@ -118,13 +99,12 @@ def accumulate_gradients_sharded(
             Microbatch,
             AccumStep,
             (args, kwargs),
-            (batched_args_spec, batched_kwarg_spec),
+            should_batch_spec,
             compute_axis_mapping,
         )
 
         # third, we want to do compute.
-        def loop(acc, microbatch):
-            mbatch_args, mbatch_kwargs = microbatch
+        def loop(acc, *mbatch_args, **mbatch_kwargs):
             loss, grad = acc
             with jax.named_scope("grad"):
                 this_loss, this_grad = f(model, *mbatch_args, **mbatch_kwargs)
@@ -137,10 +117,7 @@ def accumulate_gradients_sharded(
 
             return loss, grad
 
-        def is_scanned_if_not_none(x):
-            return x is not None
-
-        loss, grad = hax.fold(loop, AccumStep, is_scanned=(batched_args_spec, batched_kwarg_spec))((loss, grad), *args, **kwargs)
+        loss, grad = hax.fold(loop, AccumStep, is_scanned=should_batch_spec)((loss, grad), *args, **kwargs)
 
         return loss / num_micro_steps, jax.tree_map(lambda x: x / num_micro_steps, grad)
 
@@ -169,3 +146,30 @@ def _reshape_for_microbatch(
             return jax.tree_map(functools.partial(_reshape, True), x, is_leaf=is_named_array)
 
     return jax.tree_map(_reshape, should_batch_spec, inputs, is_leaf=is_named_array)
+
+
+# TODO: would be better to use inspect
+def _instantiate_arg_specs_for_args(batched_args, batched_kwargs, args, kwargs) -> Tuple[Tuple[bool], Dict[str, bool]]:
+    """Expands the provided batched_args and batched_kwargs into bool tuples with the same shape as arg and /kwargs."""
+    if batched_args is None:
+        batched_args_spec = (True,) * len(args)
+    elif isinstance(batched_args, bool):
+        batched_args_spec = (batched_args,) * len(args)
+    elif not isinstance(batched_args, tuple):
+        batched_args_spec = tuple(batched_args)
+    else:
+        batched_args_spec = batched_args
+
+    if len(batched_args_spec) < len(args):
+        batched_args_spec = batched_args_spec + (True,) * (len(args) - len(batched_args_spec))
+    elif len(batched_args_spec) > len(args):
+        raise ValueError(f"batched_args_spec must be a tuple of booleans of length {len(args)} (or less)")
+
+    if isinstance(batched_kwargs, bool):
+        batched_kwarg_spec = {k: batched_kwargs for k in kwargs}
+    elif batched_kwargs is None:
+        batched_kwarg_spec = batched_kwargs or {k: True for k in kwargs}
+    else:
+        batched_kwarg_spec = {k: batched_kwargs.get(k, True) for k in kwargs}
+
+    return batched_args_spec, batched_kwarg_spec  # type: ignore
