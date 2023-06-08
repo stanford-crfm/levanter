ac4a968af9e64600faba9882ab96cfa4ffc1d25e
David Hall <dlwh@stanford.edu>
2023-06-08 13:12:22 -0700

    vocab size patching

--- examples/convert_backpack_to_hf.py
+++ examples/convert_backpack_to_hf.py
@@ -4,13 +4,13 @@ from functools import cached_property
 from typing import Optional
 
 import jax
-import jax.numpy as jnp
+from jax.random import PRNGKey
 from transformers import GPT2Tokenizer
 
 import haliax as hax
+import haliax.tree_util as htu
 import levanter
-from haliax import Axis, NamedArray
-from haliax.util import is_named_array
+from haliax import Axis
 from levanter.compat.hf_checkpoints import HFCheckpointConverter, RepoRef
 from levanter.models.backpack import BackpackConfig, BackpackLMHeadModel
 from levanter.tensorstore_serialization import tree_deserialize_leaves_tensorstore
@@ -66,13 +66,7 @@ def main(config: ConvertConfig):
         with hax.enable_shape_checks(False):
             model = tree_deserialize_leaves_tensorstore(f"{config.checkpoint_path}/model", model)
 
-        def patch_vocab(array):
-            if is_named_array(array):
-                return patch_vocab_size(array.array, array)
-            else:
-                return array
-
-        model = jax.tree_util.tree_map(patch_vocab, model, is_leaf=is_named_array)
+        model = htu.resize_axis(model, Vocab.resize(vocab_size), key=PRNGKey(0))
 
         converter.save_model(
             model,
@@ -81,19 +75,6 @@ def main(config: ConvertConfig):
             upload_to_hf=config.hf_checkpoint or False,
             commit_message="convert to hf checkpoint",
         )
-
-
-def patch_vocab_size(inner: jnp.ndarray, like: NamedArray):
-    # for partitioning reasons we frequently round the vocab size, but we need to patch it back
-    # to the original size for the HF checkpoint to work
-    if any(ax.name == "vocab" for ax in like.axes):
-        index_of_vocab = next(i for i, ax in enumerate(like.axes) if ax.name == "vocab")
-        desired_vocab_size = like.axes[index_of_vocab].size
-        vocab_size = inner.shape[index_of_vocab]
-        if vocab_size != desired_vocab_size:
-            logger.info(f"Patching vocab size from {vocab_size} back to {desired_vocab_size} for HF checkpoint")
-            inner = jnp.take(inner, jnp.arange(desired_vocab_size), axis=index_of_vocab)
-    return inner
 
 
 if __name__ == "__main__":
