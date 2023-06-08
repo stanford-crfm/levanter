39c50500011adbdb0460a37e3a7cc85b73fd21b2
David Hall <dlwh@stanford.edu>
2023-06-07 17:18:32 -0700

    delete old gpt serialization methods

--- src/levanter/compat/hf_checkpoints.py
+++ src/levanter/compat/hf_checkpoints.py
@@ -24,7 +24,6 @@ from jax.experimental import multihost_utils
 from jax.experimental.multihost_utils import sync_global_devices
 from jax.random import PRNGKey
 from transformers import AutoConfig, AutoModel, AutoModelForCausalLM, AutoTokenizer
-from transformers import GPT2Config as HfGpt2Config
 from transformers import PretrainedConfig as HfConfig
 from transformers import PreTrainedTokenizer
 from transformers.dynamic_module_utils import get_class_from_dynamic_module
@@ -401,46 +400,6 @@ class HFCheckpointConverter(Generic[LevConfig]):
                 shutil.rmtree(tmpdir)
 
 
-def load_hf_model_checkpoint(location_or_id, device=None, revision=None):
-    """
-    Loads a safetensors or PyTorch model checkpoint.
-    If model_file is None, this function attempts to load via safetensors first, then PyTorch.
-    """
-    local = os.path.exists(f"{location_or_id}/config.json")
-    if local:
-        config = json.load(open(f"{location_or_id}/config.json"))
-    else:
-        config_path = hf_hub_download(location_or_id, "config.json", revision=revision)
-        config = json.load(open(config_path))
-
-    if local:
-        if os.path.exists(f"{location_or_id}/{SAFE_TENSORS_MODEL}"):
-            checkpoint = safetensors.numpy.load_file(f"{location_or_id}/{SAFE_TENSORS_MODEL}")
-        elif os.path.exists(f"{location_or_id}/{PYTORCH_MODEL}"):
-            import torch
-
-            if isinstance(device, str):
-                device = torch.device(device)
-            checkpoint = torch.load(f"{location_or_id}/{PYTORCH_MODEL}", map_location=device)
-            checkpoint = {k: v.cpu().numpy() for k, v in checkpoint.items()}
-        else:
-            raise ValueError(f"Could not find model file for {location_or_id}")
-    else:
-        try:
-            model_path = hf_hub_download(location_or_id, SAFE_TENSORS_MODEL, revision=revision)
-            checkpoint = safetensors.numpy.load_file(model_path)
-        except EntryNotFoundError:  # noqa: E722
-            model_path = hf_hub_download(location_or_id, PYTORCH_MODEL, revision=revision)
-            import torch
-
-            if isinstance(device, str):
-                device = torch.device(device)
-            checkpoint = torch.load(model_path, map_location=device)
-            checkpoint = {k: v.cpu().numpy() for k, v in checkpoint.items()}
-
-    return config, checkpoint
-
-
 def backpack_config_to_hf(vocab_size: int, config, auto_map_config: Optional[HFAutoMapConfig] = None) -> HfConfig:
     config = HfConfig(
         vocab_size=vocab_size,
@@ -483,64 +442,6 @@ def hf_backpack_config_to_levanter(config: HfConfig):
     )
 
 
-def load_hf_gpt2_checkpoint(location_or_id, device=None, revision=None):
-    config, checkpoint = load_hf_model_checkpoint(location_or_id, device=device, revision=revision)
-
-    config = HfGpt2Config.from_dict(config)
-
-    Vocab = Axis("vocab", config.vocab_size)
-    from levanter.models.gpt2 import Gpt2Config, Gpt2LMHeadModel
-
-    lev_config = Gpt2Config.from_hf_config(config)
-    key = PRNGKey(0)
-
-    model = Gpt2LMHeadModel.init(Vocab, lev_config, key=key)
-
-    has_transformer_prefix = False
-    for k in checkpoint.keys():
-        if k.startswith("transformer."):
-            has_transformer_prefix = True
-            break
-        elif k.startswith(".h"):
-            break
-
-    if has_transformer_prefix:
-        model = model.from_state_dict(checkpoint, prefix="transformer")
-    else:
-        model = model.from_state_dict(checkpoint)
-
-    return model
-
-
-def _save_hf_gpt2_checkpoint_local(model, path):
-    os.makedirs(path, exist_ok=True)
-    config = model.config.to_hf_config(model.vocab_size)
-    with open(f"{path}/config.json", "w") as f:
-        json.dump(config.to_dict(), f)
-
-    def get_to_cpu(arr: Union[jnp.ndarray, np.ndarray]):
-        if isinstance(arr, np.ndarray):
-            return arr
-        elif "cpu" in arr.device().device_kind:
-            return np.array(arr)
-        elif arr.is_fully_addressable:
-            r = np.array(arr)
-            return r
-        else:
-            return np.array(jax.device_get(multihost_utils.process_allgather(arr, tiled=True)))
-
-    # need to make sure the model is on *this machine* and *this machine's CPU* before saving
-    model = jax.tree_map(lambda arr: get_to_cpu(arr), model)
-
-    # TODO: it's be nice if safetensors supported an iterator or something so we could do the allgather one at a time
-    state_dict = model.to_state_dict()
-
-    # now that we've moved the model to the CPU, we don't need to do this on all processes
-    if jax.process_index() == 0:
-        # the "pt" is a lie but it doesn't seem to actually matter and HF demands it
-        safetensors.numpy.save_file(state_dict, f"{path}/{SAFE_TENSORS_MODEL}", metadata={"format": "pt"})
-
-
 def _save_backpack_hf_checkpoint_local(
     model,
     path: str,
@@ -577,53 +478,6 @@ def _save_backpack_hf_checkpoint_local(
 
 def _is_url_like(path):
     return urllib.parse.urlparse(path).scheme != ""
-
-
-def save_hf_gpt2_checkpoint(model, path, hf_repo: Optional[str] = None, **hf_upload_kwargs):
-    """
-    If hf_repo is provided, this will upload the checkpoint to the huggingface hub, passing
-    any additional kwargs to the huggingface_hub.upload_folder function.
-
-    :param path: the path to save the checkpoint to. path may be a GCS bucket path, in which case the checkpoint will be
-    uploaded to GCS after being written to a tmp
-    :param model: the model to save
-    :param hf_repo:
-    :param hf_upload_kwargs: any additional kwargs to pass to huggingface_hub.upload_folder
-    :return:
-    """
-    tmpdir: Optional[str] = None
-    if _is_url_like(path):
-        tmpdir = tempfile.mkdtemp()
-        local_path = tmpdir
-    else:
-        local_path = path
-
-    try:
-        logger.info(f"Saving HF-compatible checkpoint to {local_path}")
-        from levanter.models.gpt2 import Gpt2LMHeadModel
-
-        _save_hf_gpt2_checkpoint_local(cast(Gpt2LMHeadModel, model), local_path)
-        logger.debug(f"Finished saving HF-compatible checkpoint to {local_path}")
-
-        sync_global_devices(path)
-
-        if jax.process_index() == 0:
-            if tmpdir is not None:  # we're uploading to GCS or similar
-                logger.info(f"Copying HF-compatible checkpoint to {path}")
-                fs: AbstractFileSystem
-                fs = fsspec.core.get_fs_token_paths(path, mode="wb")[0]
-                fs.put(os.path.join(local_path, "*"), path, recursive=True)
-                logger.debug(f"Finished copying HF-compatible checkpoint to {path}")
-
-            if hf_repo is not None:
-                logger.info(f"Uploading HF-compatible checkpoint to {hf_repo}")
-                huggingface_hub.upload_folder(local_path, hf_repo, **hf_upload_kwargs)
-                logger.debug(f"Finished uploading HF-compatible checkpoint to {hf_repo}")
-
-        sync_global_devices(path + " done")
-    finally:
-        if tmpdir is not None:
-            shutil.rmtree(tmpdir)
 
 
 def save_hf_checkpoint_callback(
