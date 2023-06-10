dfbca6c99cb4afdb88f70f36d24dd6667672c869
David Hall <dlwh@stanford.edu>
2023-06-09 20:13:56 -0700

    temporary mpt export

--- examples/convert_mpt_to_hf.py
+++ examples/convert_mpt_to_hf.py
@@ -12,7 +12,7 @@ import levanter
 from haliax import Axis
 from haliax.jax_utils import filter_eval_shape
 from levanter.compat.hf_checkpoints import HFCheckpointConverter, RepoRef
-from levanter.models.gpt2 import Gpt2Config, Gpt2LMHeadModel
+from levanter.models.mpt import MptConfig, MptLmHeadModel
 from levanter.tensorstore_serialization import tree_deserialize_leaves_tensorstore
 from levanter.utils.hf_utils import load_tokenizer
 
@@ -21,18 +21,19 @@ logger = logging.getLogger(__name__)
 
 
 @dataclass
-class ConvertGpt2Config:
+class ConvertMptConfig:
     checkpoint_path: str
     output_dir: str
     upload_to_hf: Optional[RepoRef] = None
 
-    model: Gpt2Config = Gpt2Config()
+    model: Optional[MptConfig] = None
 
     save_tokenizer: bool = True  # if True, save the tokenizer to the output directory
 
     tokenizer: str = "gpt2"
 
     override_vocab_size: Optional[int] = None  # if specified, override the vocab size in the config
+    override_sequence_length: Optional[int] = None  # if specified, override the sequence length in the config
 
     @cached_property
     def the_tokenizer(self):
@@ -40,7 +41,7 @@ class ConvertGpt2Config:
 
 
 @levanter.config.main()
-def main(config: ConvertGpt2Config):
+def main(config: ConvertMptConfig):
     logger.setLevel(logging.INFO)
     tokenizer: GPT2Tokenizer = config.the_tokenizer
 
@@ -52,17 +53,25 @@ def main(config: ConvertGpt2Config):
     with jax.default_device(jax.devices("cpu")[0]):
         # we want to call this in case we're on a TPU node
         jax.distributed.initialize()
+        converter = HFCheckpointConverter(MptConfig, "mosaicml/mpt-7b", trust_remote_code=True)
+        if config.model is None:
+            model_config = converter.config_from_hf_config(converter.default_hf_config)
+        else:
+            model_config = config.model
 
-        model = filter_eval_shape(Gpt2LMHeadModel.init, Vocab, config.model, key=key)
+        if config.override_sequence_length is not None:
+            model_config.max_seq_length = config.override_sequence_length
+
+        model = filter_eval_shape(MptLmHeadModel.init, Vocab, model_config, key=key)
 
         with hax.enable_shape_checks(False):
             model = tree_deserialize_leaves_tensorstore(f"{config.checkpoint_path}/model", model)
 
         model = htu.resize_axis(model, Vocab.resize(vocab_size), key=key)
 
-        converter = HFCheckpointConverter(Gpt2Config, "gpt2")
-
-        converter.save_model(model, config.output_dir, upload_to_hf=config.upload_to_hf or False)
+        converter.save_model(
+            model, config.output_dir, upload_to_hf=config.upload_to_hf or False, save_tokenizer=config.save_tokenizer
+        )
 
 
 if __name__ == "__main__":
