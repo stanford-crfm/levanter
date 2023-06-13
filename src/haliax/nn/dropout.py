162882ff6cf88a3619f76cd7f8d8bf49a3fe9849
David Hall <dlwh@cs.stanford.edu>
2023-06-13 14:09:13 -0700

    factor out a dropout function (#195)

--- src/haliax/nn/dropout.py
+++ src/haliax/nn/dropout.py
@@ -9,6 +9,48 @@ from haliax.types import AxisSpec
 from haliax.util import ensure_tuple
 
 
+def dropout(x, pdrop, broadcast_axes=None, *, inference=False, key=None):
+    """Applies dropout.
+
+    **Arguments:**
+
+    - `x`: An any-dimensional JAX array to dropout.
+    - `pdrop`: The fraction of entries to set to zero.
+    - `broadcast_axes`: The dimensions to broadcast the dropout mask over. If set, these axes will share the same mask
+    - `key`: A `jax.random.PRNGKey` used to provide randomness for calculating
+        which elements to dropout. (Keyword only argument.)
+    - `inference`: As per [`equinox.nn.Dropout.__init__`][]. If `True` or
+        `False` then it will take priority over `self.inference`. If `None`
+        then the value from `self.inference` will be used.
+    """
+    if inference:
+        return x
+    elif isinstance(pdrop, (int, float)) and pdrop == 0:
+        return x
+    elif isinstance(pdrop, (int, float)) and pdrop == 1:
+        return haliax.zeros_like(x)
+    elif key is None:
+        raise RuntimeError("Dropout requires a key when running in non-deterministic mode.")
+    else:
+        with jax.named_scope(name="dropout"):
+            if broadcast_axes is None:
+                if isinstance(x, NamedArray):
+                    shape_to_generate = x.axes
+                else:
+                    shape_to_generate = x.shape
+            else:
+                axes = ensure_tuple(broadcast_axes)
+                shape_to_generate = tuple(ax for ax in x.axes if ax not in axes)
+
+            q = 1 - pdrop
+            mask = haliax.random.bernoulli(key, shape_to_generate, q)
+            q = x.dtype.type(q)
+
+            out = haliax.where(mask, x / q, 0)
+            assert out.dtype == x.dtype
+            return out
+
+
 class Dropout(eqx.Module):
     """Applies dropout.
 
@@ -46,30 +88,10 @@ class Dropout(eqx.Module):
             then the value from `self.inference` will be used.
         - `deterministic`: Deprecated alternative to `inference`.
         """
-
-        if isinstance(self.pdrop, (int, float)) and self.pdrop == 0:
-            return x
-        elif isinstance(self.pdrop, (int, float)) and self.pdrop == 1:
-            return haliax.zeros_like(x)
-        elif inference:
-            return x
-        elif key is None:
-            raise RuntimeError("Dropout requires a key when running in non-deterministic mode.")
-        else:
-            with jax.named_scope(name="dropout"):
-                if self.broadcast_axes is None:
-                    if isinstance(x, NamedArray):
-                        shape_to_generate = x.axes
-                    else:
-                        shape_to_generate = x.shape
-                else:
-                    axes = ensure_tuple(self.broadcast_axes)
-                    shape_to_generate = tuple(ax for ax in x.axes if ax not in axes)
-
-                q = 1 - self.pdrop
-                mask = haliax.random.bernoulli(key, shape_to_generate, q)
-                q = x.dtype.type(q)
-
-                out = haliax.where(mask, x / q, 0)
-                assert out.dtype == x.dtype
-                return out
+        return dropout(
+            x,
+            self.pdrop,
+            broadcast_axes=self.broadcast_axes,
+            inference=inference,
+            key=key,
+        )
