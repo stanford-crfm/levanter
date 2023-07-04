b60488f7cf6b37c026f2fe029838b7ae2b5845f5
David Hall <dlwh@cs.stanford.edu>
2023-07-03 20:53:08 -0700

    fix issue where we throwing out data (#232)
    * fix issue where we throwing out data
    
    * please precommit

--- src/levanter/data/loader.py
+++ src/levanter/data/loader.py
@@ -93,7 +93,7 @@ class ShardedBatchLoader(BatchLoader[Ex]):
     def __iter__(self) -> Iterator[PyTree[jax.Array]]:
         one_item_generator = non_caching_cycle(self.item_dataset)
 
-        for i, item in enumerate(one_item_generator):
+        while True:
             # ok this is a bit messy: we want to create a batch of items from our dataset, only loading
             # the relevant data for each process.
             # In general an item is represented as a PyTree, whose leaves are (named or unnamed) arrays.
@@ -149,10 +149,6 @@ class ShardedBatchLoader(BatchLoader[Ex]):
             ]
 
             gda_tree = jax.tree_util.tree_unflatten(batch_tree_structure, gda_leaves)
-
-            if i % 100 == 0 and logger.getEffectiveLevel() <= logging.DEBUG:
-                for leaf in gda_leaves:
-                    check_sharded_consistency(leaf, True)
 
             yield gda_tree  # type: ignore
 
