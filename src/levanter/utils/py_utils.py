f45f3f505f679552b0aec61662df4f659eb37436
David Hall <dlwh@stanford.edu>
2023-07-19 23:33:21 -0700

    update mypy version

--- src/levanter/utils/py_utils.py
+++ src/levanter/utils/py_utils.py
@@ -55,6 +55,28 @@ def dataclass_with_default_init(_cls=None, *args, **kwargs):
 # ==============================================================================
 
 
+class classproperty(object):  # pylint: disable=invalid-name
+    """Class property decorator.
+
+    Example usage:
+
+    class MyClass(object):
+
+      @classproperty
+      def value(cls):
+        return '123'
+
+    > print MyClass.value
+    123
+    """
+
+    def __init__(self, func):
+        self._func = func
+
+    def __get__(self, owner_self, owner_cls):
+        return self._func(owner_cls)
+
+
 class _CachedClassProperty(object):
     """Cached class property decorator.
 
