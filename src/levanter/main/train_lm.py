58e0c9889d893336f078e6b8bcd7402c4c230d13
David Hall <dlwh@stanford.edu>
2023-07-27 20:46:13 -0700

    update for latest jax, fix a small bug

--- src/levanter/main/train_lm.py
+++ src/levanter/main/train_lm.py
@@ -247,7 +247,8 @@ def main(config: TrainLmConfig):
 
                 pred_y = model(example.tokens, example.attn_mask, inference=True, key=None)
                 pred_y = mp.cast_to_output(pred_y)
-                loss = cross_entropy_loss(pred_y, Vocab, example.targets, where=example.loss_mask, reduction=None)
+                targets = hax.nn.one_hot(example.tokens, Vocab, dtype=pred_y.dtype)
+                loss = cross_entropy_loss(pred_y, Vocab, targets, where=example.loss_mask, reduction=None)
                 logprobs = -loss
                 # roll forward to get the loss for each predicted token
                 logprobs = haliax.roll(logprobs, 1, Pos)
