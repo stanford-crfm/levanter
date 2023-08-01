0e77701907da857d90d668f4e794319e5d2651ad
Ivan-Zhou <ivan.zhouyq@gmail.com>
2023-07-31 19:35:50 -0700

    test compare with hf implementation

--- tests/test_llama.py
+++ tests/test_llama.py
@@ -1,16 +1,28 @@
 from jax import random
 
+import numpy as np
+import torch
+
+# src/transformers/models/llama/modeling_llama.py
+from transformers.models.llama.modeling_llama import LlamaConfig
+from transformers.models.llama.modeling_llama import LlamaRotaryEmbedding as HFLlamaRotaryEmbedding
 from levanter.models.llama import LlamaRotaryEmbedding
 
 
 def test_llama_rotary_embedding():
+    """Match against HuggingFace's implementation of LlamaRotaryEmbedding."""
     dim = 2048
-    max_position_embeddings = 2048
     seq_len = 2048
-    base = 10000
     key = random.PRNGKey(0)
-    rotary_emb = LlamaRotaryEmbedding(dim=dim)
-    rotary_emb.setup()
-    x = random.normal(key, (1, 2048))
-    x = rotary_emb(x, seq_len=seq_len)
+    x = random.normal(key, (1, seq_len))
+    levanter_rotary_emb = LlamaRotaryEmbedding(dim=dim)
+    levanter_rotary_emb.setup()
+    levanter_output = levanter_rotary_emb(x, seq_len=seq_len)
 
+    hf_rotary_emb = HFLlamaRotaryEmbedding(dim=dim, device="cpu")
+    x_torch = torch.from_numpy(np.array(x))
+    hf_output = hf_rotary_emb(x_torch, seq_len=seq_len)
+
+    for jax_out, torch_out in zip(levanter_output, hf_output):
+        torch_out = torch_out.numpy()
+        assert np.isclose(torch_out, np.array(jax_out), rtol=1e-2, atol=1e-2).all(), f"{torch_out} != {jax_out}"
