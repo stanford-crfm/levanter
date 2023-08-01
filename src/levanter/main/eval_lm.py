3e1f224facd8bfdd0b040512fccd14b1ed7e9117
David Hall <dlwh@cs.stanford.edu>
2023-08-01 14:44:34 -0700

    eqx's filter_eval_shape is actually all we need (#268)

--- src/levanter/main/eval_lm.py
+++ src/levanter/main/eval_lm.py
@@ -3,6 +3,7 @@ import logging
 from dataclasses import dataclass
 from typing import Optional
 
+import equinox as eqx
 import jax
 import jmp
 import numpy
@@ -10,7 +11,6 @@ import tqdm
 
 import haliax as hax
 from haliax import Axis
-from haliax.jax_utils import filter_eval_shape
 from haliax.nn import cross_entropy_loss
 from haliax.partitioning import named_jit, round_axis_for_partitioning
 
@@ -88,7 +88,7 @@ def main(config: EvalLmConfig):
         if config.checkpoint_path is not None:
             # initialize the model
             with jax.default_device(jax.devices("cpu")[0]):
-                model = filter_eval_shape(config.model.build, Vocab, key=key)
+                model = eqx.filter_eval_shape(config.model.build, Vocab, key=key)
                 # TODO: don't load the entire checkpoint into CPU memory when we only need our share of the model
                 ckpt = load_checkpoint(model, None, config.checkpoint_path)
 
