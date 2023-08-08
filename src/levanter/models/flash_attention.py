70a932767f6baee7427f2ddda82218f5838e988b
David Hall <dlwh@stanford.edu>
2023-08-08 10:24:07 -0700

    mjkamflkf

--- src/levanter/models/flash_attention.py
+++ src/levanter/models/flash_attention.py
@@ -223,9 +223,9 @@ def _flash_attention_backward(
             # TODO: precision
             attn_ij = hax.dot(Key, q_i, k_j)
 
-            # if mask is not None:
-            #     mask_ij = mask.slice(QPos, QPosBlock, i * block_size).slice(KPos, KPosBlock, j * block_size)
-            #     attn_ij = hax.where(mask_ij, attn_ij, -1e10)
+            if mask is not None:
+                mask_ij = mask.slice(QPos, QPosBlock, i * block_size).slice(KPos, KPosBlock, j * block_size)
+                attn_ij = hax.where(mask_ij, attn_ij, -1e10)
 
             p_ij = hax.exp(attn_ij - L_i)
 
