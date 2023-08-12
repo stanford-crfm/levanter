f357a76065afeb9959b267830881b980dfd32c88
David Hall <dlwh@stanford.edu>
2023-08-11 23:19:09 -0700

    remove prints

--- src/levanter/grad_accum.py
+++ src/levanter/grad_accum.py
@@ -12,7 +12,6 @@ from haliax.jax_utils import named_call
 from haliax.partitioning import ResourceAxis
 from haliax.util import is_named_array
 
-from levanter.models.mpt import MptLmHeadModel
 from levanter.utils.jax_utils import reduce
 
 
@@ -109,11 +108,6 @@ def accumulate_gradients_sharded(
             with hax.axis_mapping(parameter_axis_mapping):
                 this_loss, this_grad = f(model, *microbatch, **kwargs)
                 this_grad = hax.shard_with_axis_mapping(this_grad, parameter_axis_mapping)
-            this_grad: MptLmHeadModel
-
-            jax.debug.inspect_array_sharding(
-                this_grad.transformer.blocks.stacked.attn.Wqkv.weight.array, callback=lambda x: print(x, flush=True)
-            )
 
         with jax.named_scope("accum"):
             loss += this_loss
