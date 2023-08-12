1ba201f25fbd1e7ea1181be084cd4626b2a38fed
David Hall <dlwh@stanford.edu>
2023-08-11 21:30:35 -0700

    zzzzz

--- src/levanter/grad_accum.py
+++ src/levanter/grad_accum.py
@@ -106,13 +106,14 @@ def accumulate_gradients_sharded(
             kwargs = microbatch_kwargs.copy()
             if key is not None:
                 kwargs["key"] = key
-            this_loss, this_grad = f(model, *microbatch, **kwargs)
+            with hax.axis_mapping(parameter_axis_mapping):
+                this_loss, this_grad = f(model, *microbatch, **kwargs)
+                this_grad = hax.shard_with_axis_mapping(this_grad, parameter_axis_mapping)
             this_grad: MptLmHeadModel
 
             jax.debug.inspect_array_sharding(
                 this_grad.transformer.blocks.stacked.attn.Wqkv.weight.array, callback=lambda x: print(x, flush=True)
             )
-            this_grad = hax.shard_with_axis_mapping(this_grad, parameter_axis_mapping)
 
         with jax.named_scope("accum"):
             loss += this_loss
