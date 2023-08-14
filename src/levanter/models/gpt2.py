c09d2713f5adbdc08f9f9f4168376b74c5917ee8
David Hall <dlwh@stanford.edu>
2023-08-14 13:42:39 -0700

    comment flash attention

--- src/levanter/models/gpt2.py
+++ src/levanter/models/gpt2.py
@@ -62,7 +62,8 @@ class Gpt2Config(HFCompatConfig):
     gradient_checkpointing_block_size: int = 5
 
     use_bias: bool = True
-    use_flash_attention: bool = False
+
+    use_flash_attention: bool = False  # use flash attention. This is a pure jax impl, and is not faster than normal, but it scales to long sequence lengths
     flash_attention_block_size: int = 1024
 
     # Axes
