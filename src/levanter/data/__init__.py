a4e926c2064b5dfbc2a11717ee07381e7317ea77
David Hall <dlwh@stanford.edu>
2023-10-11 10:55:06 -0700

    wip

--- src/levanter/data/__init__.py
+++ src/levanter/data/__init__.py
@@ -2,6 +2,7 @@ from levanter.data.dataset import Dataset, ShardableDataset, ShuffleDataset
 from levanter.data.loader import BatchLoader, ReplicatedBatchLoader, ShardedBatchLoader
 from levanter.data.shard_cache import build_cache
 from levanter.data.utils import batched
+from levanter.data.shard_source import ShardedDataset, dataset_from_hf, dataset_from_jsonl
 
 
 __all__ = [
@@ -13,4 +14,7 @@ __all__ = [
     "ReplicatedBatchLoader",
     "ShardedBatchLoader",
     "build_cache",
+    "ShardedDataset",
+    "dataset_from_hf",
+    "dataset_from_jsonl",
 ]
