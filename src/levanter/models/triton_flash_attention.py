f677870e5fe48c63efe82403f1de4be24a83355b
Virginia Adams <vadam5@stanford.edu>
2023-11-16 09:18:16 -0800

    most pieces in place for explicit fwd/bwd kernel call

--- src/levanter/models/triton_flash_attention.py
+++ src/levanter/models/triton_flash_attention.py
@@ -1,128 +1,203 @@
-# JAX wrapper around triton fused attention kernel
-# https://github.com/openai/triton/blob/main/python/triton/ops/flash_attention.py
-# Another example of calling triton kernels in JAX:
-# https://github.com/jax-ml/jax-triton/blob/main/examples/fused_attention.py#L127
-
 from typing import Tuple
 
 import equinox
 import jax
+import jax.numpy as jnp
 import jax_triton as jt
-from triton.ops.flash_attention import attention
+from triton import cdiv
+from triton.ops.flash_attention import _bwd_kernel, _bwd_preprocess, _fwd_kernel
 
 import haliax as hax
-from haliax import NamedArray
-from haliax.jax_utils import named_call
 
 
-@named_call
 def triton_flash_attention(
-    q: NamedArray,
-    k: NamedArray,
-    v: NamedArray,
+    q: hax.NamedArray,
+    k: hax.NamedArray,
+    v: hax.xNamedArray,
     softmax_scale: float = 1.0,
     causal: bool = True,
-    sequence_parallel: bool = False,
-) -> hax.NamedArray:
-
-    output_shape = jax.ShapeDtypeStruct(shape=q.shape.values(), dtype=q.dtype)
+):
+    out_shape = jax.ShapeDtypeStruct(shape=q.shape.values(), dtype=q.dtype)
     qkv = (q, k, v)
 
     return _triton_flash_attention(
         qkv,
-        output_shape=output_shape,
+        out_shape=out_shape,
         softmax_scale=softmax_scale,
         causal=causal,
-        sequence_parallel=sequence_parallel,
     )
 
 
 @equinox.filter_custom_vjp
 def _triton_flash_attention(
     qkv: Tuple[hax.NamedArray, hax.NamedArray, hax.NamedArray],
-    output_shape: jax.ShapeDtypeStruct,
+    out_shape: jax.ShapeDtypeStruct,
     softmax_scale: float = 1.0,
     causal: bool = True,
     sequence_parallel: bool = False,
 ):
-    """_triton_flash_attention exists (instead of applying the forward and backward
-    functions directly to triton_flash_attention) because the custom_vjp decorator
-    only differentiates the first argument of the function its applied to.
-
-    Args:
-        qkv (Tuple[hax.NamedArray, hax.NamedArray, hax.NamedArray]): _description_
-        output_shape (jax.ShapeDtypeStruct): _description_
-        softmax_scale (float, optional): _description_. Defaults to 1.0.
-        causal (bool, optional): _description_. Defaults to True.
-        sequence_parallel (bool, optional): _description_. Defaults to False.
-
-    Returns:
-        _type_: _description_
-    """
-
     return _triton_flash_attention_forward(
         None,
         qkv,
-        output_shape=output_shape,
+        out_shape=out_shape,
         softmax_scale=softmax_scale,
         causal=causal,
-        sequence_parallel=sequence_parallel,
     )
 
 
 def _triton_flash_attention_forward(
     ignore,
     qkv,
-    output_shape: jax.ShapeDtypeStruct,
+    out_shape: jax.ShapeDtypeStruct,
     softmax_scale: float = 1.0,
     causal: bool = True,
-    sequence_parallel: bool = False,
 ):
-    """Calls the Triton kernel implemention of flash attention.
-
-    Args:
-        q (NamedArray): _description_
-        k (NamedArray): _description_
-        v (NamedArray): _description_
-        output_shape (jax.ShapeDtypeStruct): _description_
-        softmax_scale (float, optional): _description_. Defaults to 1.0.
-        causal (bool, optional): _description_. Defaults to True.
-        sequence_parallel (bool, optional): _description_. Defaults to False.
-
-    Returns:
-        _type_: _description_
-    """
     del ignore
     q, k, v = qkv
 
-    jax.debug.print("\n\n MAKING CALL TO KERNEL NOW!\n\n")
+    # only support for Ampere now
+    BLOCK_M = 128
+    BLOCK_N = 64
+
+    # shape constraints
+    Lq, Lk, Lv = q.shape["head_size"], k.shape["head_size"], v.shape["head_size"]
+    assert Lq == Lk and Lk == Lv
+    assert Lk in {16, 32, 64, 128}
+
+    # Init output placeholers
+    out = jnp.empty_like(q)
+    L = jnp.empty((q.shape["batch"] * q.shape["heads"], q.shape["position"]), device=q.device, dtype=jnp.float32)
+
+    grid = (cdiv(q.shape["position"], BLOCK_M), q.shape["batch"] * q.shape["heads"], 1)
+    num_warps = 4 if Lk <= 64 else 8
+
     attn_output = jt.triton_call(
         q.array,
         k.array,
         v.array,
-        causal,
-        sequence_parallel,
         softmax_scale,
-        kernel=attention,
-        out_shape=output_shape,
-        grid=None,
+        L,
+        out,
+        q.array.stride(0),
+        q.array.stride(1),
+        q.array.stride(2),
+        q.array.stride(3),
+        k.array.stride(0),
+        k.array.stride(1),
+        k.array.stride(2),
+        k.array.stride(3),
+        v.array.stride(0),
+        v.array.stride(1),
+        v.array.stride(2),
+        v.array.stride(3),
+        out.stride(0),
+        out.stride(1),
+        out.stride(2),
+        out.stride(3),
+        q.shape["batch"],
+        q.shape["heads"],
+        q.shape["position"],
+        q.shape["batch"] * q.shape["heads"] * q.shape["position"],
+        BLOCK_M=BLOCK_M,
+        BLOCK_N=BLOCK_N,
+        BLOCK_DMODEL=Lk,
+        IS_CAUSAL=causal,
+        num_warps=num_warps,
+        num_stages=4,
+        kernel=_fwd_kernel,
+        out_shape=out_shape,
+        grid=grid,
     )
-    return hax.named(attn_output, q.axes)
+
+    return hax.named(attn_output, q.axes), (out, L, Lk)
 
 
 def _triton_flash_attention_backward(
-    d_out: NamedArray,
+    residuals,
+    grad_in: hax.NamedArray,
+    ignore,
+    qkv,
+    out_shape: jax.ShapeDtypeStruct,
+    softmax_scale: float = 1.0,
+    causal: bool = True,
 ):
-    outshape = jax.ShapeDtypeStruct(shape=d_out.shape.values(), dtype=d_out.dtype)
+    del ignore
+    out, L, Lk = residuals
+    q, k, v = qkv
+    d_out = grad_in
 
-    jax.debug.print("\n\nCALLING TRITON FLASH ATTEN BACKWARD\n\n")
-    backward_out = jt.triton_call(
-        do=d_out.array,
-        kernel=attention.backward,
-        out_shape=outshape,
-        grid=None,
+    BLOCK = 128
+    MMA_V3 = False
+    seq_len_kv = k.shape["position"]
+    d_out = d_out.contiguous()
+    out_shape = jax.ShapeDtypeStruct(shape=q.shape.values(), dtype=q.dtype)
+
+    # Create output objects
+    dq = jnp.zeros_like(q, dtype=q.dtype)
+    dk = jnp.empty_like(k)
+    dv = jnp.empty_like(v)
+    delta = jnp.empty_like(L)
+    bwd_preprocess_grid = (cdiv(q.shape["position"], BLOCK) * q.shape["batch"] * q.shape["heads"],)
+
+    jt.triton_call(
+        out,
+        d_out,
+        delta,
+        BLOCK_M=BLOCK,
+        D_HEAD=Lk,
+        kernel=_bwd_preprocess,
+        out_shape=out_shape,
+        grid=bwd_preprocess_grid,
     )
-    return hax.named(backward_out, d_out.axes)
+
+    bwd_grid = (q.shape["batch"] * q.shape["heads"], 1)
+
+    jt.triton_call(
+        q.array,
+        k.array,
+        v.array,
+        softmax_scale,
+        out,
+        d_out,
+        dq,
+        dk,
+        dv,
+        L,
+        delta,
+        out.array.numel(),
+        q.array.stride(0),
+        q.array.stride(1),
+        q.array.stride(2),
+        q.array.stride(3),
+        k.array.stride(0),
+        k.array.stride(1),
+        k.array.stride(2),
+        k.array.stride(3),
+        v.array.stride(0),
+        v.array.stride(1),
+        v.array.stride(2),
+        v.array.stride(3),
+        q.shape["batch"],
+        q.shape["head"],
+        q.shape["position"],
+        q.shape["batch"] * q.shape["head"] * q.shape["position"],
+        cdiv(seq_len_kv, BLOCK) * q.shape["batch"] * q.shape["head"] * q.shape["position"],
+        BLOCK_M=BLOCK,
+        BLOCK_N=BLOCK,
+        BLOCK_DMODEL=Lk,
+        SEQUENCE_PARALLEL=False,
+        CAUSAL=causal,
+        MMA_V3=MMA_V3,
+        num_warps=8,
+        num_stages=1,
+        kernel=_bwd_kernel,
+        grid=bwd_grid,
+    )
+
+    if len(dq.shape) == 5:
+        dq = dq.sum(dim=0)
+
+    return dq, dk, dv, None, None, None
 
 
 _triton_flash_attention.def_fwd(_triton_flash_attention_forward)
---
+++ src/levanter/models/triton_flash_attention_temp.py
@@ -0,0 +1,129 @@
+# JAX wrapper around triton fused attention kernel
+# https://github.com/openai/triton/blob/main/python/triton/ops/flash_attention.py
+# Another example of calling triton kernels in JAX:
+# https://github.com/jax-ml/jax-triton/blob/main/examples/fused_attention.py#L127
+
+from typing import Tuple
+
+import equinox
+import jax
+import jax_triton as jt
+from triton.ops.flash_attention import attention
+
+import haliax as hax
+from haliax import NamedArray
+from haliax.jax_utils import named_call
+
+
+@named_call
+def triton_flash_attention(
+    q: NamedArray,
+    k: NamedArray,
+    v: NamedArray,
+    softmax_scale: float = 1.0,
+    causal: bool = True,
+    sequence_parallel: bool = False,
+) -> hax.NamedArray:
+
+    output_shape = jax.ShapeDtypeStruct(shape=q.shape.values(), dtype=q.dtype)
+    qkv = (q, k, v)
+
+    return _triton_flash_attention(
+        qkv,
+        output_shape=output_shape,
+        softmax_scale=softmax_scale,
+        causal=causal,
+        sequence_parallel=sequence_parallel,
+    )
+
+
+@equinox.filter_custom_vjp
+def _triton_flash_attention(
+    qkv: Tuple[hax.NamedArray, hax.NamedArray, hax.NamedArray],
+    output_shape: jax.ShapeDtypeStruct,
+    softmax_scale: float = 1.0,
+    causal: bool = True,
+    sequence_parallel: bool = False,
+):
+    """_triton_flash_attention exists (instead of applying the forward and backward
+    functions directly to triton_flash_attention) because the custom_vjp decorator
+    only differentiates the first argument of the function its applied to.
+
+    Args:
+        qkv (Tuple[hax.NamedArray, hax.NamedArray, hax.NamedArray]): _description_
+        output_shape (jax.ShapeDtypeStruct): _description_
+        softmax_scale (float, optional): _description_. Defaults to 1.0.
+        causal (bool, optional): _description_. Defaults to True.
+        sequence_parallel (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """
+
+    return _triton_flash_attention_forward(
+        None,
+        qkv,
+        output_shape=output_shape,
+        softmax_scale=softmax_scale,
+        causal=causal,
+        sequence_parallel=sequence_parallel,
+    )
+
+
+def _triton_flash_attention_forward(
+    ignore,
+    qkv,
+    output_shape: jax.ShapeDtypeStruct,
+    softmax_scale: float = 1.0,
+    causal: bool = True,
+    sequence_parallel: bool = False,
+):
+    """Calls the Triton kernel implemention of flash attention.
+
+    Args:
+        q (NamedArray): _description_
+        k (NamedArray): _description_
+        v (NamedArray): _description_
+        output_shape (jax.ShapeDtypeStruct): _description_
+        softmax_scale (float, optional): _description_. Defaults to 1.0.
+        causal (bool, optional): _description_. Defaults to True.
+        sequence_parallel (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """
+    del ignore
+    q, k, v = qkv
+
+    jax.debug.print("\n\n MAKING CALL TO KERNEL NOW!\n\n")
+    attn_output = jt.triton_call(
+        q.array,
+        k.array,
+        v.array,
+        causal,
+        sequence_parallel,
+        softmax_scale,
+        kernel=attention,
+        out_shape=output_shape,
+        grid=None,
+    )
+    return hax.named(attn_output, q.axes)
+
+
+def _triton_flash_attention_backward(
+    d_out: NamedArray,
+):
+    outshape = jax.ShapeDtypeStruct(shape=d_out.shape.values(), dtype=d_out.dtype)
+
+    jax.debug.print("\n\nCALLING TRITON FLASH ATTEN BACKWARD\n\n")
+    backward_out = jt.triton_call(
+        do=d_out.array,
+        kernel=attention.backward,
+        out_shape=outshape,
+        grid=None,
+    )
+    return hax.named(backward_out, d_out.axes)
+
+
+_triton_flash_attention.def_fwd(_triton_flash_attention_forward)
+_triton_flash_attention.def_bwd(_triton_flash_attention_backward)
