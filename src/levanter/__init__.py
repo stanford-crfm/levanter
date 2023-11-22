e3746971a35864c2e9f26c79b566728cbc94f690
David Hall <dlwh@stanford.edu>
2023-11-21 23:50:55 -0600

    tweak

--- src/levanter/__init__.py
+++ src/levanter/__init__.py
@@ -5,4 +5,4 @@ import levanter.distributed as distributed
 import levanter.logging as logging
 import levanter.tracker as tracker
 import levanter.visualization as visualization
-from levanter.tracker import current_tracker, get_tracker, jit_log_metrics, log_metrics, log_summary
+from levanter.tracker import current_tracker, get_tracker, log_metrics, log_summary
