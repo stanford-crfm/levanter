727283e53fbbdc5f4be8fdd65285fc5bbfea803e
David Hall <dlwh@stanford.edu>
2023-11-28 20:16:30 -0800

    disable logging for now

--- src/levanter/optim.py
+++ src/levanter/optim.py
@@ -18,7 +18,7 @@ from optax._src import numerics
 from optax._src.schedule import InjectHyperparamsState, _convert_floats
 from optax._src.transform import bias_correction, update_moment
 
-import levanter.tracker
+# import levanter.tracker
 from levanter.utils.jax_utils import parameter_count
 
 
@@ -522,8 +522,9 @@ def _sophia_gradient_transform(
             stats["optim/unclipped_fraction"] = unclipped_count / parameter_count(updates)
 
         # this doesn't work well on CPU, so skip if cpu
-        if jax.lib.xla_bridge.get_backend().platform != "cpu":
-            levanter.tracker.jit_log_metrics(stats, step=state.count)
+        # TODO: bring this back
+        # if jax.lib.xla_bridge.get_backend().platform != "cpu":
+        #     levanter.tracker.jit_log_metrics(stats, step=state.count)
 
         if mu_dtype is not None:
             mu = jax.tree_util.tree_map(lambda t: t.astype(mu_dtype), mu)
