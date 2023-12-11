92a615fb7b4a801b20405566ff996699403c29a3
David Hall <dlwh@stanford.edu>
2023-12-11 00:23:50 -0800

    should use a tempdir

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,11 +1,15 @@
+import tempfile
+
 from levanter.data.text import LMDatasetConfig
 
 
 def test_dont_blow_up_without_validation_set():
-    config = LMDatasetConfig(
-        train_urls=["kaa"],
-        validation_urls=[],
-    )
+    with tempfile.TemporaryDirectory() as tmpdir:
+        config = LMDatasetConfig(
+            train_urls=["kaa"],
+            validation_urls=[],
+            cache_dir=tmpdir,
+        )
 
     # mostly just making sure this doesn't blow up
     assert config.validation_set(10) is None
