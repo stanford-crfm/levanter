9f16fbe10dd207f05ea6aacea6b5bf7425be717f
David Hall <dlwh@stanford.edu>
2024-01-15 11:55:09 -0800

    make logging list names of caches

--- src/levanter/data/text.py
+++ src/levanter/data/text.py
@@ -568,9 +568,11 @@ class LMDatasetConfig(LMDatasetSourceConfig, LMTaskConfig):
         return TokenSeqDataset(cache, seq_len)
 
     def build_or_load_cache(
-        self, split: str, monitors: Union[bool, List[MetricsMonitor]] = True
+        self, split: str, monitors: Union[bool, List[MetricsMonitor]] = True, logger_name: Optional[str] = None
     ) -> Optional[TokenizedDocumentCache]:
         split_cache_dir = os.path.join(self.cache_dir, split)
+        name = logger_name or os.path.basename(self.cache_dir)
+
         try:
             return TokenizedDocumentCache.load(split_cache_dir, flatten_docs=True)
         except FileNotFoundError:
@@ -585,8 +587,8 @@ class LMDatasetConfig(LMDatasetSourceConfig, LMTaskConfig):
 
         if monitors is True:
             monitors = [
-                LoggingMetricsMonitor(prefix=f"preprocessing/{split}", commit=False),
-                LoggerMetricsMonitor(f"preprocessing.{split}"),
+                LoggingMetricsMonitor(prefix=f"preprocessing/{name}/{split}", commit=False),
+                LoggerMetricsMonitor(f"preprocessing.{name}.{split}"),
             ]
         elif monitors is False:
             monitors = []
