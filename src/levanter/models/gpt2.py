fab75a1c70b43d7970bdb4cddd4f01dd4263ff3d
Ahmed Ahmed <ahmedah@stanford.edu>
2024-02-10 21:57:29 -0800

    fix

--- src/levanter/models/gpt2.py
+++ src/levanter/models/gpt2.py
@@ -286,7 +286,7 @@ class Gpt2Block(StateDictSerializationMixin, eqx.Module):
                 if mode == "integrated":
                     return hax.sin(0.1*hax.sum(prev_x, axis='position')) + attn_output + ff_output
                 elif mode == "mod":
-                    return x + ff_output# hax.sin(0.1*hax.sum(x, axis='position')) + ff_output
+                    return hax.sin(0.1*x) + ff_output
                 else:
                     return x + ff_output
             def false_fun(_):
