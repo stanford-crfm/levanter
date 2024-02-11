ac704ef516061ffa5dd27106c36a35c5bc64d7b2
David Hall <dlwh@cs.stanford.edu>
2024-02-10 23:38:02 -0800

    Misc fixes (#461)
    * ensure we do the final softmax in fp32
    
    * tweak number of cores for tokenization
    
    * use the trainer better
    
    * switch to tracker in shard_cache
    
    * expose current_tracker in levanter base package
    
    * add back in logging of sophia metrics
    
    * config fixes
    
    * missed a spot
    
    * raise if there's validation set in eval_lm
    
    * is precommit being crappy?
    
    * sigh

--- src/levanter/optim/sophia.py
+++ src/levanter/optim/sophia.py
@@ -11,7 +11,7 @@ from jax import numpy as jnp
 from jax.random import PRNGKey
 from jaxtyping import PRNGKeyArray
 
-# import levanter.tracker
+import levanter.tracker
 from levanter.optim.config import HessianOptConfig, OptimizerConfig
 from levanter.optim.util import hvp, tree_gaussian_like
 from levanter.utils.jax_utils import parameter_count, tree_filter_like
@@ -348,9 +348,7 @@ def _sophia_gradient_transform(
             updates = jax.tree_util.tree_map(lambda u: jnp.clip(u, -clip_threshold, clip_threshold), updates)
             stats["optim/unclipped_fraction"] = unclipped_count / parameter_count(updates)
 
-        # this doesn't work well on CPU, so skip if cpu
-        # if jax.lib.xla_bridge.get_backend().platform != "cpu":
-        #     levanter.tracker.jit_log_metrics(stats, step=state.count)
+        levanter.tracker.jit_log_metrics(stats, step=state.count)
 
         if mu_dtype is not None:
             mu = jax.tree_util.tree_map(lambda t: t.astype(mu_dtype), mu)
