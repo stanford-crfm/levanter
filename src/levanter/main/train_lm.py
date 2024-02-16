853f7730fea5b71609a1164ea18227337549eeeb
Ahmed Ahmed <ahmedah@stanford.edu>
2024-02-15 16:08:31 -0800

    fix

--- src/levanter/main/train_lm.py
+++ src/levanter/main/train_lm.py
@@ -150,7 +150,7 @@ def main(config: TrainLmConfig):
 
                 # what is the f here?
                 logger.info(f"Interpolating between the two models with alpha={alpha}")
-                merged_model = named_jit(lambda m1, m2: jax.tree_util.tree_map(add_floats, m1, m2), donate=True)(model, model_2)
+                merged_model = named_jit(lambda m1, m2: jax.tree_util.tree_map(add_floats, m1, m2), donate_args=True)(model, model_2)
                 state = dataclasses.replace(state, model=model)
             else:
                 logger.info("No checkpoint found. Starting from scratch.")
