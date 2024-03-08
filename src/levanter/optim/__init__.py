895437a88b9b58fb9bbe411127820bc4dc6c452a
Ahmed Ahmed <ahmedah@stanford.edu>
2024-03-08 09:51:20 -0800

    bring back optim

---
+++ src/levanter/optim/__init__.py
@@ -0,0 +1,7 @@
+from .config import AdamConfig, OptimizerConfig
+from .sophia import (  # SophiaGConfig,; SophiaGObjective,
+    ScaleBySophiaState,
+    SophiaHConfig,
+    scale_by_sophia_g,
+    scale_by_sophia_h,
+)
