2298b2f95d35d07c80b19ce08d782a4fafeafced
Helw150 <wbh230@nyu.edu>
2024-03-10 22:23:30 +0000

    Abstract out loading from HF with FSSpec

--- src/levanter/compat/hf_checkpoints.py
+++ src/levanter/compat/hf_checkpoints.py
@@ -793,8 +793,9 @@ def save_hf_checkpoint_callback(
     return cb
 
 
-def load_tokenizer(model_name_or_path, revision=None, local_cache_dir=None, trust_remote_code=True):
-    """Like AutoTokenizer.from_pretrained, but works with gs:// paths or anything on fsspec"""
+def arbitrary_load_from_hf(
+    model_name_or_path, loading_lambda, revision=None, local_cache_dir=None, trust_remote_code=True
+) -> Union[PreTrainedTokenizerBase | ProcessorMixin]:
     is_url_like = urlparse(model_name_or_path).scheme != ""
     if is_url_like:
         if revision is not None:
@@ -806,35 +807,33 @@ def load_tokenizer(model_name_or_path, revision=None, local_cache_dir=None, trus
         fs, path = fsspec.core.url_to_fs(model_name_or_path)
         fs.get(path, local_cache_dir, recursive=True)
         base_path = os.path.basename(path)
-        return AutoTokenizer.from_pretrained(
-            os.path.join(local_cache_dir, base_path), trust_remote_code=trust_remote_code
-        )
+        return loading_lambda(os.path.join(local_cache_dir, base_path), trust_remote_code=trust_remote_code)
     else:
-        return AutoTokenizer.from_pretrained(
-            model_name_or_path, revision=revision, trust_remote_code=trust_remote_code
-        )
+        return loading_lambda(model_name_or_path, revision=revision, trust_remote_code=trust_remote_code)
+
+
+def load_tokenizer(
+    model_name_or_path, revision=None, local_cache_dir=None, trust_remote_code=True
+) -> PreTrainedTokenizerBase:
+    """Like AutoTokenizer.from_pretrained, but works with gs:// paths or anything on fsspec"""
+    return arbitrary_load_from_hf(
+        model_name_or_path,
+        AutoTokenizer.from_pretrained,
+        revision=revision,
+        local_cache_dir=local_cache_dir,
+        trust_remote_code=trust_remote_code,
+    )
 
 
 def load_processor(model_name_or_path, revision=None, local_cache_dir=None, trust_remote_code=True) -> ProcessorMixin:
-    """Like AutoTokenizer.from_pretrained, but works with gs:// paths or anything on fsspec"""
-    is_url_like = urlparse(model_name_or_path).scheme != ""
-    if is_url_like:
-        if revision is not None:
-            raise ValueError("revision is not supported for URLs")
-        # tokenizers are directories, so we have to copy them locally
-        if local_cache_dir is None:
-            local_cache_dir = tempfile.mkdtemp()
-
-        fs, path = fsspec.core.url_to_fs(model_name_or_path)
-        fs.get(path, local_cache_dir, recursive=True)
-        base_path = os.path.basename(path)
-        return AutoProcessor.from_pretrained(
-            os.path.join(local_cache_dir, base_path), trust_remote_code=trust_remote_code
-        )
-    else:
-        return AutoProcessor.from_pretrained(
-            model_name_or_path, revision=revision, trust_remote_code=trust_remote_code
-        )
+    """Like AutoProcessor.from_pretrained, but works with gs:// paths or anything on fsspec"""
+    return arbitrary_load_from_hf(
+        model_name_or_path,
+        AutoProcessor.from_pretrained,
+        revision=revision,
+        local_cache_dir=local_cache_dir,
+        trust_remote_code=trust_remote_code,
+    )
 
 
 _sync_count = 0
