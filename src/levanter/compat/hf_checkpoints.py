cd70dce1aae859ecd01a6977b54cade7b04b7cb7
Helw150 <wbh230@nyu.edu>
2024-03-15 18:34:02 -0400

    Merge right object

--- src/levanter/compat/hf_checkpoints.py
+++ src/levanter/compat/hf_checkpoints.py
@@ -682,11 +682,11 @@ class HFCheckpointConverter(Generic[LevConfig]):
                 tokenizer_dependent_config["decoder_start_token_id"] = self.tokenizer.bos_token_id
                 suppress_tokens.append(self.tokenizer.bos_token_id)
             if len(suppress_tokens) > 0:
-                tokenizer_dependent_config["begin_suppress_tokens"] = suppress_tokens
+                tokenizer_dependent_config["begin_suppress_tokens"] = set(suppress_tokens)
             dict_config = mergedeep.merge(
                 {},
                 dict_config,
-                suppress_tokens,
+                tokenizer_dependent_config,
             )
 
         if self.config_overrides:
