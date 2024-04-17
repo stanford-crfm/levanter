8a9fb99d4defdc1a6a5ca5d14eab56db940022c6
David Hall <dlwh@stanford.edu>
2024-04-16 21:19:37 -0700

    fix examples

--- src/levanter/main/train_lm.py
+++ src/levanter/main/train_lm.py
@@ -145,7 +145,10 @@ def main(config: TrainLmConfig):
                 (CausalLmDataset(ds, Pos, KeyPos, ignore_index=config.data.ignore_token_id), tags)
                 for ds, tags in tagged_eval_datasets
             ]
-            max_eval_examples_per_ds = config.trainer.max_eval_batches * config.trainer.eval_batch_size
+            max_eval_examples_per_ds = config.trainer.max_eval_batches
+            if max_eval_examples_per_ds is not None:
+                max_eval_examples_per_ds *= config.trainer.eval_batch_size
+
             cb = levanter.eval.cb_tagged_lm_evaluate(
                 EvalBatch, causal_datasets, trainer.device_mesh, compute_axis_mapping, max_eval_examples_per_ds
             )
