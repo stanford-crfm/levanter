9f9b447975b43c548dcef2b3a07115c427a341ba
David Hall <dlwh@stanford.edu>
2024-04-16 20:58:05 -0700

    dadaadadadad

--- src/levanter/eval.py
+++ src/levanter/eval.py
@@ -208,7 +208,7 @@ class TaggedEvaluator:
 
         micro_avg_loss = total_loss.mean.item()
         tag_avg_loss = losses_per_tag.mean
-        print(tag_avg_loss.sharding, flush=True)  # type: ignore
+        print(tag_avg_loss.array.sharding, flush=True)  # type: ignore
         tag_avg_loss = hax.shard(tag_avg_loss, self.loader.axis_resources)
         macro_avg_loss = hax.mean(tag_avg_loss).item()
 
