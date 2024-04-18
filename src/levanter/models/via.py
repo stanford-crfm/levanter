9bf4d5c57a03945a513980bdbaaae13be484e9cf
Helw150 <wbh230@nyu.edu>
2024-04-18 11:32:05 -0400

    STE

--- src/levanter/models/via.py
+++ src/levanter/models/via.py
@@ -167,6 +167,13 @@ class ViaModel(eqx.Module, ModelWithHfSerializationMixin[ViaConfig]):
     def query_position_embeds(self) -> NamedArray:
         return self.connector.embeddings.position_embeddings.embed(hax.arange(self.config.TimeGroup))
 
+    def straight_through_nn(self, x):
+        grad_store = x - jax.lax.stop_gradient(x)
+        lm_logits = hax.sum(x**2, axis="embed") - (2 * hax.dot("embed", self.decoder.embeddings.token_embeddings, x))
+        nearest_neighbor = jax.lax.stop_gradient(hax.argmax(lm_logits, axis=self.Vocab))
+        nearest_neighbor_embed = self.decoder.embeddings.embed(nearest_neighbor)
+        return grad_store + jax.lax.stop_gradient(nearest_neighbor_embed)
+
     def __call__(
         self,
         mel: NamedArray,
