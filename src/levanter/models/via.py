0ed3a0ed11a26a46ba675befd81b2e91bdb15897
Helw150 <wbh230@nyu.edu>
2024-04-23 14:47:30 -0400

    Shuffle

--- src/levanter/models/via.py
+++ src/levanter/models/via.py
@@ -109,7 +109,7 @@ def connector_only(model):
     return eqx.tree_at(
         lambda tree: (tree.query_tokens, tree.projection.weight, tree.projection.bias, tree.connector),
         frozen_tree,
-        (True, True, True, True),
+        (True, True, True, False),
     )
 
 
@@ -181,10 +181,15 @@ class ViaModel(eqx.Module, ModelWithHfSerializationMixin[ViaConfig]):
         audio_features = self.encoder(mel, key=k_encoder)
 
         # Convert to Virtual LLM Tokens
-        virt_whisper_tokens = self.connector.transformer(
-            (self.query_tokens + self.query_position_embeds).broadcast_axis(OtherAxes),
-            audio_features,
-            key=k_connector,
+        virt_whisper_tokens = hax.roll(
+            self.connector.transformer(
+                (self.query_tokens + self.query_position_embeds).broadcast_axis(OtherAxes),
+                audio_features,
+                causal_mask,
+                key=k_connector,
+            ),
+            -12,
+            self.Pos,
         )
 
         virtual_tokens = self.projection(virt_whisper_tokens.rename({"embed": "whisp_embed"}))
