cdcd9689c0d15b64f04e9d06bf976f8ab35f32ad
Helw150 <wbh230@nyu.edu>
2024-04-24 03:24:45 -0400

    Wrong sort order and off by one

--- src/levanter/models/via.py
+++ src/levanter/models/via.py
@@ -176,7 +176,7 @@ class ViaModel(eqx.Module, ModelWithHfSerializationMixin[ViaConfig]):
             "position",
             [
                 prefix.broadcast_axis(OtherAxes),
-                virtual_tokens["position", -10:],
+                virtual_tokens,
                 suffix.broadcast_axis(OtherAxes),
             ],
         )
@@ -197,6 +197,12 @@ class ViaModel(eqx.Module, ModelWithHfSerializationMixin[ViaConfig]):
 
         text_embeds = self.decoder.embeddings.embed(text_tokens)
         # Create LLM Response
+        assert text[
+            {
+                "batch": hax.arange(text_tokens.resolve_axis("batch")),
+                "position": (hax.sum(text_tokens == pad_token_id, "position") * -1) - 1,
+            }
+        ] == suffix["position", -1].broadcast_axis(OtherAxes)
         audio = self.decoder.transformer(audio_embeds, attn_mask=causal_mask, key=k_decoder)
         text = self.decoder.transformer(text_embeds, attn_mask=causal_mask, key=k_decoder)
         return (
