ae6c90c52ef5f7139512ca087a4d9853cce3b8d2
Jason Wang <blahblahj.wsy@gmail.com>
2024-05-01 15:40:29 -0700

    register sfadam

--- src/levanter/optim/__init__.py
+++ src/levanter/optim/__init__.py
@@ -1,8 +1,9 @@
 from .config import AdamConfig, OptimizerConfig
+from .schedulefree_adam import ScheduleFreeAdamConfig
+from .schedulefree_sophia import ScheduleFreeSophiaHConfig
 from .sophia import (  # SophiaGConfig,; SophiaGObjective,
     ScaleBySophiaState,
     SophiaHConfig,
     scale_by_sophia_g,
     scale_by_sophia_h,
 )
-from .schedulefree_sophia import ScheduleFreeSophiaHConfig
