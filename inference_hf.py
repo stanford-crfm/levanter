2db39bc210f3eba8d9f97efc8acdd7a636b1c0d0
Ivan-Zhou <ivanzhouyq@gmail.com>
2024-05-13 20:22:39 -0700

    update config

--- inference_hf.py
+++ inference_hf.py
@@ -10,6 +10,6 @@ tokenizer = AutoTokenizer.from_pretrained(path)
 
 prompt = "Hey, are you conscious? Can you talk to me?"
 inputs = tokenizer(prompt, return_tensors="pt")
-generate_ids = model.generate(inputs.input_ids, max_length=30)
+generate_ids = model.generate(inputs.input_ids, max_length=100)
 output = tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
 print(output)
