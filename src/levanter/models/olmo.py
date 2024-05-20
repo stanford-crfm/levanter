f999ee4ec1e04e34802cbd2d4c7b424d353cd069
Ahmed Ahmed <ahmedah@stanford.edu>
2024-05-20 15:28:37 -0700

    fix config olmo

--- src/levanter/models/olmo.py
+++ src/levanter/models/olmo.py
@@ -92,6 +92,7 @@ class OlmoConfig(HFCompatConfig):
         self.num_kv_heads = 32
         self.num_heads = 32
         print(f"Worker: {worker_id}")
+        print(f" {dir(self)}")
         print(f"\n num_heads={self.num_heads},\n num_kv_heads={self.num_kv_heads}\n")
         assert (
             self.num_heads % self.num_kv_heads == 0
