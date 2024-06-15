b8fa3aa26a44d5c81320546c6c9c776ebfc5e095
Jason Wang <blahblahj.wsy@gmail.com>
2024-06-15 15:56:44 -0700

    refactor queued-resources

--- infra/helpers/cli.py
+++ infra/helpers/cli.py
@@ -12,6 +12,11 @@ def run_command(*args, **kwargs):
     return subprocess.check_call(args, **kwargs)
 
 
+def run_command_parallel(*args, **kwargs):
+    print("Running:", " ".join(list(args)))
+    return subprocess.Popen(args, **kwargs)
+
+
 def add_ssh_key(ssh_key_filename):
     # format 3072 SHA256:... key-name (RSA)
     key_hash = subprocess.check_output(["ssh-keygen", "-lf", ssh_key_filename]).decode("utf-8").split()[1]
@@ -23,9 +28,12 @@ def add_ssh_key(ssh_key_filename):
     subprocess.check_call(["ssh-add", ssh_key_filename])
 
 
-def tpu_ssh(tpu_name, zone, *args, ignore_failure=False):
+def tpu_ssh(tpu_name, zone, node_count, *args, ignore_failure=False):
     add_ssh_key(os.path.expanduser("~/.ssh/google_compute_engine"))
     try:
+        if node_count > 1:
+            return _tpu_ssh_multislice(tpu_name, zone, node_count, *args, ignore_failure)
+
         return run_command(
             "gcloud",
             "alpha",
@@ -43,6 +51,28 @@ def tpu_ssh(tpu_name, zone, *args, ignore_failure=False):
             print("Ignoring failure:", e)
         else:
             raise
+
+
+def _tpu_ssh_multislice(tpu_name, zone, node_count, *args, ignore_failure=False):
+    processes = []
+    for i in range(node_count):
+        p = run_command_parallel(
+            "gcloud",
+            "alpha",
+            "compute",
+            "tpus",
+            "queued-resources",
+            "ssh",
+            tpu_name,
+            "--worker=all",
+            f"--node={i}",
+            f"--zone={zone}",
+            "--command=%s" % " ".join(args),
+        )
+        processes.append(p)
+    for p in processes:
+        if p.wait() != 0:
+            raise subprocess.CalledProcessError(p.returncode)
 
 
 # Oddly enough, there's no API to simply fetch the current gcloud configuration...
