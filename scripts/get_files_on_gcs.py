fa9fd25211082265be3323050e760a6636577767
Ivan Zhou <ivan.zhouyq@gmail.com>
2024-06-25 02:50:23 +0000

    Add llama fineweb yaml

---
+++ scripts/get_files_on_gcs.py
@@ -0,0 +1,95 @@
+from google.cloud import storage
+from tqdm import tqdm
+import re
+import os
+from braceexpand import braceexpand
+
+
+def get_subdirectories(bucket_name, directory_path, suffix) -> set:
+    """Given a GCS bucket name, directory path and suffix, list all the subdirectories that contain files with the given suffix."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+
+    subdirectories = set()
+    blobs = bucket.list_blobs(prefix=directory_path)
+    for blob in blobs:
+        if blob.name.endswith(suffix):
+            subdirectory = os.path.dirname(blob.name)
+            if subdirectory not in subdirectories:
+                subdirectories.add(subdirectory)
+                yield subdirectory
+
+
+def list_files_in_subdirectory(bucket_name, subdirectory, suffix):
+    """Given a GCS bucket name, subdirectory and suffix, list all the files in the subdirectory with the given suffix.
+    And generate braceexpand paths for the files with the same prefix and suffix.
+    Note that we intentionally limit this to subdirectories in order to generate braceexpand paths for the files in the same subdirectory.
+    """
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs(bucket_name, prefix=subdirectory)
+
+    paths = []
+    for blob in blobs:
+        if blob.name.endswith(suffix):
+            paths.append(f"gs://{bucket_name}/{blob.name}")
+
+    ranges = find_number_ranges(paths)
+    braceexpand_paths = []
+    for start, end in ranges:
+        path = os.path.join(f"gs://{bucket_name}", subdirectory, f"{{{start}..{end}}}{suffix}")
+        braceexpand_paths.append(path)
+    validate_with_braceexpand(braceexpand_paths, paths)
+    return braceexpand_paths
+
+
+def find_number_ranges(file_paths):
+    numbers = []
+    for file_path in file_paths:
+        file_name = os.path.basename(file_path)
+        match = re.search(r"^(\d+)_", file_name)
+        if match:
+            numbers.append(int(match.group(1)))
+
+    numbers.sort()
+    ranges = []
+    start = end = numbers[0]
+
+    for i in range(1, len(numbers)):
+        if numbers[i] == end + 1:
+            end = numbers[i]
+        else:
+            ranges.append((start, end))
+            start = end = numbers[i]
+
+    ranges.append((start, end))
+    return ranges
+
+
+def validate_with_braceexpand(braceexpand_paths, paths):
+    be_paths = []
+    for be_path in braceexpand_paths:
+        be_paths.extend(list(braceexpand(be_path)))
+
+    if set(be_paths) != set(paths):
+        print("Braceexpand paths are not equal to the original paths")
+        print("Braceexpand paths:")
+        print(be_paths)
+        print("Original paths:")
+        print(paths)
+
+
+def main(bucket_name = "marin-data", directory = "processed/fineweb/fw-v1.0/", suffix = "_processed_html.jsonl.gz", limit=None):
+    counter = 0
+    subdirectories = get_subdirectories(bucket_name, directory, suffix)
+    for subdirectory in subdirectories:
+        for braceexpand_path in list_files_in_subdirectory(bucket_name, subdirectory, suffix):
+            print(f"- {braceexpand_path}")
+            counter += 1
+            if limit and counter >= limit:
+                return
+
+
+if __name__ == "__main__":
+    # suffix = "_processed_html.jsonl.gz"
+    suffix = "_processed_md.jsonl.gz"
+    main(suffix=suffix, limit=10)
