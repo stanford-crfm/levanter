88ddc346fb26c827f4e3c09d0dcf76256f0c51d1
David Hall <dlwh@stanford.edu>
2024-07-11 13:25:41 -0700

    work on core representation

--- src/levanter/newdata/core.py
+++ src/levanter/newdata/core.py
@@ -1,43 +1,67 @@
-from typing import Optional, Protocol, TypeVar
+from typing import Optional, Protocol, Sequence, TypeVar
 
-T = TypeVar("T")
+
+T = TypeVar("T", covariant=True)
 
 # We take a lot of inspiration from PyGrain
 # https://github.com/google/grain/blob/main/docs/samplers.md
 #
-# Our approach is similar but with a key difference: We require that samplers be random access.
-
-# TODO: figure out "stateless shuffle buffer"
-# TODO: figure out a simpler prefix sampler
+# Those docs seem to be quite stale: samplers are actually random access, not just sequential.
+# We also require random access samplers. PyGrain seems to allow for stateful samplers, but we don't.
 
 
 class Sampler(Protocol):
     """
-    A source of indices for indexing into a dataset. In general, we strive for these to be stateless.
+    A source of indices for indexing into a dataset. For now we require these to be stateless.
+    We may add stateful support in the future.
 
-    Conceptually, a Sampler is something like a permutation, but it also allows for randomness to be injected
-    if preferred.
+    Conceptually, a Sampler is something like a permutation.
 
+    Some samplers might be infinite (e.g. the identity sampler or the era sampler)
     """
 
-    def __call__(self, item_index: int, *, key) -> int:
+    # TODO: maybe support slices/sequences here?
+    def __call__(self, idx: int) -> int:
         pass
 
     def __len__(self) -> int:
+        """Returns the length of the sampler. May raise if the length is not known."""
         pass
 
-    def has_len(self) -> bool:
+    def has_known_len(self) -> bool:
         pass
 
 
-class Dataset(Protocol[T]):
+class DataSet(Protocol[T]):
     def __len__(self) -> int:
+        """
+        Returns the final length of the data store.
+        May raise if the length is not known.
+        """
         pass
 
     def has_len(self) -> bool:
+        """
+        Whether the data store currently has a known length. If this returns False, then the length of the data store
+        may change in the future.
+        """
+        pass
+
+    def will_have_len(self) -> bool:
+        """
+        Whether the data store will have a known length in the future. If this returns False, then the length of the
+        data store is infinite or unknowable.
+        """
+        pass
+
+    def current_len(self) -> Optional[int]:
+        """
+        Returns the current length of the data store. If the length is infinite or not known, returns None.
+        """
         pass
 
     def __getitem__(self, index: int) -> T:
+        return self.get_batch([index])[0]
+
+    def get_batch(self, indices: Sequence[int]) -> Sequence[T]:
         pass
-
-
--- src/levanter/newdata/sampler.py
+++ src/levanter/newdata/sampler.py
@@ -1,45 +1,51 @@
-from typing import Optional
+import functools
 
-import jax
 from jax import random as jrandom
 
 from levanter.newdata.core import Sampler
 from levanter.newdata.prp import PseudoRandomPermutation
 
 
-class IdentitySampler(Sampler):
-    def __call__(self, item_index: int, *, key) -> int:
-        return item_index
+# TODO: figure out "stateless shuffle buffer"
+# TODO: figure out a simpler prefix sampler
 
-    def has_len(self) -> bool:
+
+class IdentitySampler(Sampler):
+    def __call__(self, idx: int) -> int:
+
+        return idx
+
+    def has_known_len(self) -> bool:
         return False
 
+    def __len__(self) -> int:
+        raise NotImplementedError("IdentitySampler does not support __len__")
 
 
 class PermutationSampler(Sampler):
-    def __init__(self, length: int, *, allow_epochs: bool = True, key: jrandom.PRNKey):
+    def __init__(self, length: int, *, allow_epochs: bool = True, key: jrandom.PRNGKey):
         self.length = length
         self.allow_epochs = allow_epochs
-        self.permutations = [PseudoRandomPermutation(length, key)]
-        self.key = key
 
-    def __call__(self, item_index: int, *, key) -> int:
-        del key
+        @functools.lru_cache(maxsize=4)  # we're mostly going to be going sequentially, so even 4 is overkill
+        def gen_epoch_permutation(epoch: int) -> PseudoRandomPermutation:
+            mix_key = jrandom.fold_in(key, epoch)
+            return PseudoRandomPermutation(length, mix_key)
 
-        if not self.allow_epochs and item_index >= self.length:
-            raise ValueError(f"item_index {item_index} is out of bounds for length {self.length}")
+        self.gen_epoch_permutation = gen_epoch_permutation
 
-        if item_index < 0:
-            raise ValueError(f"item_index {item_index} is out of bounds for length {self.length}")
+    def __call__(self, idx: int) -> int:
+        if not self.allow_epochs and idx >= self.length:
+            raise ValueError(f"item_index {idx} is out of bounds for length {self.length}")
 
-        epoch = item_index // self.length
-        while epoch >= len(self.permutations):
-            perm_key, self.key = jrandom.split(self.key)
-            self.permutations.append(PseudoRandomPermutation(self.length, perm_key))
+        if idx < 0:
+            raise ValueError(f"item_index {idx} is out of bounds for length {self.length}")
 
-        permutation = self.permutations[epoch]
+        epoch = idx // self.length
 
-        return permutation(item_index)
+        permutation = self.gen_epoch_permutation(epoch)
+
+        return permutation(idx)
 
     def __len__(self) -> int:
         return self.length
@@ -48,24 +54,38 @@ class PermutationSampler(Sampler):
         return True
 
 
-#
-# class EraLengthPolicy:
-#     untils: list[int]
-#     lengths: list[int]
-#
+# era sampling is a decent solution to the "stateless shuffle buffer" problem
+# TODO: but i think we might like to let this grow the era length over time?
+class EraPermutationSampler(Sampler):
+    """
+    A sampler that divides the stream into "eras" of (currently) fixed length. Within each era, the indices are
+    permuted. After the era is exhausted, a new permutation is generated.
 
-# class EraPermutationSampler(Sampler):
-#     """
-#     A sampler that divides the stream into "eras" of (currently) fixed length. Within each era, the indices are
-#     permuted. After the era is exhausted, a new permutation is generated.
-#     """
-#     # TODO: add an era policy
-#
-#
-#     def __init__(self, era_length: int, *, key: jrandom.PRNKey):
-#         self.era_length = era_length
-#         self.permutations = [PseudoRandomPermutation(era_length, key)]
-#         self.key = key
-#
-#
-#     def __call__(self, item_index: int, *, key) -> int:
+    Note that this sampler has no notion of "epoch" so you have to munge the returned index
+    """
+
+    def __init__(self, era_length: int, *, key: jrandom.PRNGKey):
+        self.era_length = era_length
+        self.key = key
+
+        @functools.lru_cache(maxsize=4)  # we're mostly going to be going sequentially
+        def gen_era_permutation(era: int) -> PseudoRandomPermutation:
+            mix_key = jrandom.fold_in(key, era)
+            return PseudoRandomPermutation(era_length, mix_key)
+
+        self.gen_era_permutation = gen_era_permutation
+
+    def __call__(self, idx: int) -> int:
+        if idx < 0:
+            raise ValueError(f"item_index {idx} is out of bounds for era length {self.era_length}")
+
+        era = idx // self.era_length
+        permutation = self.gen_era_permutation(era)
+
+        return permutation(idx) + era * self.era_length
+
+    def __len__(self) -> int:
+        raise NotImplementedError("EraPermutationSampler does not support __len__")
+
+    def has_known_len(self) -> bool:
+        return False
