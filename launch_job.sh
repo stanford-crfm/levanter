2caf00b32b91957b662f74da81b1cbe7c338fd5e
Ivan Zhou <ivan.zhouyq@gmail.com>
2024-08-14 04:06:23 +0000

    add launch script

---
+++ launch_job.sh
@@ -0,0 +1,16 @@
+EXP_NAME="ivan-ray-multislice"
+TPU_TYPE="v4-128"
+NODE_COUNT=2
+TPU_ZONE="us-central2-b"
+
+DATE=$(TZ='America/Los_Angeles' date +%m%d)
+UNIQUE_ID=$(uuidgen | md5sum | head -c 8)
+TPU_NAME="$EXP_NAME-$UNIQUE_ID-$TPU_ZONE-$TPU_TYPE"
+RUN_ID="$EXP_NAME-$UNIQUE_ID-$TPU_ZONE-$TPU_TYPE-$DATE"
+RETRIES=100
+
+CMD="python ray_scripts/ray_tpu_actor.py"
+echo $CMD
+python infra/launch.py --retries=$RETRIES --foreground \
+    --tpu_name $TPU_NAME --tpu_type $TPU_TYPE --zone $TPU_ZONE --node_count $NODE_COUNT \
+    --run_id $RUN_ID -- $CMD
