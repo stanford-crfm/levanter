b3fd65cd1e7bafacc3e2db3b3785c05ba3402630
Ivan-Zhou <ivanzhouyq@gmail.com>
2024-08-25 15:33:48 -0700

    including api in the launch command

--- execute_submit_ray_job.sh
+++ execute_submit_ray_job.sh
@@ -1,5 +1,7 @@
 ray job submit --address http://127.0.0.1:8265 --working-dir . \
     --runtime-env ray_runtime_env.yaml \
-    --entrypoint-num-cpus 7168 \
+    --entrypoint-num-cpus 448 \
     --no-wait \
-    -- python -m levanter.main.train_lm --config_path config/gpt2_nano.yaml
+    -- WANDB_API_KEY=be441272f3bd2812a2eb009739e26a202f14d7ba \
+    WANDB_PROJECT=marin \
+    python -m levanter.main.train_lm --config_path config/gpt2_nano.yaml
