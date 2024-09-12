da0fbcb813df5368fa7631362508bd10c8d0c1d3
David Hall <dlwh@cs.stanford.edu>
2024-09-11 20:46:17 -0700

    Fix eqx (#725)
    * don't hit ray if we don't need to in TreeCache
    
    * make ray exit quieter
    
    * reduce log spam of wandb
    
    * .aider ignore
    
    * sigh
    
    * make BackgroundIterable work not in the background
    
    * fix regression caused by new Equinox

--- src/levanter/store/cache.py
+++ src/levanter/store/cache.py
@@ -54,7 +54,8 @@ DEFAULT_LOG_LEVEL = pylogging.INFO
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 
 # TODO: should probably do this in terms of bytes
-MIN_ITEMS_TO_WRITE = 8192
+# this is kinda silly, but the bigger the better.
+MIN_ITEMS_TO_WRITE = 32 * 1024
 MAX_TIME_BETWEEN_WRITES = 100.0
 
 
@@ -883,6 +884,7 @@ class TreeCache(AsyncDataset[T_co]):
         self.logger = pylogging.getLogger(f"TreeCache.{name}")
         self._store_future: threading_Future[TreeStore] = threading_Future()
         self._stop = False
+        # assert _broker is None
 
         if self._broker is not None:
             self._monitor_thread = threading.Thread(target=self._monitor_metrics, daemon=True)
@@ -1078,11 +1080,15 @@ class TreeCache(AsyncDataset[T_co]):
         return start, step, stop
 
     def await_finished(self, timeout: Optional[float] = None):
+        if self._broker is None:
+            return
         x = ray.get(self.finished_sentinel(), timeout=timeout)
         self._attempt_to_load_store()
         return x
 
     async def finished(self):
+        if self._broker is None:
+            return
         x = await self.finished_sentinel()
         # TODO: make an async version of this
         self._attempt_to_load_store()
