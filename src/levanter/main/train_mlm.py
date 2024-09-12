5b97400def34e0e4631322d33998167f50261bbc
JulienDarve <jdarve@stanford.edu>
2024-09-11 20:43:55 -0700

    update 2

--- src/levanter/main/train_mlm.py
+++ src/levanter/main/train_mlm.py
@@ -83,7 +83,7 @@ def main(config: TrainMlmConfig):
 
     levanter.initialize(config)
     optimizer = config.optimizer.build(config.trainer.num_train_steps)
-    loss_function = functools.partial(compute_next_token_loss, logsumexp_weight=config.z_loss_weight)
+    loss_function = functools.partial(compute_next_token_loss)
 
     # Using the trainer as a context manager does 3 things:
     # 1. Sets the device mesh
