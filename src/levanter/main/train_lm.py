d92034558a8ef812a9be9043aaa5417b6775fd78
Ahmed Ahmed <ahmedah@stanford.edu>
2024-09-25 12:03:59 -0700

    epoch fix

--- src/levanter/main/train_lm.py
+++ src/levanter/main/train_lm.py
@@ -215,7 +215,7 @@ def main(config: TrainLmConfig):
             train_loader = iter(train_loader)
 
         ## OK, actually run training!
-        trainer.train(state, train_loader, num_epochs=3)
+        trainer.train(state, train_loader)
         # checkpointer.on_step(last_step, force=True)
 
 
