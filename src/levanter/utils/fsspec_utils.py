2f625d3c952eb3e933a9834b8beef3a9bf9aafc0
Ahmed Ahmed <ahmedah@stanford.edu>
2024-10-09 14:02:17 -0700

    address david's comments

--- src/levanter/utils/fsspec_utils.py
+++ src/levanter/utils/fsspec_utils.py
@@ -1,5 +1,5 @@
 import fsspec
-
+import braceexpand
 
 def exists(url, **kwargs) -> bool:
     """Check if a file exists on a remote filesystem."""
@@ -11,3 +11,18 @@ def mkdirs(path):
     """Create a directory and any necessary parent directories."""
     fs, path = fsspec.core.url_to_fs(path)
     fs.makedirs(path, exist_ok=True)
+
+def fsspec_expand_glob(url):
+    expanded_urls = braceexpand.braceexpand(url)
+    for expanded_url in expanded_urls:
+        if "*" in expanded_url:
+            fs = fsspec.core.url_to_fs(expanded_url)[0]
+            globbed = fs.glob(expanded_url)
+            # have to append the fs prefix back on
+            protocol, _ = fsspec.core.split_protocol(expanded_url)
+            if protocol is None:
+                yield from globbed
+            else:
+                yield from [f"{protocol}://{path}" for path in globbed]
+        else:
+            yield expanded_url
