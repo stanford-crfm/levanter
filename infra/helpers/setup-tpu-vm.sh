6bb1bba9be7961d7d086137094c17c7b6dbe769c
Ahmed Ahmed <ahmedah@stanford.edu>
2024-10-15 20:34:30 -0700

    fix

--- infra/helpers/setup-tpu-vm.sh
+++ infra/helpers/setup-tpu-vm.sh
@@ -8,7 +8,7 @@ if [ "$DEBUG" == "1" ]; then
 fi
 
 REPO="https://github.com/stanford-crfm/levanter.git"
-BRANCH=prefetch_actor_tokenizer
+BRANCH=main
 
 if [ "$GIT_BRANCH" != "" ]; then
   BRANCH="$GIT_BRANCH"
