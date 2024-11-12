b49e69d6cabef5cb5afb8b8761a1ffb081bdbba1
David Hall <dlwh@stanford.edu>
2024-11-12 10:37:15 -0800

    tracker.finish to deal with subprocess stuff

--- src/levanter/tracker/wandb.py
+++ src/levanter/tracker/wandb.py
@@ -72,6 +72,10 @@ class WandbTracker(Tracker):
     def log_artifact(self, artifact_path, *, name: Optional[str] = None, type: Optional[str] = None):
         self.run.log_artifact(artifact_path, name=name, type=type)
 
+    def finish(self):
+        logger.info("Finishing wandb run...")
+        self.run.finish()
+
 
 def is_wandb_available():
     try:
