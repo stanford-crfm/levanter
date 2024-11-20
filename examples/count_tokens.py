02fd529725927c5537d1cecdfde8af7a0ca64446
Ahmed Ahmed <ahmedah@stanford.edu>
2024-11-20 14:00:27 -0800

    WIP

--- examples/count_tokens.py
+++ examples/count_tokens.py
@@ -3,3 +3,5 @@ from levanter.store import JaggedArrayStore
 a = JaggedArrayStore.open("gs://marin-us-central2/tokenized/dolma/algebraic-stack-cc00cf/train/input_ids", dtype=int)
 
 a.data_size
+
+150,849,275
