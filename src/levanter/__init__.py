e2f430c15e4ae4d0635b82590398b417443031a7
Nikil Ravi <nravi@stanford.edu>
2024-12-05 13:48:56 -0800

    make eval_harness part of levanter namespace

--- src/levanter/__init__.py
+++ src/levanter/__init__.py
@@ -3,6 +3,7 @@ import levanter.config as config
 import levanter.data as data
 import levanter.distributed as distributed
 import levanter.eval as eval
+import levanter.eval_harness as eval_harness
 import levanter.models as models
 import levanter.optim as optim
 import levanter.tracker as tracker
