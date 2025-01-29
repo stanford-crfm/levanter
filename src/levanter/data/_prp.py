refactor: Switch permutation to numpy for int64 and overflow fix

src/levanter/data/_prp.py
@@ -21,40 +21,27 @@ class Permutation:
 
     def __init__(self, length, prng_key):
         self.length = length
-        self.prng_key = prng_key
-        a_key, b_key = jrandom.split(prng_key)
-        self._a = jrandom.randint(a_key, (), 1, length)
-        self._b = jrandom.randint(b_key, (), 0, length)
+        # Convert jax.random.PRNGKey to numpy.random.Generator
+        self.rng = np.random.Generator(np.random.PCG64(jax.random.randint(prng_key, (), 0, 2**32).item()))
+        self._permutation = None # permutation is generated on demand
 
-        cond = lambda a_and_key: jnp.all(jnp.gcd(a_and_key[0], length) != 1)
-
-        def loop_body(a_and_key):
-            a, key = a_and_key
-            this_key, key = jrandom.split(key)
-            a = jrandom.randint(this_key, (), 1, length)
-            return a, key
-
-        self._a, key = jax.lax.while_loop(cond, loop_body, (self._a, a_key))
-
-        self._a = int(self._a)
-        self._b = int(self._b)
+    def _generate_permutation(self):
+        if self._permutation is None:
+            self._permutation = self.rng.permutation(self.length).astype(np.int64)
 
     @typing.overload
     def __call__(self, indices: int) -> int:
         ...
 
     @typing.overload
-    def __call__(self, indices: jnp.ndarray) -> jnp.ndarray:
+    def __call__(self, indices: np.ndarray) -> np.ndarray:
         ...
 
     def __call__(self, indices):
+        self._generate_permutation() # generate permutation on first call
+
         was_int = False
-        if isinstance(indices, jnp.ndarray):
-            # TODO: use error_if?
-            # import equinox as eqx
-            if jnp.any(indices < 0) or jnp.any(indices >= self.length):
-                raise IndexError(f"index {indices} is out of bounds for length {self.length}")
-        elif isinstance(indices, np.ndarray):
+        if isinstance(indices, np.ndarray):
             if np.any(indices < 0) or np.any(indices >= self.length):
                 raise IndexError(f"index {indices} is out of bounds for length {self.length}")
         else:
@@ -64,9 +51,7 @@ class Permutation:
             indices = np.array(indices)
             was_int = True
 
-        old_settings = np.seterr(over="raise")
-        out = (self._a * indices + self._b) % self.length
-        np.seterr(**old_settings)
+        out = self._permutation[indices]
 
         if was_int:
             return int(out)

src/levanter/data/permutation.py
@@ -1,6 +1,7 @@
 from typing import Optional, Sequence
 
 import jax.random
+import numpy as np
 from async_lru import alru_cache
 
 from levanter.data import AsyncDataset
@@ -41,7 +42,7 @@ class PermutationDataset(AsyncDataset[T_co]):
 
     async def get_batch(self, indices: Sequence[int]) -> Sequence[T_co]:
         permutation = await self._get_permutation()
-        return await self.dataset.get_batch([permutation(i) for i in indices])
+        return await self.dataset.get_batch([int(permutation(i)) for i in indices]) # cast to int to be sure it's python int
 
     async def _get_permutation(self):
         if self._permutation is None:
@@ -83,10 +84,11 @@ class EraShufflingDataset(AsyncDataset[T_co]):
             # TODO: support epochs
             # edge case: final era may be shorter than era_length
             current_len = await self.dataset.wait_until_len_at_least((era + 1) * self.era_length)
-            era_length = min(self.era_length, current_len - era * self.era_length)
+            era_length_val = min(self.era_length, current_len - era * self.era_length)
 
-            mix_key = jax.random.fold_in(key, era)
-            return Permutation(era_length, mix_key)
+            numpy_key = np.random.Generator(np.random.PCG64(jax.random.fold_in(key, era).item()))
+            mix_key = numpy_key
+            return Permutation(era_length_val, mix_key)
 
         self.gen_era_permutation = gen_era_permutation
 