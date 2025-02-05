a9d911f4981ee8b9c150e912059df9b6dd05068c
David Hall <dlwh@cs.stanford.edu>
2025-02-05 13:43:08 -0800

    add support for BatchSchedules (#880)

--- src/levanter/data/loader.py
+++ src/levanter/data/loader.py
@@ -1,7 +1,9 @@
 import logging
 import time
 from collections import defaultdict
-from typing import AsyncIterator, Callable, Iterable, Iterator, Optional, Tuple, TypeVar
+from dataclasses import dataclass
+from functools import lru_cache
+from typing import AsyncIterator, Callable, Generic, Iterable, Iterator, Optional, Tuple, TypeVar
 
 import equinox
 import jax
@@ -18,7 +20,7 @@ from haliax._src.util import index_where
 from haliax.partitioning import ResourceMapping
 
 from levanter.data.dataset import AsyncDataset
-from levanter.data.utils import batched
+from levanter.schedule import BatchSchedule, IntSchedule
 from levanter.shapes import NamedShapeSpec, ShapeSpec, to_raw_shape
 from levanter.utils.background_iterable import BackgroundIterator
 from levanter.utils.jax_utils import local_cpu_mesh
@@ -31,29 +33,66 @@ _TensorSliceIndex = tuple[slice, ...]
 logger = logging.getLogger(__name__)
 
 
+def local_indices_for_bs_and_sharding(sharding: jax.sharding.Sharding, batch_size: int) -> dict[jax.Device, range]:
+    """Return the local indices for each device for a given batch size and sharding."""
+    local_indices_map = sharding.addressable_devices_indices_map((batch_size,))
+    # we just want all the indices
+    local_device_indices: dict[jax.Device, range] = {
+        device: range(*idx[0].indices(batch_size)) for device, idx in local_indices_map.items() if idx is not None
+    }
+    return local_device_indices
+
+
+# NOTE: In general there are a lot of different indices flying around. Here's a quick guide:
+# - `step` or `batch_number` or `bn` is the training step or batch number
+# - `global` indices refer to the index into the datastore
+# - `local` indices refer to the index into the batch. These start at 0 for every batch and go up to the batch size.
+
+# That is, for each batch, we have:
+# global_offset = global_offset_at_step(bn)
+# batch_size = batch_size_at_step(bn)
+# local_data_to_global_map = {i: global_offset + i for i in range(batch_size)}
+
+# But note that we only load the data we need for our local devices, so we only load a subset of the data for each batch.
+
+
+@dataclass
+class _Batch(Generic[Ex]):
+    index: int
+    global_data_offset: int
+    global_size: int
+    data_by_local_index: dict[int, Ex]
+
+
 class DataLoader(Iterable[Ex]):
     def __init__(
         self,
-        Batch: hax.Axis,
         data: AsyncDataset[Ex],
-        max_buffered_batches: Optional[int],
+        batch_size: int | IntSchedule | hax.Axis,
+        *,
+        batch_axis_name: str | None = None,
+        max_buffered_batches: Optional[int] = 64,
         mesh: Mesh,
         axis_resources: Optional[ResourceMapping],
-        # this is set heuristically for the typical tokenseqdataset we use. Should probably tune
         prefetch_size: int = 32,
     ):
         """
+        Batch- and NamedArray-aware data loader. This class works with an [AsyncDataset][], a Mesh,
+        and a resource mapping to load data in a way that is aware of the batch axis and the sharding
+        of the data. In general, each machine only loads the data that it needs to process.
 
-        TODO: document this
+        (At the moment, it's not smart enough to load parts of examples if examples are sharded across machines,
+        but whole examples are handled correctly.)
 
         Args:
-            Batch (hax.Axis): The batch axis
+            batch_size (int | IntSchedule | None): The size of the batch or a schedule for the size of the batch
             data (AsyncDataset[Ex]): The dataset to load from
             max_buffered_batches (Optional[int]): The maximum number of batches to buffer. If None, the buffer is unbounded.
              If <0, the buffer is disabled and single threaded operation is used.
             axis_resources (Optional[ResourceMapping]): axis mapping
             prefetch_size (int): The number of batches to prefetch at once
             mesh (Mesh): The mesh to use
+            batch_axis_name (str | None): The name of the batch axis. If None, defaults to "batch" unless batch_size is an Axis.
 
         """
         self.max_buffered_batches = max_buffered_batches
@@ -61,7 +100,16 @@ class DataLoader(Iterable[Ex]):
         self.axis_resources = axis_resources
         self.data_store = data
         self.mesh = mesh
-        self.Batch = Batch
+
+        if isinstance(batch_size, hax.Axis):
+            assert batch_axis_name is None
+            self.batch_axis_name = batch_size.name
+            self.scheduler = BatchSchedule(batch_size.size)
+        else:
+            self.batch_axis_name = batch_axis_name or "batch"
+            self.scheduler = BatchSchedule(batch_size)
+
+        self._batch_sharding = hax.partitioning.sharding_for_axis(self.batch_axis_name, axis_resources, mesh)
 
         with local_cpu_mesh():
             # It's important that all data loading happens CPU side. We might relax this one day.
@@ -69,32 +117,34 @@ class DataLoader(Iterable[Ex]):
                 blocking_wait(self.data_store.getitem_async(0)), is_leaf=is_named_array
             )
 
-        local_device_indices, local_indices = self._compute_local_device_indices()
+    def local_data_indices_by_device_for_step(self, step: int) -> dict[jax.Device, range]:
+        """
 
-        self._local_device_indices: dict[jax.Device, range] = local_device_indices
-        # this is just the flat indices
-        self._local_indices: list[int] = local_indices
+        Returns the 0-based indices of the data that should be loaded by each device for a given step.
 
-    def _compute_local_device_indices(self):
-        sharding: jax.sharding.Sharding = hax.partitioning.sharding_for_axis(
-            self.Batch.name, self.axis_resources, self.mesh
-        )
-        # this is a map from devices to the slice of the array that they contain (in the global array)
-        local_indices_map = sharding.addressable_devices_indices_map((self.batch_size,))
-        # we just want all the indices
-        local_device_indices: dict[jax.Device, range] = {
-            device1: range(*idx[0].indices(self.batch_size))
-            for device1, idx in local_indices_map.items()
-            if idx is not None
-        }
-        local_indices: list[int] = []
-        for device, indices in local_device_indices.items():
-            local_indices.extend(indices)
-        return local_device_indices, local_indices
+        That is, if we are on step i with a fixed batch size of b, then device d should load the data at indices
+        [step * bs + (d * local_bs), step * bs + ((d + 1) * local_bs) ) where local_bs is the number of data points
+        that device d should load.
+        """
+        batch_size = self.scheduler.batch_size_at_step(step)
+        local_indices = self.compute_local_device_indices_for_bs(batch_size)
 
-    @property
-    def batch_size(self):
-        return self.Batch.size
+        return local_indices
+
+    def global_data_indices_by_device_for_step(self, step: int) -> dict[jax.Device, range]:
+        local_indices = self.local_data_indices_by_device_for_step(step)
+        offset = self.scheduler.global_data_offset_by_step(step)
+
+        return {device: range(offset + r.start, offset + r.stop, r.step) for device, r in local_indices.items()}
+
+    def batch_axis_at_step(self, step: int):
+        size = self.scheduler.batch_size_at_step(step)
+
+        return hax.Axis(self.batch_axis_name, size)
+
+    @lru_cache
+    def compute_local_device_indices_for_bs(self, bs) -> dict[jax.Device, range]:
+        return local_indices_for_bs_and_sharding(self._batch_sharding, bs)
 
     def __iter__(self):
         return self.iter_from_step(None)
@@ -123,8 +173,7 @@ class DataLoaderIterator(Iterator[Ex]):
     def __next__(self):
         time_start = time.time()
         individual_data_batch = next(self._batches)
-        data_for_this_batch = {index: datum for index, datum in zip(self.dl._local_indices, individual_data_batch)}
-        batch = self._batchify_local_data(data_for_this_batch)
+        batch = self._batchify_local_data(individual_data_batch)
 
         time_end = time.time()
         if (time_end - time_start) > 0.5:
@@ -135,6 +184,7 @@ class DataLoaderIterator(Iterator[Ex]):
         batch_number = self._start_from_batch or 0
         done = False
         while not done:
+            # we try to prefetch multiple batches at a time
             target_next_batch_number = batch_number + self.dl.prefetch_size
             max_achievable_batch_number = await self._dataset_get_available_batch_number(target_next_batch_number)
             if max_achievable_batch_number < target_next_batch_number:
@@ -145,34 +195,44 @@ class DataLoaderIterator(Iterator[Ex]):
             if len(next_batch_numbers) == 0:
                 break
 
-            batch_number = next_batch_numbers[-1] + 1
+            time_start = time.time()
+            batch_of_batches: list[_Batch[Ex]] = await self._do_retrieve_batch_of_batches(next_batch_numbers)
+            time_end = time.time()
+            logger.debug(f"Time to get {len(next_batch_numbers)} batches: {time_end - time_start:.3f}")
 
-            async for batch in self._retrieve_batches(next_batch_numbers):
+            for batch in batch_of_batches:
                 yield batch
 
-    async def _dataset_get_available_batch_number(self, target_max_batch_number: int) -> int:
-        if self.dl.data_store.is_finite():
-            next_end = (target_max_batch_number + 1) * self.dl.batch_size
-            available_len = await self.dl.data_store.wait_until_len_at_least(next_end)
-            max_achievable_batch_number = available_len // self.dl.batch_size
+            batch_number = next_batch_numbers[-1] + 1
 
-            return max_achievable_batch_number
+    async def _dataset_get_available_batch_number(self, target_max_batch_number: int) -> int:
+        """
+        Wait until the data store has enough data to support the given batch number. If
+        the data store is finite, this will wait until the data store has at least `target_max_batch_number` batches
+        or until the data store is exhausted, in which case it will return the last batch number that the data store
+        has data for.
+        """
+        if self.dl.data_store.is_finite():
+            next_end = self.dl.scheduler.global_data_offset_by_step(target_max_batch_number)
+            available_len = await self.dl.data_store.wait_until_len_at_least(next_end)
+
+            while available_len < next_end:
+                # backoff until we find a batch size we can support
+                # TODO: we could be much smarter about this but unlikely to be a bottle neck
+                target_max_batch_number -= 1
+                next_end = self.dl.scheduler.global_data_offset_by_step(target_max_batch_number)
+                if target_max_batch_number < 0:
+                    raise ValueError("No data available")
 
         return target_max_batch_number
 
-    async def _retrieve_batches(self, batch_numbers: list[int]):
-        with local_cpu_mesh():
-            time_start = time.time()
-            individual_datums_for_each_batch = await self._do_retrieve_batch_of_batches(batch_numbers)
-            # reshape to be per batch
-            time_end = time.time()
-            logger.debug(f"Time to get {len(batch_numbers)} batches: {time_end - time_start:.3f}")
-
-            for data in individual_datums_for_each_batch:
-                yield data
-
-    def _batchify_local_data(self, data_for_this_batch: dict[int, Array]):
+    def _batchify_local_data(self, batch: _Batch[Ex]) -> Ex:
+        """
+        Stacks the individual examples (pytrees) into a single example (pytree) with the batch axis added
+        and creates a global array for each leaf of the example.
+        """
         cache: dict[tuple[int, int], list[Array | hax.NamedArray]] = {}
+        Batch = hax.Axis(self.dl.batch_axis_name, batch.global_size)
 
         def get_local_batch(begin: int, end: int) -> list:
             if (begin, end) in cache:
@@ -180,7 +240,9 @@ class DataLoaderIterator(Iterator[Ex]):
 
             # TODO: if we ever do "big data" (i.e. huge examples) we might want to be able to load part of an example
             # which will require support from the datastore (i.e. tensorstore)
-            device_batch = stack_tree(self.dl.Batch.name, [data_for_this_batch[i] for i in range(begin, end)])
+            device_batch = stack_tree(
+                self.dl.batch_axis_name, [batch.data_by_local_index[i] for i in range(begin, end)]
+            )
             batch_leaves = hax.tree_util.tree_leaves(device_batch)
 
             cache[(begin, end)] = batch_leaves
@@ -189,7 +251,7 @@ class DataLoaderIterator(Iterator[Ex]):
 
         def get_local_data_for_leaf(indices: _TensorSliceIndex, leaf_index: int) -> Array:
             batch_slice = indices[0]
-            begin, end, stride = batch_slice.indices(self.dl.batch_size)
+            begin, end, stride = batch_slice.indices(batch.global_size)
             if stride != 1:
                 raise ValueError("Stride must be 1")
 
@@ -197,7 +259,7 @@ class DataLoaderIterator(Iterator[Ex]):
 
             if isinstance(leaf_data, hax.NamedArray):
                 # select out the batch axis
-                batch_index = index_where(lambda ax: ax.name == self.dl.Batch.name, leaf_data.axes)
+                batch_index = index_where(lambda ax: ax.name == Batch.name, leaf_data.axes)
                 new_indices = list(indices)
                 new_indices[batch_index] = slice(None)
                 return leaf_data.array[tuple(new_indices)]
@@ -223,25 +285,64 @@ class DataLoaderIterator(Iterator[Ex]):
                 return raw_array
 
         gda_leaves = [
-            make_global_array_for_leaf(leaf_index, _batchified_shape(self.dl.Batch, item_leaf))
+            make_global_array_for_leaf(leaf_index, _batchified_shape(Batch, item_leaf))
             for leaf_index, item_leaf in enumerate(self.dl._ex_leaves)
         ]
         gda_tree = jax.tree.unflatten(self.dl._ex_structure, gda_leaves)
         return gda_tree
 
-    async def _do_retrieve_batch_of_batches(self, batch_numbers):
-        indices_for_this_batch_of_batches: list[int] = []
+    async def _do_retrieve_batch_of_batches(self, batch_numbers: list[int]) -> list[_Batch[Ex]]:
+        """
+        Retrieve the data for a batch of batches.
+
+        - For each batch number, we get the mapping from our local devices to the indices in the data store that we need
+          to load.
+        - We then load the data for all the devices for all the batch numbers.
+        - we then return each batch as a dictionary from device to the data for that device.
+        """
+        global_indices_for_each_batch: list[list[int]] = []
+        sizes = [self.dl.scheduler.batch_size_at_step(bn) for bn in batch_numbers]
+        offsets = []
+
         for bn in batch_numbers:
-            indices_this_batch = range(bn * self.dl.batch_size, (bn + 1) * self.dl.batch_size, 1)
-            indices_this_batch_this_process = [indices_this_batch[i] for i in self.dl._local_indices]
-            indices_for_this_batch_of_batches.extend(indices_this_batch_this_process)
+            global_offset = self.dl.scheduler.global_data_offset_by_step(bn)
+            offsets.append(global_offset)
+            local_indices_for_device = self.dl.local_data_indices_by_device_for_step(bn)
+
+            distinct_local_indices_this_batch = set()
+            for indices in local_indices_for_device.values():
+                for local_index in indices:
+                    distinct_local_indices_this_batch.add(local_index)
+
+            global_indices_for_this_batch = [global_offset + i for i in distinct_local_indices_this_batch]
+            global_indices_for_each_batch.append(global_indices_for_this_batch)
+
+        # flattened view so we can load all the data at once
+        indices_for_this_batch_of_batches: list[int] = [
+            i for indices in global_indices_for_each_batch for i in indices
+        ]
         individual_datums = await self.dl.data_store.get_batch(indices_for_this_batch_of_batches)
-        individual_datums_for_each_batch = list(batched(individual_datums, len(self.dl._local_indices)))
-        return individual_datums_for_each_batch
+
+        # unflatten
+        global_map: dict[int, Ex] = {}
+        for i, datum in enumerate(individual_datums):
+            global_map[indices_for_this_batch_of_batches[i]] = datum
+
+        out: list[_Batch[Ex]] = []
+
+        for bn, sz, offset, global_indices_batch in zip(batch_numbers, sizes, offsets, global_indices_for_each_batch):
+            local_index_to_example = {}
+            for global_index in global_indices_batch:
+                local_index = global_index - offset
+                local_index_to_example[local_index] = global_map[global_index]
+
+            out.append(_Batch(bn, offset, sz, local_index_to_example))
+
+        return out
 
     def _pspec_for(self, shape_spec: ShapeSpec | NamedShapeSpec) -> PartitionSpec:
         if isinstance(shape_spec, ShapeSpec):  # type: ignore
-            batch_name = hax.partitioning.physical_axis_name(self.dl.Batch, self.dl.axis_resources)
+            batch_name = hax.partitioning.physical_axis_name(self.dl.batch_axis_name, self.dl.axis_resources)
             return PartitionSpec(batch_name, *((None,) * (len(shape_spec.shape) - 1)))
         else:
             return hax.partitioning.pspec_for_axis(shape_spec.shape, self.dl.axis_resources)  # type: ignore
