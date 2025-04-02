16c19ddb9302cba6757e52161e4735936871c0f7
David Hall <dlwh@stanford.edu>
2025-04-02 12:13:42 -0700

    polyfill strenum, convert to enum correctly

--- src/levanter/utils/activation.py
+++ src/levanter/utils/activation.py
@@ -8,17 +8,25 @@ import haliax as hax
 import haliax.nn as hnn
 
 
+try:
+    from enum import StrEnum
+except ImportError:
+    # <= Python 3.10
+    from backports.strenum import StrEnum
+
+
 _A = typing.TypeVar("_A", hax.Scalar, hax.NamedArray, jax.Array)
 ActivationFunction = typing.Callable[[_A], _A]
 
 
-class ActivationFunctionEnum(enum.Enum):
+class ActivationFunctionEnum(StrEnum):
     relu = enum.auto()
     silu = enum.auto()
     swish = enum.auto()
     gelu = enum.auto()
     gelu_new = enum.auto()
     quick_gelu = enum.auto()
+    tanh = enum.auto()
 
     def to_fn(self) -> ActivationFunction:
         return TO_FN[self]
@@ -31,4 +39,5 @@ TO_FN: dict[ActivationFunctionEnum, ActivationFunction] = {
     ActivationFunctionEnum.gelu: partial(hnn.gelu, approximate=False),
     ActivationFunctionEnum.gelu_new: partial(hnn.gelu, approximate=True),
     ActivationFunctionEnum.quick_gelu: hnn.quick_gelu,
+    ActivationFunctionEnum.tanh: hax.tanh,
 }
