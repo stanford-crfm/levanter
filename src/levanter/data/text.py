619bc09337c3bc61e4fe106dcc45fe589785a3cc
David Hall <dlwh@stanford.edu>
2025-04-15 10:55:48 -0700

    unneeded function

--- src/levanter/data/text.py
+++ src/levanter/data/text.py
@@ -472,50 +472,6 @@ class BatchTokenizer(BatchProcessor[str, dict]):
         return 0
 
 
-def concatenate_and_group_texts(
-    encoding: BatchEncoding,
-    seq_len: int,
-    stride: Optional[int] = None,
-    drop_remainder: bool = True,
-    mask_stride_overlap=True,
-) -> Iterator[BatchEncoding]:
-    """Groups texts in a batch together. Typically, you'll want to use this with a fairly large
-    set of texts, e.g. 1000 docs.
-
-    You should set mask_stride_overlap to True and drop_remainder to False if you want to use this for test data
-
-    Args:
-        encoding: The batch of texts to concatenate and group.
-        seq_len: The max length of sequences to emit
-        stride: The stride to use when grouping texts. If None, then the stride is set to seq_len.
-        mask_stride_overlap: Whether to mask out overlapping tokens if we're using a stride.
-        drop_remainder: Whether to drop the last batch if it's not a multiple of the seq_len.
-
-    Returns:
-        An iterator of tokenized texts, one at a time.
-    """
-    concatenated = BatchEncoding(data={k: np.array(list(chain(*v))) for k, v in encoding.items()})
-    total_length = len(concatenated.input_ids)
-    stride = stride or seq_len
-
-    # Drop the "very last" bit of the dataset that doesn't fit into block size...
-    if drop_remainder and total_length % stride != 0:
-        total_length = ((total_length - seq_len + stride) // stride) * stride
-
-    # Split by Chunks of Maximum Length
-    # we want to take chunks up until we've covered all "total_length" tokens with a sliding window of size "stride"
-    for begin in range(0, total_length - seq_len + stride, stride):
-        data = {k: v[begin : begin + seq_len] for k, v in concatenated.items()}
-
-        if mask_stride_overlap and stride != seq_len:
-            labels = data.get("labels", data["input_ids"])
-            if begin != 0:
-                labels = _mask_overlap(labels, seq_len, stride)
-            data["labels"] = labels
-
-        yield BatchEncoding(data=data)
-
-
 # -100 is pytorch's label mask
 def _mask_overlap(labels, target_len, stride, sentinel=-100):
     """Masks out overlapping tokens in a sequence when we're using a stride."""
