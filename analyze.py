fac50551b7d155d94f0c319f3a919088f5d30bd7
Kaiyue Wen <kaiyue@sc.stanford.edu>
2025-05-25 16:21:20 -0700

    fix soap

--- analyze.py
+++
@@ -1,9 +0,0 @@
-with open("output.log", "r") as f:
-    lines = f.readlines()
-    new_lines = []
-    for line in lines:
-        if "cc" not in line:
-            new_lines.append(line)
-
-with open("output_new.log", "w") as f:
-    f.write("\n".join(new_lines))
