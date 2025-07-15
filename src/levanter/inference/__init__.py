8bae46c4a7166b008ba412f2357c42262eef32cd
David Hall <dlwh@stanford.edu>
2025-07-15 13:28:25 -0700

    Revert "Update JittedScheduler to avoid deprecated outfeed (#1062)"
    This reverts commit 7303f6f5c377a5b759628382c025acb0ed98d6e9.

--- src/levanter/inference/__init__.py
+++
@@ -1,5 +0,0 @@
-from .llm_engine import LLM, LLMEngine
-from .sampling_params import SamplingParams
-from .scheduler import JittedScheduler, Scheduler, run
-
-__all__ = ["LLM", "LLMEngine", "SamplingParams", "Scheduler", "JittedScheduler", "run"]
