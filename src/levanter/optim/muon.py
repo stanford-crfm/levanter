cce5da3862c783ac3cc8dac12c9c73d1ef4bb813
Pinlin [Calvin] Xu <calvinxu806@gmail.com>
2025-09-11 01:43:40 -0700

    format source

--- src/levanter/optim/muon.py
+++ src/levanter/optim/muon.py
@@ -52,7 +52,11 @@ class MuonConfig(OptimizerConfig):
         def optimizer(learning_rate, adam_lr):
             def muon_transform():
                 components = []
-                components.append(scale_with_muon(self.momentum, self.nesterov, self.backend_steps, self.muon_epsilon, self.use_kimi_scaling))
+                components.append(
+                    scale_with_muon(
+                        self.momentum, self.nesterov, self.backend_steps, self.muon_epsilon, self.use_kimi_scaling
+                    )
+                )
                 if self.weight_decay > 0:
                     components.append(optax.add_decayed_weights(self.weight_decay, self.build_weight_decay_mask()))
                 components.append(optax.scale(-learning_rate))
@@ -75,11 +79,13 @@ class MuonConfig(OptimizerConfig):
                 "adamw": adamw_transform(),
             }
 
-            return optax.multi_transform(transformations, partial(self.create_mask, use_kimi_scaling = self.use_kimi_scaling))
+            return optax.multi_transform(
+                transformations, partial(self.create_mask, use_kimi_scaling=self.use_kimi_scaling)
+            )
 
         return optax.inject_hyperparams(optimizer)(learning_rate=learning_rate_schedule, adam_lr=adam_lr_schedule)
 
-    def create_mask(self, params, use_kimi_scaling = True):
+    def create_mask(self, params, use_kimi_scaling=True):
         """
         Creates a mask that labels parameters as 'muon' or 'adamw' based on their
         dimensionality and module path, using AdamW for Embedding and lm_head parameters.
@@ -92,7 +98,9 @@ class MuonConfig(OptimizerConfig):
                 return "adamw"
             elif isinstance(param, Linear):
                 # muon for linear layers
-                assert (param._out_first or use_kimi_scaling) # if we don't use kimi's version of scaling, then we need to assume out_first to ensure we are scaling like Out/In
+                assert (
+                    param._out_first or use_kimi_scaling
+                )  # if we don't use kimi's version of scaling, then we need to assume out_first to ensure we are scaling like Out/In
                 return dataclasses.replace(param, weight="muon", bias="adamw" if param.bias is not None else None)
             else:
                 return "adamw"
@@ -139,7 +147,9 @@ def scale_with_muon(momentum=0.95, nesterov=True, steps=5, muon_eps=1e-8, use_ki
             updated_weight_array = zeropower_via_newtonschulz5(array, steps=steps, eps=muon_eps)
 
             if not use_kimi_scaling:
-                scale = jnp.sqrt(jnp.maximum(1, updated_weight_array.shape[0] / updated_weight_array.shape[1])) # sqrt(Out/In)
+                scale = jnp.sqrt(
+                    jnp.maximum(1, updated_weight_array.shape[0] / updated_weight_array.shape[1])
+                )  # sqrt(Out/In)
             else:
                 scale = 0.2 * jnp.sqrt(jnp.maximum(updated_weight_array.shape[0], updated_weight_array.shape[1]))
             updated_weight_array *= scale
